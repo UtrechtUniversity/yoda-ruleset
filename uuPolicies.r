2af4e1e3728116b06a773fe26c697ad2d948a464
Ton Smeele <a.p.m.smeele@uu.nl>
2015-03-18 15:41:38 +0100

    Setup of system wide policies trash/acl/execcmd

---
+++ uuPolicies.r
@@ -0,0 +1,32 @@
+# \file
+# \brief iRODS policies for Yoda (changes to core.re)
+# \author Ton Smeele
+# \copyright Copyright (c) 2015, Utrecht university. All rights reserved
+# \license GPLv3, see LICENSE
+#
+#test {
+#	acPreProcForExecCmd("foo");
+#}
+
+
+# \brief preProcForExecCmd
+# 			limit the use of OS callouts to a user group "priv-execcmd-all"
+#
+# \param[in]		cmd  name of executable
+#
+acPreProcForExecCmd(*cmd) {
+	*accessAllowed = false;
+	foreach (*rows in SELECT USER_GROUP_NAME WHERE USER_NAME='$userNameClient'
+		             AND USER_ZONE='$rodsZoneClient') {
+		msiGetValByKey(*rows, "USER_GROUP_NAME", *group);
+		if (*group == "priv-execcmd-all") {
+			*accessAllowed = true;
+		}
+	}
+	if (*accessAllowed == false) {
+		msiOprDisallowd;
+	}
+}
+
+#input null
+#output ruleExecOut
