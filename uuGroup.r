e01861135bbda627c6ba4a828d504854d97ea31a
Ton Smeele <a.p.m.smeele@uu.nl>
2015-03-20 11:45:12 +0100

    Added support for zones in uuGroup

--- uuGroup.r
+++ uuGroup.r
@@ -5,7 +5,7 @@
 # \license GPLv3, see LICENSE
 #
 #test() {
-#	*user = "ron";
+#	*user = "ron#tsm";
 #	*group = "yoda";
 #	uuGroupUserExists(*group, *user, *membership);
 #	writeLine("stdout","*user membership of group *group : *membership");
@@ -15,14 +15,23 @@
 # 
 # \param[in] group        name of the irods group
 # \param[in] user         name of the irods user
+#                         username can optionally include zone ('user#zone')
+#                         default is to use the local zone
 # \param[out] membership  true if user is a member of this group
 #
-
 uuGroupUserExists(*group, *user, *membership) {
 	*membership = false;
-	foreach (*row in SELECT USER_NAME WHERE USER_GROUP_NAME=*group) {
+	*userAndZone = split(*user, "#");
+	*userName = elem(*userAndZone,0);
+	if (size(*userAndZone) > 1) {
+		*userZone = elem(*userAndZone,1);
+	} else {
+		*userZone = $rodsZoneClient;
+	}
+	foreach (*row in SELECT USER_NAME,USER_ZONE WHERE USER_GROUP_NAME=*group) {
 		msiGetValByKey(*row, "USER_NAME", *member);
-		if (*member == *user) {
+		msiGetValByKey(*row, "USER_ZONE", *memberZone);
+		if ((*member == *userName) && (*memberZone == *userZone)) {
 			*membership = true;
 		}
 	}
