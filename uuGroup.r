158ec16291d00971327cbbf1566e614651dd1928
Ton Smeele <a.p.m.smeele@uu.nl>
2015-03-24 12:24:55 +0100

    Added uuGroupMemberships

--- uuGroup.r
+++ uuGroup.r
@@ -5,12 +5,35 @@
 # \license GPLv3, see LICENSE
 #
 #test() {
-#	*user = "ron#tsm";
+#	*user = "bert#tsm";
 #	*group = "yoda";
 #	uuGroupUserExists(*group, *user, *membership);
 #	writeLine("stdout","*user membership of group *group : *membership");
+#	uuGroupMemberships(*user, *groups);
+#	foreach (*grp in split(*groups,',')){
+#		writeLine("stdout","grp = *grp");
+#	}
 #}
 
+
+
+# \brief uuGetUserAndZone extract username and zone in separate fields
+# 
+# \param[in] user       name of the irods user
+#                       username can optionally include zone ('user#zone')
+#                       default is to use the local zone
+# \param[out] userName  name of user exclusing zone information
+# \param[out] userZone  name of the zone of the user
+#
+uuGetUserAndZone(*user,*userName,*userZone) {
+	*userAndZone = split(*user, "#");
+	*userName = elem(*userAndZone,0);
+	if (size(*userAndZone) > 1) {
+		*userZone = elem(*userAndZone,1);
+	} else {
+		*userZone = $rodsZoneClient;
+	}
+}
 # \brief uuGroupUserExists check if user if member of a group
 # 
 # \param[in] group        name of the irods group
@@ -21,19 +44,30 @@
 #
 uuGroupUserExists(*group, *user, *membership) {
 	*membership = false;
-	*userAndZone = split(*user, "#");
-	*userName = elem(*userAndZone,0);
-	if (size(*userAndZone) > 1) {
-		*userZone = elem(*userAndZone,1);
-	} else {
-		*userZone = $rodsZoneClient;
-	}
+	uuGetUserAndZone(*user,*userName,*userZone);
 	foreach (*row in SELECT USER_NAME,USER_ZONE WHERE USER_GROUP_NAME=*group) {
 		msiGetValByKey(*row, "USER_NAME", *member);
 		msiGetValByKey(*row, "USER_ZONE", *memberZone);
 		if ((*member == *userName) && (*memberZone == *userZone)) {
 			*membership = true;
 		}
+	}
+}
+
+# \brief uuGroupMemberships lists all groups the user belongs to
+#
+# \param[in] user     name of the irods user
+#                     username can optionally include zone ('user#zone')
+#                     default is to use the local zone
+# \param[out] groups  comma separated list of groupnames
+#
+uuGroupMemberships(*user, *groups) {
+	uuGetUserAndZone(*user,*userName,*userZone);
+	*groups="";
+	foreach (*row in SELECT USER_GROUP_NAME 
+				WHERE USER_NAME = '*userName' AND USER_ZONE = '*userZone') {
+		msiGetValByKey(*row,"USER_GROUP_NAME",*group);
+		*groups = "*groups,*group";
 	}
 }
 #input *group="grp-yc-intake"
