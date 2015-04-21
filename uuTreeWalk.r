1fe4e9d897e6cb19407b793fcc6b5f8b4567b1ba
Ton Smeele <a.p.m.smeele@uu.nl>
2015-04-21 15:32:30 +0200

    Moved buffer control outside uuTreeWalk

--- uuTreeWalk.r
+++ uuTreeWalk.r
@@ -5,19 +5,22 @@
 # \license GPLv3, see LICENSE
 
 #test {
-#	uuTreeWalk(*direction, *topLevelCollection, *ruleToProcess, *error);
+#	*buffer."count" = "0";   # initialize buffer
+#	uuTreeWalk(*direction, *topLevelCollection, *ruleToProcess, *buffer, *error);
 #	writeLine("stdout","result treewalk = *error");
 #}
 
 # here is an example of a rule that can be processed via uuTreeWalk:
 #
-#uuTreeWalkTestrule(*itemParent, *itemName, *itemIsCollection, *buffer) {
-#	writeLine("stdout","*itemParent - *itemName - *itemIsCollection");
-#	*buffer."error" = "0"; # zero means no error 
-#   if (*itemName == "tree.r" ) {
-#		*buffer."error" = "0";
-#	}
-#}
+uuTreeWalkTestrule(*itemParent, *itemName, *itemIsCollection, *buffer, *error) {
+	writeLine("stdout","*itemParent - *itemName - *itemIsCollection - " ++ *buffer."count");
+	*error = 0; # zero means no error 
+   if (*itemName == "tutorial.pdf" ) {
+		*error = 1;
+	} else {
+		*buffer."count" = str( int(*buffer."count") + 1);
+	}
+}
 
 
 # \brief walks through a collection tree and calls an arbitrary rule for each tree-item
@@ -40,23 +43,20 @@
 #                                       to communicate data to subsequent rule invocations
 #                                       buffer."error" can be updated by the rule to indicate
 #                                       an error, the treewalk will stop
+# \param[in/out] buffer          an arbitrary buffer variable that is passed on to the rule
 # \param[out] error             error result as may be set by the rule that is processed
-#                                  a value of "0" means no error
+#                                  a value of 0 means no error
 #                                  other values indicate an error and process is interrupted
-uuTreeWalk(*direction, *topLevelCollection, *ruleToProcess,*error) {
-
-# create a buffer that can be used by the rule that we will call for each item
-# content is arbitrary, just put something in to cast the variable to KV in this scope
-	*buffer."path" = *topLevelCollection;
-	*buffer."error" = "0";
+uuTreeWalk(*direction, *topLevelCollection, *ruleToProcess, *buffer, *error) {
+	*error = 0;
 # start walking at the root of the tree...
 	uuTreeWalkCollection(
 			*direction,
 			*topLevelCollection,
 			*buffer, 
-			*ruleToProcess
+			*ruleToProcess,
+			*error
 	);
-	*error = *buffer."error";
 }
 
 # \brief walk a subtree 
@@ -64,30 +64,32 @@ uuTreeWalk(*direction, *topLevelCollection, *ruleToProcess,*error) {
 # \param [in] path
 # \param [in/out] buffer  (exclusively to be used by the rule we will can)
 # \param [in] rule        name of the rule to be executed in the context of a tree-item 
+# \param [out] error      0 if ok else nonzero
 uuTreeWalkCollection(
 			*direction,
 			*path,
 			*buffer, 
-			*ruleToProcess
+			*ruleToProcess,
+			*error
 	) {
 	uuChopPath(*path, *parentCollection, *collection);
 	if (*direction == "forward") {
 		# first process this collection itself
-		if (*buffer."error" == "0") {
+		if (*error == 0) {
 			# ugly: need to use many if's, irods offers no means to break to end of action
-			eval("{ *ruleToProcess(\*parentCollection,\*collection,true,\*buffer); }");
+			eval("{ *ruleToProcess(\*parentCollection,\*collection,true,\*buffer,\*error); }");
 		}
 		# and the dataobjects located directly within the collection
-		if (*buffer."error" == "0" ) {
+		if (*error == 0 ) {
 			foreach (*row in SELECT DATA_NAME WHERE COLL_NAME = *path) {
 				msiGetValByKey(*row, "DATA_NAME", *dataObject);
-				eval("{ *ruleToProcess(\*path,\*dataObject,false,\*buffer); }");
-				if (*buffer."error" != "0" ) {
+				eval("{ *ruleToProcess(\*path,\*dataObject,false,\*buffer,\*error); }");
+				if (*error != 0 ) {
 					break;
 				}
 			}
 		}
-		if (*buffer."error" == "0" ) {
+		if (*error == 0 ) {
 			# then increase depth to walk through the subcollections
 			foreach (*row in SELECT COLL_NAME WHERE COLL_PARENT_NAME = *path) {
 			msiGetValByKey(*row, "COLL_NAME", *subCollectionPath);
@@ -95,42 +97,44 @@ uuTreeWalkCollection(
 						*direction,
 						*subCollectionPath,
 						*buffer, 
-						*ruleToProcess
+						*ruleToProcess,
+						*error
 				);
 			}
 		}
 	}
 	if (*direction == "reverse") {
 		# first deal with any subcollections within this collection
-		if (*buffer."error" == "0") {
+		if (*error == 0) {
 			foreach (*row in SELECT COLL_NAME WHERE COLL_PARENT_NAME = *path) {
 				msiGetValByKey(*row, "COLL_NAME", *subCollectionPath);
 				uuTreeWalkCollection(
 						*direction, 
 						*subCollectionPath, 
 						*buffer,
-						*ruleToProcess
+						*ruleToProcess,
+						*error
 				);
 			}
 		}
 		# when done then process the dataobjects directly located within this collection
-		if (*buffer."error" == "0") {
+		if (*error == 0) {
 			foreach (*row in SELECT DATA_NAME WHERE COLL_NAME = *path) {
 				msiGetValByKey(*row, "DATA_NAME", *dataObject);
-				eval("{ *ruleToProcess(\*path,\*dataObject,false,\*buffer); }");
-				if (*buffer."error" != "0" ) {
+				eval("{ *ruleToProcess(\*path,\*dataObject,false,\*buffer,\*error); }");
+				if (*error != 0 ) {
 					break;
 				}
 			}
 		}
 		# and lastly process the collection itself
-		if (*buffer."error" == "0") {
-			eval("{ *ruleToProcess(\*parentCollection,\*collection,true,\*buffer); }");
+		if (*error == 0) {
+			eval("{ *ruleToProcess(\*parentCollection,\*collection,true,\*buffer,\*error); }");
 		}
 	}
 }
 
 
 #
-#input *direction="forward",*topLevelCollection="/tsm/home/rods",*ruleToProcess="uuTreeWalkTestrule"
+#input *direction="forward",*topLevelCollection="/nluu1ot/home/ton",*ruleToProcess="uuTreeWalkTestrule"
 #output ruleExecOut
