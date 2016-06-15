493c00e394a01de22bd568ac06ac9529f6dc7e40
Jan de Mooij <A.J.deMooij@gmail.com>
2016-06-15 15:04:13 +0200

    Added policies, but can't get webdav to work, so testing's tricky

--- iiPolicies.r
+++ iiPolicies.r
@@ -1,1 +1,59 @@
 # Policies go in here!
+
+acDataDeletePolicy {
+	uuIiDisallowOperationIfLockedOrFrozen($collParentName, true);
+	uuIiDisallowOperationIfLockedOrFrozen($collName, true);
+}
+
+
+acPreProcForCollCreate {
+	uuIiDisallowOperationIfLockedOrFrozen($collParentName, true);
+	uuIiDisallowOperationIfLockedOrFrozen($collName, true);
+}
+
+# only SuserAndConn available. Need information on object as well
+# acPreProcForModifyAccessControl {
+
+# }
+
+# only SuserAndConn available. Need information on object as well
+#acPreProcForModifyAVUMetadata {
+#
+#}
+
+acPreProcForModifyCollMeta {
+	uuIiDisallowOperationIfLockedOrFrozen($collParentName, true);
+	uuIiDisallowOperationIfLockedOrFrozen($collName, true);
+}
+
+acPreProcForModifyDataObjMeta {
+	uuIiDisallowOperationIfLockedOrFrozen($objPath, false);
+}
+
+acPreProcForObjRename {
+	uuIiDisallowOperationIfLockedOrFrozen($objPath, false);
+}
+
+acPreProcForRmColl {
+	uuIiDisallowOperationIfLockedOrFrozen($collParentName, true);
+	uuIiDisallowOperationIfLockedOrFrozen($collName, true);
+}
+
+acPreProcForDataObjOpen {
+	ON ($writeFlag == "1") {
+		uuIiDisallowOperationIfLockedOrFrozen($objectPath, false);
+	}
+}
+
+uuIiDisallowOperationIfLockedOrFrozen(*objectPath, *isCollection) {
+	uuYcObjectIsLocked(*objPath, *locked);
+	iiObjectIsSnapshotLocked(*objPath, *isCollection, *snaplocked, *frozen);
+
+	if(*locked || *snaplocked || *frozen) {
+		uuYcIsAdminUser(*isAdminUser);
+		if(!*isAdminUser) {
+			cut;
+			msiOprDisallowed;
+		}
+	}
+}
