f6bb574005ea494c8fdf9a9ba6862174902aa6e0
Jan de Mooij <A.J.deMooij@gmail.com>
2016-07-26 15:45:33 +0200

    Removed subof option, as it is inutile

--- iiDataset.r
+++ iiDataset.r
@@ -12,17 +12,17 @@
 #                                                               each value is of format <unix>:userName#userZone
 #
 uuIiGetSnapshotHistory(*collection, *buffer) {
-        *buffer = "";
-        foreach(*row in SELECT META_COLL_ATTR_VALUE
-                        WHERE META_COLL_ATTR_NAME = 'dataset_snapshot_createdAtBy'
-                        AND COLL_NAME = '*collection') {
-                msiGetValByKey(*row, "META_COLL_ATTR_VALUE", *value);
-                if(strlen(*buffer) == 0) {
-                        *buffer = *value;
-                } else {
-                        *buffer = "*buffer,*value";
-                }
-        }
+	*buffer = "";
+	foreach(*row in SELECT META_COLL_ATTR_VALUE
+		WHERE META_COLL_ATTR_NAME = 'dataset_snapshot_createdAtBy'
+		AND COLL_NAME = '*collection') {
+		msiGetValByKey(*row, "META_COLL_ATTR_VALUE", *value);
+		if(strlen(*buffer) == 0) {
+				*buffer = *value;
+		} else {
+				*buffer = "*buffer,*value";
+		}
+	}
 }
 
 # \brief getSnapshotHistory     Gets a history of all snapshots created
@@ -33,27 +33,27 @@ uuIiGetSnapshotHistory(*collection, *buffer) {
 # \param[out] userZone                  Zone of user who created latest snapshot
 #
 uuIiGetLatestSnapshotInfo(*collection, *time, *userName, *userZone) {
-        *buffer = "";
-        *time = 0;
-        foreach(*row in SELECT META_COLL_ATTR_VALUE
-                        WHERE META_COLL_ATTR_NAME = 'dataset_snapshot_createdAtBy'
-                        AND COLL_NAME = '*collection') {
-                msiGetValByKey(*row, "META_COLL_ATTR_VALUE", *value);
-                *timeAndUser = split(*value, ":");
-                *t = int(elem(*timeAndUser, 0));
-                if(*t > *time) {
-                        *buffer = *value;
-                        *time = *t;
-                }
-        }
-        *timeAndUser = split(*buffer, ":");
-        if(size(*timeAndUser) > 0) {
-                uuGetUserAndZone(elem(*timeAndUser, 1),*userName,*userZone);
-        } else {
-                *time = 0;
-                *userName = "";
-                *userZone = "";
-        }
+	*buffer = "";
+	*time = 0;
+	foreach(*row in SELECT META_COLL_ATTR_VALUE
+		WHERE META_COLL_ATTR_NAME = 'dataset_snapshot_createdAtBy'
+		AND COLL_NAME = '*collection') {
+		msiGetValByKey(*row, "META_COLL_ATTR_VALUE", *value);
+		*timeAndUser = split(*value, ":");
+		*t = int(elem(*timeAndUser, 0));
+		if(*t > *time) {
+				*buffer = *value;
+				*time = *t;
+		}
+	}
+	*timeAndUser = split(*buffer, ":");
+	if(size(*timeAndUser) > 0) {
+		uuGetUserAndZone(elem(*timeAndUser, 1),*userName,*userZone);
+	} else {
+		*time = 0;
+		*userName = "";
+		*userZone = "";
+	}
 }
 
 # \brief getFilteredMembers     Returns a list of members of a group, filtered by role
@@ -65,13 +65,76 @@ uuIiGetLatestSnapshotInfo(*collection, *time, *userName, *userZone) {
 # \param[out] memberList        List of group members, filtered by means of argument values
 #
 uuIiGetFilteredMembers(*groupName, *showAdmins, *showUsers, *showReadonly, *memberList) {
-        uuGroupGetMembers(*groupName, *members);
-        *buffer = "";
-        foreach(*userName in *members) {
-                uuGroupUserIsManager(*groupName, *userName, *isManager)
-                if(*showAdmins && *isManager || *showUsers && !*isManager) {
-                        *buffer = "*buffer;*userName";
-                }
-        }
-        *memberList = split(*buffer, ";");
+	uuGroupGetMembers(*groupName, *members);
+	*buffer = "";
+	foreach(*userName in *members) {
+		uuGroupUserIsManager(*groupName, *userName, *isManager)
+		if(*showAdmins && *isManager || *showUsers && !*isManager) {
+				*buffer = "*buffer;*userName";
+		}
+	}
+	*memberList = split(*buffer, ";");
+}
+
+# \brief getDirectories         Returns all collections that match the criteria. Requires
+#                               read access on the project the directories are in
+# 
+# \param[in] showProjects       Show collections that serve as project root
+# \param[in] showStudies        Show collections that serve as study root
+# \param[in] showDatasets       Show collections that serve as datapackage root
+# \param[in] showSubcollections Show collections that appear inside a data package
+# \param[in] requiresContribute Show directories the user has at least contribute access to
+# \param[in] requiresManager    Show directories the user has at least manager access to
+# \param[out] directoryList     List of directories that match the criteria
+#
+uuIiGetDirectories(
+	*showProjects, 
+	*showStudies, 
+	*showDatasets, 
+	*requiresContribute, 
+	*requiresManager,
+	*directoryList ) {
+	if((*showProjects || *showStudies || *showDatasets)) {
+		*user = "$userNameClient#$rodsZoneClient";
+		uuGroupMemberships(*user, *groups);
+		uuIiGetIntakePrefix(*intakePrefix);
+		foreach(*group in *groups) {
+			uuGroupUserIsManager(*group, *user, *isManager);
+			if(!*requireManager || *isManager){
+				#TODO: adapt for readonly role addition
+				writeLine("stdout", *group);
+				writeLine("stdout", *intakePrefix);
+				writeLine("stdout", *group like '*intakePrefix*');
+				writeLine("stdout", "\n");
+				if(*group like "*intakePrefix*") {
+					foreach(*project in SELECT COLL_NAME WHERE COLL_NAME like '%/*group') {
+						*cn = *project.COLL_NAME;
+						if(*showProjects){
+							*directories = "*directories:*cn";
+						}
+						if(*showStudies || *showDatasets) {
+							foreach(*study in SELECT COLL_NAME WHERE COLL_PARENT_NAME = '*cn') {
+								*sn = *study.COLL_NAME;
+								if(*showStudies) {
+									*directories = "*directories:*sn";
+								}
+								if(*showDatasets) {
+									foreach(*dataset in SELECT COLL_NAME WHERE COLL_PARENT_NAME = '*sn') {
+										*dn = *dataset.COLL_NAME;
+										if(*showDatasets) {
+											*directories = "*directories:*dn";
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		*directories = triml(*directories,":");
+		*directoryList = split(*directories, ":");
+	} else {
+		*directoryList = list();
+	}
 }
