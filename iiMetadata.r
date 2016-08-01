53dcd4e63ae7034e96e377b264f8cc4a5778a446
Jan de Mooij <A.J.deMooij@gmail.com>
2016-08-01 09:43:42 +0200

    (re?)added rule to extract all possible values for a key, given a certain search string

--- iiMetadata.r
+++ iiMetadata.r
@@ -267,3 +267,32 @@ uuExplode(*string, *separator, *resultList) {
 
 	*resultList = cons(*tail, *result);
 }
+
+# \brief GetAvailableValuesForKeyLike Returns list of values that exist in the
+# 										icat database for a certain key, which
+# 										are like a certain value
+#
+# \param[in] *key 						Key to look for
+# \param[in] *searchString 				String that should be a substring of the
+# 										returned values
+# \param[in] *isCollection 				Wether to look in collection or data 
+# \param[out] *values 					List of possible values for the given key
+# 										where the given search string is a substring of
+uuIiGetAvailableValuesForKeyLike(*key, *searchString, *isCollection, *values);
+	*values = list();
+
+	if(*isCollection){
+		foreach(*row in SELECT META_COLL_ATTR_VALUE WHERE 
+			META_COLL_ATTR_NAME = '*key' AND
+			META_COLL_ATTR_VALUE like '%*searchString%') {
+			writeLine("stdout", *row.META_COLL_ATTR_VALUE);
+			*values = cons(*row.META_COLL_ATTR_VALUE,*values);
+		}
+	} else {
+		foreach(*row in SELECT META_DATA_ATTR_VALUE WHERE 
+			META_DATA_ATTR_NAME = '*key' AND
+			META_DATA_ATTR_VALUE like '%*searchString%') {
+			*values = cons(*row.META_DATA_ATTR_VALUE,*values);
+		}
+	}
+}
