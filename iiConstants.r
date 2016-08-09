7a99215c8f747b5d040f94d540740ac80d086165
Jan de Mooij <A.J.deMooij@gmail.com>
2016-08-09 13:47:50 +0200

    Created postProcessing rule for new collections, which adds a meta data flag indicating it is the first version of a dataset, if the collection is created on the versioning level (i.e. if it's a dataset / can be written to the vault). Removed uuExplode, because the microservice 'split' has the same functionality and works better

--- iiConstants.r
+++ iiConstants.r
@@ -31,10 +31,32 @@ uuIiGetVaultPrefix(*vaultPrefix) {
 	*vaultPrefix = *grp ++ "vault-";
 }
 
-# \breif uuIiGetMetadataPrefix 	Get prefix for metadata which the portal uses
+# \brief uuIiGetMetadataPrefix 	Get prefix for metadata which the portal uses
 #								If this constant is updated, also update
 # 								the PHP Portal config
 # \param[out] metadataPrefix 	The prefix used for metadata by the portal
 uuIiGetMetadataPrefix(*metadataPrefix) {
 	*metadataPrefix = "ilab_intake_metadata_";
+}
+
+# \brief uuIiIntakeLevel 		Get the depth at which level the intake
+# 								takes place, i.e. which level is defined
+# 								to be a dataset, which can have versions
+#								and which can be snapshotted or archived
+# 								MAKE SURE TO have this correspond to the
+#								PHP Portal!
+# 
+# \param[out] *level 			The depth of the datapackage counted from
+# 								anything below /<zone>/home/<level1>/<level2>...
+uuIiIntakeLevel(*level) {
+	*level = 3;
+}
+
+# \breif uuIiVersionPrefix 		Get the prefix used for versions (on top
+# 								of the default metadata prefix)
+# \param[out] versionPrefix 	The prefix used for versions in iRods metadata
+uuIiVersionKey(*versionKey, *dependsKey) {
+	uuIiGetMetadataPrefix(*prfx);
+	*versionKey = *prfx ++ "version";
+	*dependsKey = *prfx ++ "depends_on";
 }
