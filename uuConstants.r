e7e5dd3c32f2eed2dfb31276c7b9dae25d24af91
Paul Frederiks <paul.frederiks@uu.nl>
2017-03-21 17:10:26 +0100

    Rename system collection to yoda

--- uuConstants.r
+++ uuConstants.r
@@ -6,7 +6,7 @@ UUUSERMETADATAPREFIX = "usr_"
 
 # \constant UUSYSTEMCOLLECTION   irods path of a system collection to store system support files in
 # Needs to be prepended with irods zone.
-UUSYSTEMCOLLECTION = "/system"
+UUSYSTEMCOLLECTION = "/yoda"
 
 # \constant  UUREVISIONCOLLECTION   irods path where all revisions will be stored
 UUREVISIONCOLLECTION = UUSYSTEMCOLLECTION ++ "/revisions"
