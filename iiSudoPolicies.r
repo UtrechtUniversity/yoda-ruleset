d5707c48cb60400bf83c6062111cb642e99febcb
Paul Frederiks <paul.frederiks@uu.nl>
2017-05-15 10:28:24 +0200

    Remove metadata sudo policies

--- iiSudoPolicies.r
+++ iiSudoPolicies.r
@@ -1,17 +1,3 @@
-
-acPreSudoObjMetaSet(*objName, *objType, *attribute, *value, *unit, *policyKv) {
-	ON (*objType == "-c") {
-		iiDatamanagerPreSudoObjMetaSet(*objName, *objType, *attribute, *value, *unit, *policyKv);
-	}
-}
-
-
-acPreSudoObjMetaAdd(*objName, *objType, *attribute, *value, *unit, *policyKv) {
-        ON (*objType == "-c") {
-                iiDatamanagerPreSudoObjMetaAdd(*objName, *objType, *attribute, *value, *unit, *policyKv);
-        }
-}
-
 acPreSudoObjAclSet(*recursive, *accessLevel, *otherName, *objPath, *policyKv) {
 	ON (*otherName like "datamanager-*") {
 		iiDatamanagerPreSudoObjAclSet(*recursive, *accessLevel, *otherName, *objPath, *policyKv);
