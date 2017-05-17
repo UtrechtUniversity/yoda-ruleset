aa98df4ff9099c85ffdbc45821f0a5656867a3a3
Paul Frederiks <paul.frederiks@uu.nl>
2017-05-17 17:15:23 +0200

    Split datamanager sudo policy in two parts

--- iiDatamanagerPolicies.r
+++ iiDatamanagerPolicies.r
@@ -2,25 +2,42 @@
 # \brief Sudo microservices policy implementations to enable datamanager control of vault process
 # \author Paul Frederiks
 # \copyright Copyright (c) 2017, Utrecht University. All rights reserved
-# \licens GPLVv3 LICENSE
+# \licens GPLv3 LICENSE
 
+# \brief iiDatamanagerPreSudoObjAclSet
 iiDatamanagerPreSudoObjAclSet(*recursive, *accessLevel, *otherName, *objPath, *policyKv) {
-	*actor = *policyKv.actor;
-	iiFolderStatus(*objPath, *folderStatus);
-	writeLine("serverLog", "iiDatamanagerPreSudoObjAclSet: folderStatus = *folderStatus");
-	if (*folderStatus == SUBMITTED || *folderStatus == ACCEPTED || *folderStatus == REJECTED) {
-		iiCollectionGroupName(*objPath, *groupName);
-		uuGroupGetCategory(*groupName, *category, *subcategory);
-		writeLine("serverLog", "iiDatamanagerPreSudoObjAclSet: *category; *otherName");
-		if (*otherName == "datamanager-*category") {
-			
-			uuGroupGetMemberType(*otherName, *actor, *userTypeIfDatamanager);
-			writeLine("serverLog", "iiDatamanagerPreSudoObjAclSet: *actor - *userTypeIfDatamanager");
-			if (*userTypeIfDatamanager == "normal" || *userTypeIfDatamanager == "manager") {
-				
-				succeed;
-			}
+	if (*recursive == 0 && (*accesslevel == "write" || *accessLevel == "read")) {
+		*actor = uuClientFullName;
+		iiCanDatamanagerAclSet(*objPath, uuClientFullName, *allowed, *reason);
+		if (*allowed) {
+			succeed;
 		}
 	}
 	fail;
+}
+
+
+# \brief iiCanDatamanagerAclSet
+iiCanDatamanagerAclSet(*objPath, *actor, *allowed, *reason) {
+	iiFolderStatus(*objPath, *folderStatus);
+	if (*folderStatus == SUBMITTED || *folderStatus == ACCEPTED || *folderStatus == REJECTED) {
+		iiCollectionGroupName(*objPath, *groupName);
+		uuGroupGetCategory(*groupName, *category, *subcategory);
+		if (*otherName == "datamanager-*category") {
+			uuGroupGetMemberType(*otherName, *actor, *userTypeIfDatamanager);
+			if (*userTypeIfDatamanager == "normal" || *userTypeIfDatamanager == "manager") {
+				*allowed = true;
+				*reason = "User is a datamanager of category *category.";
+			} else {
+				*allowed = false;
+				*reason = "User is not a datamanager.";
+			}
+		} else {
+			*allowed = false;
+			*reason = "Permission can only be granted to the datamanager-*category group, not *otherName.";
+		}
+	} else {
+		*allowed = false;
+		*reason = "A datamanager has no permission to alter *objPath with status '*folderStatus'";
+	}
 }
