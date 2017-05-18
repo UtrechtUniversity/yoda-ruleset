9ce0b438c12b3e05d33dbd74a9ba1918cf5df640
Paul Frederiks <paul.frederiks@uu.nl>
2017-05-18 17:54:54 +0200

    More WIP

--- iiVault.r
+++ iiVault.r
@@ -1,4 +1,13 @@
 iiCopyFolderToVault(*folder) {
-	writeLine("serverLog", "iiCopyFolderToVault: Dummy rule triggered for *folder");
+	*err = errorcode(iiCollectionGroupName(*folder, *groupName));
+	if (*err < 0) {
+		failmsg(-1, "NoResearchGroup");
+	}
+	uuChop(*groupName, *_, *baseName, "-", true);
+	uuChopPath(*folder, *parent, *datapackageName);
+	msiGetIcatTime(*timestamp, "unix");
+        *vaultGroupName = "vault-*baseName";
+	*target = "/$rodsZoneClient/home/*vaultGroupName/*datapackageName" ++ "_*timestamp";
+	
 	iiFolderSecure(*folder);
 }
