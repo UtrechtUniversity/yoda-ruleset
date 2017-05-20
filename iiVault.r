a057a316282175e938ccc8371cba7ed640e74e7a
Paul Frederiks <paul.frederiks@uu.nl>
2017-05-20 19:29:22 +0200

    Use treewalk to copy data

--- iiVault.r
+++ iiVault.r
@@ -8,6 +8,47 @@ iiCopyFolderToVault(*folder) {
 	msiGetIcatTime(*timestamp, "unix");
         *vaultGroupName = "vault-*baseName";
 	*target = "/$rodsZoneClient/home/*vaultGroupName/*datapackageName" ++ "_*timestamp";
-	
+	*buffer.source = *folder;
+	*buffer.destination = *target;
+	uuTreeWalk("forward", *folder, "iiIngestObject", *buffer, *error);
+	iiCopyRelevantMetadata(*folder, *target);
 	iiFolderSecure(*folder);
+}
+
+iiIngestObject(*itemParent, *itemName, *itemIsCollection, *buffer) {
+	*sourcePath = "*itemParent/*itemName";
+	*destPath = *buffer.destination;
+	if (*sourcePath != *buffer."source") {
+		# rewrite path to copy objects that are located underneath the toplevel collection
+		*sourceLength = strlen(*sourcePath);
+		*relativePath = substr(*sourcePath, strlen(*buffer."source") + 1, *sourceLength);
+		*destPath = *buffer."destination" ++ "/" ++ *relativePath;
+	}
+	if (*itemIsCollection) {
+		msiCollCreate(*destPath, "1", *status);
+	} else {
+		msiDataObjChksum(*sourcePath, "forceChksum=", *checksum);
+		msiDataObjCopy(*sourcePath, *destPath, "verifyChksum=", *status);
+	}
+
+}
+
+iiCopyRelevantMetadata(*source, *destination) {
+	*userMetadataPrefix = UUUSERMETADATAPREFIX ++ "%";
+	foreach(*row in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE
+			WHERE COLL_NAME = *source
+			AND META_COLL_ATTR_NAME like *userMetadataPrefix) {
+		msiString2KeyValPair("", *kvp);
+		msiAddKeyVal(*kvp, *row.META_COLL_ATTR_NAME, *row.META_COLL_ATTR_VALUE);
+		msiAssociateKeyValuePairsToObj(*kvp, *destination, "-C");
+	}
+
+	*actionLog = UUORGMETADATAPREFIX ++ "action_log";	
+	foreach(*row in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE
+	       		WHERE META_COLL_ATTR_NAME = *actionLog
+		       	AND COLL_NAME = *source) {
+		msiString2KeyValPair("", *kvp);
+		msiAddKeyVal(*kvp, *row.META_COLL_ATTR_NAME, *row.META_COLL_ATTR_VALUE);
+		msiAssociateKeyValuePairsToObj(*kvp, *destination, "-C");
+	}
 }
