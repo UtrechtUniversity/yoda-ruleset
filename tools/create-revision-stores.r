949be93c39faa92c3a20aaeee1387f59c73dee2f
Paul Frederiks <paul.frederiks@uu.nl>
2017-06-08 15:12:29 +0200

    migration script create-revision-stores.r

---
+++ tools/create-revision-stores.r
@@ -0,0 +1,21 @@
+createRevisionStores {
+	uuGetUserType("$userNameClient#$rodsZoneClient", *usertype);
+
+	if (*usertype != "rodsadmin") {
+		failmsg(-1, "This script needs to be run by a rodsadmin");
+	}
+
+	foreach (*row in SELECT USER_NAME WHERE USER_TYPE = 'rodsgroup' AND USER_NAME like 'research-%') {
+		*groupName = *row.USER_NAME;
+
+		*revisionStore = "/" ++ $rodsZoneClient ++ UUREVISIONCOLLECTION ++ "/" ++ *groupName;
+		if (!uuCollectionExists(*revisionStore)) {
+			writeLine("stdout", "Creating *revisionStore");
+			msiCollCreate(*revisionStore, 1, *status);
+		}
+
+	}
+}
+
+input null
+output ruleExecOut
