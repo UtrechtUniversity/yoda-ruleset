a31585b8dc18d8ee7b35fd249671aedcbe9c111b
Paul Frederiks <paul.frederiks@uu.nl>
2017-06-09 14:59:57 +0200

    create-revision-stores will now make initial revisions

--- tools/create-revision-stores.r
+++ tools/create-revision-stores.r
@@ -1,3 +1,4 @@
+#!/usr/bin/irule -F
 createRevisionStores {
 	uuGetUserType("$userNameClient#$rodsZoneClient", *usertype);
 
@@ -12,6 +13,115 @@ createRevisionStores {
 		if (!uuCollectionExists(*revisionStore)) {
 			writeLine("stdout", "Creating *revisionStore");
 			msiCollCreate(*revisionStore, 1, *status);
+			msiSetACL("recursive", "own", *groupName, *revisionStore);
+
+			*previousPath = "";
+			*groupColl = "/$rodsZoneClient/home/*groupName"
+			writeLine("stdout", "Scanning *groupColl for data objects to create initial revisions");
+
+			# rodsadmin needs at least read access to research group to copy data
+			# unfortunately msiCheckAccess does not check for group membership, but it won't be a problem
+			# when we add a user level ACL.
+			msiCheckAccess(*groupColl, "read object", *readPermission);
+
+			if (*readPermission == 0) {
+				writeLine("stdout", "Granting read access to *groupColl");
+				msiSetACL("recursive", "admin:read", uuClientFullName, *groupColl);
+			}
+
+			# First process the main collection	
+			foreach(*row in SELECT COLL_NAME, DATA_NAME, DATA_RESC_NAME where COLL_NAME = *groupColl) {
+				*collName = *row.COLL_NAME;
+				*dataName = *row.DATA_NAME;
+				*resource = *row.DATA_RESC_NAME;
+				*path = "*collName/*dataName";
+
+				*skip = false;	
+
+				foreach(*filter in UUBLACKLIST) {
+					if (*dataName like *filter) {
+						writeLine("stdout", "Ignore *path for revision store. Filter *filter matches");
+						*skip = true;
+						break;
+					}
+				}
+
+				if (*path == *previousPath) {
+					# Skip replicas
+					*skip = true;
+				}
+
+				if (!*skip) {
+					# We need read access on the original object
+					# unfortunately msiCheckAccess does not check for group membership, but it won't be a problem
+					# when we add a user level ACL.
+					msiCheckAccess(*path, "read object", *objectReadPermission);
+					if (*objectReadPermission == 0) {
+						writeLine("stdout", "Granting read access to *path");
+						msiSetACL("default", "admin:read", uuClientFullName, *path);
+					}
+
+					iiRevisionCreate(*resource, *path, UUMAXREVISIONSIZE, *id);
+					if (*id != "") {
+				        	writeLine("serverLog", "Revision created for *path with id: *id");	
+					}
+
+					if (*objectReadPermission == 0) {
+						writeLine("stdout", "Revoking read access to *path");
+						msiSetACL("default", "admin:null", uuClientFullName, *path);
+					}
+
+					*previousPath = *path;
+				}
+			}
+			# Then process the rest of the tree	
+			foreach(*row in SELECT COLL_NAME, DATA_NAME, DATA_RESC_NAME where COLL_NAME like "*groupColl/%") {
+				*collName = *row.COLL_NAME;
+				*dataName = *row.DATA_NAME;
+				*resource = *row.DATA_RESC_NAME;
+				*path = "*collName/*dataName";
+
+				foreach(*filter in UUBLACKLIST) {
+					if (*dataName like *filter) {
+						writeLine("stdout", "Ignore *path for revision store. Filter *filter matches");
+						*skip = true;
+					}
+				}
+
+				if (*path == *previousPath) {
+					# Skip replicas
+					*skip = true;
+				}
+
+				if (!*skip) {
+					msiCheckAccess(*path, "read object", *objectReadPermission);
+					if (*objectReadPermission == 0) {
+						writeLine("stdout", "Granting read access to *path");
+						msiSetACL("default", "admin:read", uuClientFullName, *path);
+					}
+
+					iiRevisionCreate(*resource, *path, UUMAXREVISIONSIZE, *id);
+					if (*id != "") {
+				        	writeLine("serverLog", "Revision created for *path with id: *id");	
+					}
+					
+					if (*objectReadPermission == 0) {
+						writeLine("stdout", "Revoking read access to *path");
+						msiSetACL("default", "admin:null", uuClientFullName, *path);
+					}
+
+					*previousPath = *path;
+				}
+			}
+			
+			# Remove the temporary read ACL
+			if (*readPermission == 0) {
+				writeLine("stdout", "Revoking read access to *groupColl");
+				msiSetACL("recursive", "admin:null", uuClientFullName, *groupColl);
+			}
+
+					
+
 		}
 
 	}
