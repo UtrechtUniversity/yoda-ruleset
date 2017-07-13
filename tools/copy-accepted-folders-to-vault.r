ff9471624d22b22b68cc59a301b7e0258de384c2
Paul Frederiks <paul.frederiks@uu.nl>
2017-07-13 17:27:01 +0200

    After succesful vault copy add flag

--- tools/copy-accepted-folders-to-vault.r
+++ tools/copy-accepted-folders-to-vault.r
@@ -1,4 +1,5 @@
 copyToVault {
+	# This script is kept as dumb as possible. All processing and error handling is done by iiFolderSecure
 	*ContInxOld = 1;
 	msiAddSelectFieldToGenQuery("COLL_NAME", "", *GenQInp);	
 	msiAddConditionToGenQuery("META_COLL_ATTR_NAME", "=", IISTATUSATTRNAME, *GenQInp);
@@ -11,7 +12,8 @@ copyToVault {
 	while(*ContInxOld > 0) {
 		foreach(*row in *GenQOut) {
 			*folder = *row.COLL_NAME;
-			iiCopyFolderToVault(*folder) ::: nop;
+			# When iiFolderSecure fails continue with the other folders.
+			iiFolderSecure(*folder) ::: nop;
 		}
 
 		*ContInxOld = *ContInxNew;
