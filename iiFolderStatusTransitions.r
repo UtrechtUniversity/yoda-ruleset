305db1d30655dd0f1435de09b694f3cd5d8923d5
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-08-18 14:22:09 +0200

    Fix for backwards compatibility.

--- iiFolderStatusTransitions.r
+++ iiFolderStatusTransitions.r
@@ -281,7 +281,7 @@ iiFolderApprove(*folder, *status, *statusInfo) {
 	*statusInfo = "An internal error has occurred";
 
 	iiFolderStatus(*folder, *currentFolderStatus);
-	if (*currentFolderStatus == SECURED) {
+	if (*currentFolderStatus != APPROVED) {
 		*folderStatusStr = IISTATUSATTRNAME ++ "=" ++ APPROVED;
 		msiString2KeyValPair(*folderStatusStr, *folderStatusKvp);
 		*err = errormsg(msiSetKeyValuePairsToObj(*folderStatusKvp, *folder, "-C"), *msg);
