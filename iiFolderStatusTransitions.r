44881f86aba9ea3cb8e09ab931f97770b19a664b
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-08-18 14:12:52 +0200

    Check for correct folder status.

--- iiFolderStatusTransitions.r
+++ iiFolderStatusTransitions.r
@@ -281,7 +281,7 @@ iiFolderApprove(*folder, *status, *statusInfo) {
 	*statusInfo = "An internal error has occurred";
 
 	iiFolderStatus(*folder, *currentFolderStatus);
-	if (*currentFolderStatus == FOLDER) {
+	if (*currentFolderStatus == SECURED) {
 		*folderStatusStr = IISTATUSATTRNAME ++ "=" ++ APPROVED;
 		msiString2KeyValPair(*folderStatusStr, *folderStatusKvp);
 		*err = errormsg(msiSetKeyValuePairsToObj(*folderStatusKvp, *folder, "-C"), *msg);
