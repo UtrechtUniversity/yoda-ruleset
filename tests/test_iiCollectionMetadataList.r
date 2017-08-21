42c89b55ffa2ab414a496d1a2741bffb14c161ed
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-08-21 16:36:07 +0200

    Remove old test.

--- tests/test_iiCollectionMetadataList.r
+++
@@ -1,9 +0,0 @@
-testRule {
-	iiCollectionMetadataList(*path, *prefix, *lst)
-	writeLine("stdout", *lst);
-
-	uuKvpList2JSON(*lst, *lst_str, *size);
-	writeLine("stdout", *lst_str);
-}
-input *path="", *prefix=""
-output ruleExecOut
