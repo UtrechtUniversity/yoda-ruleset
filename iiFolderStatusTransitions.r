78791b630643900e5b474d0b1fea4e2b97fba7a3
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-08-31 14:34:49 +0200

    Revert "Rework vault status transitions."
    This reverts commit 8411014e36da5080db84ec6036af48a3b2779cdb.

--- iiFolderStatusTransitions.r
+++ iiFolderStatusTransitions.r
@@ -372,7 +372,7 @@ iiFolderReject(*folder, *status, *statusInfo) {
 # \param[out] status        status of the action
 # \param[out] statusInfo    Informative message when action was not successfull
 iiFolderApprove(*folder, *status, *statusInfo) {
-	iiFolderDatamanagerAction(*folder, APPROVED_FOR_PUBLICATION, *status, *statusInfo);
+	iiFolderDatamanagerAction(*folder, APPROVED, *status, *statusInfo);
 }
 
 
@@ -406,8 +406,13 @@ iiFolderSecure(*folder) {
 	}
 
 	iiCopyActionLog(*folder, *target);
-	msiString2KeyValPair(UUORGMETADATAPREFIX ++ "vault_status=" ++ UNPUBLISHED, *vaultStatusKvp);
+	msiString2KeyValPair(UUORGMETADATAPREFIX ++ "vault_status=" ++ COMPLETE, *vaultStatusKvp);
 	msiSetKeyValuePairsToObj(*vaultStatusKvp, *target, "-C");
+
+	# Set status of vault package to unpublished.
+	*folderStatusStr = IISTATUSATTRNAME ++ "=" ++ UNPUBLISHED;
+	msiString2KeyValPair(*folderStatusStr, *folderStatusKvp);
+        msiSetKeyValuePairsToObj(*folderStatusKvp, *target, "-C");
 }
 
 
