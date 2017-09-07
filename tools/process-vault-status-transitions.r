492bfc6d2d1223e531bb6d940b1c88649dcd9bde
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-09-07 16:57:48 +0200

    More work on vault status cronjob.

--- tools/process-vault-status-transitions.r
+++ tools/process-vault-status-transitions.r
@@ -12,38 +12,38 @@ processVaultActions {
 		foreach(*row in *GenQOut) {
 			*collName = *row.COLL_NAME;
 
-`			# Check if vault status transition is requested in research or datamanager group.
+			# Check if vault status transition is requested in research or datamanager group.
 			if (*collName like regex "/[^/]+/home/research-.*" ||
 			    *collName like regex "/[^/]+/home/datamanager-.*") {
-                               *folder = "";
-                               *action = "";
-                               *actor = "";
-                               msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *folder, "get", 0);
-                               msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *action, "get", 1);
-                               msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *actor, "get", 2);
+                                *folder = "";
+                                *action = "";
+                                *actor = "";
+                                msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *folder, "get", 0);
+                                msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *action, "get", 1);
+                                msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *actor, "get", 2);
 
-			       # Retrieve collection id from folder.
-			       foreach(*row in SELECT COLL_ID WHERE COLL_NAME = *folder) {
-			               *collId = *row.COLL_ID;
-			       }
+                                # Retrieve collection id from folder.
+                                foreach(*row in SELECT COLL_ID WHERE COLL_NAME = *folder) {
+                                        *collId = *row.COLL_ID;
+                                }
 
-			       # Check if vault package is currently pending for status transition.
-			       *pending = false;
-			       *vaultActionStatus = UUORGMETADATAPREFIX ++ "vault_status_action_*collId";
-			       foreach(*row in SELECT COLL_ID WHERE META_COLL_ATTR_NAME = *vaultActionStatus AND META_COLL_ATTR_VALUE = 'PENDING') {
-			               *pending = true;
-			       }
+                                # Check if vault package is currently pending for status transition.
+                                *pending = false;
+                                *vaultActionStatus = UUORGMETADATAPREFIX ++ "vault_status_action_*collId";
+                                foreach(*row in SELECT COLL_ID WHERE META_COLL_ATTR_NAME = *vaultActionStatus AND META_COLL_ATTR_VALUE = 'PENDING') {
+                                        *pending = true;
+                                }
 
 			       # Perform status transition if action is pending.
 			       if (*pending) {
-                                        *err = errorcode(iiVaultProcessStatusTransition(*folder, *action, *actor, *status, *statusInfo));
-				        if (*err < 0) {
-				               writeLine("stdout", "iiVaultProcessStatusTransition: *err");
-					       *status = "InternalError";
-					       *statusInfo = "";
-				        }
+					*err = errorcode(iiVaultProcessStatusTransition(*folder, *action, *actor, *status, *statusInfo));
+					if (*err < 0) {
+						writeLine("stdout", "iiVaultProcessStatusTransition: *err");
+						*status = "InternalError";
+						*statusInfo = "";
+					}
 
-				        if (*status != "Success") {
+					if (*status != "Success") {
 						*vaultAction = UUORGMETADATAPREFIX ++ "vault_action_" ++ "*collId" ++ "=" ++ *row.META_COLL_ATTR_VALUE;
 						*vaultStatus = UUORGMETADATAPREFIX ++ "vault_status_action_" ++ "*collId" ++ "=FAIL";
 						msiString2KeyValPair(*vaultAction, *vaultActionKvp);
@@ -51,16 +51,16 @@ processVaultActions {
 						*err = errormsg(msiRemoveKeyValuePairsFromObj(*vaultActionKvp, *collName, "-C"), *msg);
 						msiSetKeyValuePairsToObj(*vaultStatusKvp, *collName, "-C");
 						writeLine("stdout", "iiVaultProcessStatusTransition: *status - *statusInfo");
-				        } else {
-					        *vaultAction = UUORGMETADATAPREFIX ++ "vault_action_" ++ "*collId" ++ "=" ++ *row.META_COLL_ATTR_VALUE;
+					} else {
+						*vaultAction = UUORGMETADATAPREFIX ++ "vault_action_" ++ "*collId" ++ "=" ++ *row.META_COLL_ATTR_VALUE;
 						*vaultStatus = UUORGMETADATAPREFIX ++ "vault_status_action_" ++ "*collId" ++ "=PENDING";
 						msiString2KeyValPair(*vaultAction, *vaultActionKvp);
 						msiString2KeyValPair(*vaultStatus, *vaultStatusKvp);
 						*err = errormsg(msiRemoveKeyValuePairsFromObj(*vaultActionKvp, *collName, "-C"), *msg);
 						*err = errormsg(msiRemoveKeyValuePairsFromObj(*vaultStatusKvp, *collName, "-C"), *msg);
 						writeLine("stdout", "iiVaultProcessStatusTransition: Successfully processed *action by *actor on *folder");
-				        }
-			        }
+					}
+				}
 			}
 		}
 
