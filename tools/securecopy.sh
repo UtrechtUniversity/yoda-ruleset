bf0098426f54fadd3530214dbbd054f21a61581c
Paul Frederiks <p.frederiks@uu.nl>
2017-10-05 09:42:18 +0200

    Add timeouts to securecopy.sh

--- tools/securecopy.sh
+++ tools/securecopy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -e
 PHYPATH=$1
 HOST=$2
 USER=$3
-PRIVATEKEY=$4
-DESTINATION=$5
+DESTINATION=$4
+DESTDIR=$(dirname $DESTINATION)
 echo "Calling:"
+echo "ssh $USER@$HOST mkdir -p $DESTDIR"
+timeout 5s ssh $USER@$HOST mkdir -p $DESTDIR
 echo "scp -i $HOME/.ssh/$4 $PHYPATH $USER@$HOST:$DESTINATION"
-scp -i $HOME/.ssh/$4 $PHYPATH $USER@$HOST:$DESTINATION
+timeout 5s scp $PHYPATH $USER@$HOST:$DESTINATION
+echo "ssh $USER@$HOST chmod g+r $DESTINATION"
+timeout 5s ssh $USER@$HOST chmod g+r $DESTINATION
