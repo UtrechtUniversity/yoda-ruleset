688998fa1d7c06a350b4a600d6dd58750ff9af87
Paul Frederiks <p.frederiks@uu.nl>
2017-11-06 05:51:57 +0100

    Skip vault requests with invalid JSON

--- tools/process-vault-status-transitions.r
+++ tools/process-vault-status-transitions.r
@@ -15,13 +15,16 @@ processVaultActions {
 			# Check if vault status transition is requested in research or datamanager group.
 			if (*collName like regex "/[^/]+/home/research-.*" ||
 			    *collName like regex "/[^/]+/home/datamanager-.*") {
+				
                                 *folder = "";
                                 *action = "";
                                 *actor = "";
-                                msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *folder, "get", 0);
-                                msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *action, "get", 1);
-                                msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *actor, "get", 2);
-
+                                *err1 = errorcode(msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *folder, "get", 0));
+                                *err2 = errorcode(msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *action, "get", 1));
+                                *err3 = errorcode(msi_json_arrayops(*row.META_COLL_ATTR_VALUE, *actor, "get", 2));
+				if (*err1 < 0 || *err2 < 0 || *err3 < 0) {
+					writeLine("stdout", "Failed to process vault request on *collName");
+				} else { # skip processing this vault request	
                                 # Retrieve collection id from folder.
                                 foreach(*row in SELECT COLL_ID WHERE COLL_NAME = *folder) {
                                         *collId = *row.COLL_ID;
@@ -75,6 +78,7 @@ processVaultActions {
                                                 msiSetACL("default", "admin:null", uuClientFullName, *collName);
                                         }
 				}
+			        } #/skip processing this vault request
 			}
 		}
 
