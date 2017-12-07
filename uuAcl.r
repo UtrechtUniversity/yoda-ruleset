13eb54aa001dc5be216acb9b55c3c0973952a594
Paul Frederiks <p.frederiks@uu.nl>
2017-12-07 14:58:23 +0000

    Comment uuAcl and enhance uuAclListSetDiff with idOnly option

--- uuAcl.r
+++ uuAcl.r
@@ -1,6 +1,19 @@
+# \file	     uuAcl.r
+# \brief     Functions to work with ACL's as lists of tuples 
+# \author    Paul Frederiks
+# \copyright Copyright (c) 2017 Utrecht University. All rights reserved
+# \license   GPLv3, see LICENSE
+
+
+
+# \brief uuacl a datatype to represent an ACL consisting of a tuple with two integers.
+#        the first should be a user or group ID, the second an access type as found in COl_ACCESS_TYPE or DATA_ACCESS_TYPE
 data uuacl = 
 	| uuacl : int * int -> uuacl
 
+# \brief uuids function to return all the user and group ids found in a list of uuacl's
+# \param[in] aclList   a list of uuacl's
+# \returnvalue   a list of user ids (integer)
 uuids(*aclList) {
 	*ids = list();
 	foreach(*acl in *aclList) {
@@ -10,6 +23,8 @@ uuids(*aclList) {
 	*ids;
 }
 
+# \brief uuAclEqual function to compare two uuacl's
+# \returnvalue false if uuacl aren't equal and true if they are
 uuAclEqual(*aclA, *aclB) {
 	uuacl(*userIdA, *accessTypeA) = *aclA;
 	uuacl(*userIdB, *accessTypeB) = *aclB;
@@ -20,7 +35,14 @@ uuAclEqual(*aclA, *aclB) {
 	*equality;
 }
 	
-
+# \brief uuAclToStrings   find the user name and access level belonging to an uuacl
+# \param[in] acl          an uuacl
+# \param[out] userName    the username of the user or group belonging to the id
+# \param[out] accessLevel One of 'own', 'write', 'read' or 'null'. These are
+#                         converted to access types again during a msiSetACL call.
+#                         There are a few other access types available in the 
+#                         irods token table, but they aren't included in either
+#                         the icommands or microservices.
 uuAclToStrings(*acl, *userName, *accessLevel) {
 	*userName = "";
 	*accessLevel = "";
@@ -30,17 +52,20 @@ uuAclToStrings(*acl, *userName, *accessLevel) {
 	}
 
 	if (*accessType > 1120) {
-		*accessLevel = 'own';
+		*accessLevel = 'own'; # 1200
 	} else if (*accessType > 1050) {
-		*accessLevel = 'write';
+		*accessLevel = 'write'; # 1120
 	} else if (*accessType > 1000) {
-		*accessLevel = 'read';
+		*accessLevel = 'read'; # 1050
 	} else {
-		*accessLevel = 'null';
+		*accessLevel = 'null'; # 1000
 	}
 	
 }
 
+# \brief uuAclListOfPath generate a list of uuacl's from a path
+# \param[in] path      collection or data object
+# \param[out] aclList  list of uuacl's
 uuAclListOfPath(*path, *aclList) {
 	msiGetObjType(*path, *objType);
 	if (*objType == "-c") {
@@ -50,25 +75,37 @@ uuAclListOfPath(*path, *aclList) {
 	}
 }
 
+# \brief uuAclListOfColl generate a list of uuacl's of a collection
+# \param[in] collName  name (path) of collection
+# \param[out] aclList  list of uuacl's
 uuAclListOfColl(*collName, *aclList) {
 	*aclList = list();
+	# The COLL_ACCESS_USER_ID's are sorted high to low but consing will reverse that order
 	foreach(*row in SELECT ORDER_DESC(COLL_ACCESS_USER_ID), COLL_ACCESS_TYPE WHERE COLL_NAME = *collName) {
 		*acl = uuacl(int(*row.COLL_ACCESS_USER_ID), int(*row.COLL_ACCESS_TYPE));
 		*aclList = cons(*acl, *aclList);
 	}
 }
 
+# \brief uuAclListOfDataObj generate a list of uuacl's of a data object
+# \param[in] path      data object
+# \param[out] aclList  list of uuacl's
 uuAclListOfDataObj(*path, *aclList) {
-	*aclList = list();
 	uuChopPath(*path, *collName, *dataName);
+	*aclList = list();
+	# The DATA_ACCESS_USER_ID's are sorted high to low but consing will reverse that order
 	foreach(*row in SELECT ORDER_DESC(DATA_ACCESS_USER_ID), DATA_ACCESS_TYPE WHERE COLL_NAME = *collName AND DATA_NAME = *dataName) {
 		*acl = uuacl(int(*row.DATA_ACCESS_USER_ID), int(*row.DATA_ACCESS_TYPE));
 		*aclList = cons(*acl, *aclList);
 	}
 }
 
-
-uuAclListSetDiff(*aclListA, *aclListB, *setDiff) {
+# \brief uuAclListSetDiff  generate a list consisting of the set difference (see set theory) between two lists of uuacl's.
+# \param[in] aclListA   set A
+# \param[in] aclListB   set B
+# \param[in] idOnly     if true, only check for userId equivalence and ignore accessType. This is useful when determining ACL's to remove.
+# \returnvalue set difference of A \ B
+uuAclListSetDiff(*aclListA, *aclListB, *idOnly) {
 	*setDiff = list();
 	foreach(*aclA in *aclListA) {
 		uuacl(*userIdA, *accessTypeA) = *aclA;
@@ -76,17 +113,20 @@ uuAclListSetDiff(*aclListA, *aclListB, *setDiff) {
 		foreach(*aclB in *aclListB) {	
 			uuacl(*userIdB, *accessTypeB) = *aclB;
 			if (*userIdA < *userIdB) {
+				# This algorithm takes advantage of the fact that the uuacl lists are sorted low to high.
 				break;
 			}
-			if (*userIdA == *userIdB && *accessTypeA == *accessTypeB) {
-				*foundMatch = true;
-				break;
+			# match on userId and if idOnly is false, match on accessType too
+			if (*userIdA == *userIdB && (*idOnly || *accessTypeA == *accessTypeB)) {
+					*foundMatch = true;
+					break;	
 			}
 		}
 		if (!*foundMatch) {
 			*setDiff = cons(*aclA, *setDiff);
 		}
 	}
+	*setDiff;
 }
 
 # \brief uuEnforceGroupAcl   Enforce the ACL's of the group collection on a child path
@@ -108,29 +148,25 @@ uuEnforceGroupAcl(*path) {
 			uuAclListOfDataObj(*path, *aclList);
 		}
 
-		#DEBUG writeLine("serverLog", "aclList: *aclList");
+		#DEBUG writeLine("serverLog", "uuEnforceGroupAcl: aclList -> *aclList");
 		uuAclListOfColl("/*rodsZone/home/*groupName", *groupAclList);
-		#DEBUG writeLine("serverLog", "groupAclList: *groupAclList");
-		uuAclListSetDiff(*aclList, *groupAclList, *aclsToRemove);
-		#DEBUG writeLine("serverLog", "aclsToRemove: *aclsToRemove");
-		uuAclListSetDiff(*groupAclList, *aclList, *aclsToAdd);
-		#DEBUG writeLine("serverLog", "aclsToAdd: *aclsToAdd");
-		*idsToAdd = uuids(*aclsToAdd);
+		#DEBUG writeLine("serverLog", "uuEnforceGroupAcl: groupAclList -> *groupAclList");
+		*aclsToRemove = uuAclListSetDiff(*aclList, *groupAclList, true);
+		#DEBUG writeLine("serverLog", "uuEnforceGroupAcl: aclsToRemove -> *aclsToRemove");
+		*aclsToAdd = uuAclListSetDiff(*groupAclList, *aclList, false);
+		#DEBUG writeLine("serverLog", "uuEnforceGroupAcl: aclsToAdd -> *aclsToAdd");
 
 		*recurse = if *objType == "-c" then "recursive" else "default"
 		 
 		foreach(*acl in *aclsToAdd) {
 			uuAclToStrings(*acl, *userName, *accessLevel);
-			#DEBUG writeLine("serverLog", "acPostProcForObjRename: Setting ACL *accessLevel *userName *path");
+			#DEBUG writeLine("serverLog", "uuEnforceGroupAcl: Setting ACL *accessLevel *userName *path");
 			msiSetACL(*recurse, *accessLevel, *userName, *path);
 		}
 		
 		foreach(*acl in *aclsToRemove) {
-			uuacl(*userId, *accessType) = *acl;
-			if (!uuinlist(*userId, *idsToAdd)) {
 				uuAclToStrings(*acl, *userName, *accessLevel);
-				#DEBUG writeLine("serverLog", "acPostProcForObjRename: Removing ACL *accessLevel *userName *path");
+				#DEBUG writeLine("serverLog", "uuEnforceGroupAcl: Removing ACL *accessLevel *userName *path");
 				msiSetACL(*recurse, "null", *userName, *path);	
-			}
 		}
 }
