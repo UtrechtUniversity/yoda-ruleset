96c51bf98226f44fcd278042e095edad547f2c6f
Felix A. Croes <felix.croes@profit4cloud.nl>
2018-06-08 09:31:20 +0200

    YDA-1853 Don't map userId to userName until the last moment.
    And only if it's at all possible, the userId might no longer have
    an associated name.

--- tools/check-research-acl.r
+++ tools/check-research-acl.r
@@ -42,16 +42,13 @@ getCollAccess(*coll) {
     foreach (*row in SELECT COLL_ACCESS_NAME, COLL_ACCESS_USER_ID WHERE COLL_NAME = *coll) {
 	*name = *row.COLL_ACCESS_NAME;
 	*userId = *row.COLL_ACCESS_USER_ID;
-	foreach (*user in SELECT USER_NAME WHERE USER_ID = *userId) {
-	    *userName = *user.USER_NAME;
-	}
 	if (*name == "own") {
-	    *access.own = *userName;
-	    *access."*userName" = "own";
+	    *access.own = *userId;
+	    *access."*userId" = "own";
 	} else if (*name == "read object") {
-	    *access."*userName" = "read";
+	    *access."*userId" = "read";
 	} else if (*name == "modify object") {
-	    *access."*userName" = "write";
+	    *access."*userId" = "write";
 	}
     }
     *access;
@@ -63,16 +60,13 @@ getDataAccess(*coll, *data) {
     foreach (*row in SELECT DATA_ACCESS_NAME, DATA_ACCESS_USER_ID WHERE COLL_NAME = *coll AND DATA_NAME = *data) {
 	*name = *row.DATA_ACCESS_NAME;
 	*userId = *row.DATA_ACCESS_USER_ID;
-	foreach (*user in SELECT USER_NAME WHERE USER_ID = *userId) {
-	    *userName = *user.USER_NAME;
-	}
 	if (*name == "own") {
-	    *access.own = *userName;
-	    *access."*userName" = "own";
+	    *access.own = *userId;
+	    *access."*userId" = "own";
 	} else if (*name == "read object") {
-	    *access."*userName" = "read";
+	    *access."*userId" = "read";
 	} else if (*name == "modify object") {
-	    *access."*userName" = "write";
+	    *access."*userId" = "write";
 	}
     }
     *access;
@@ -82,22 +76,36 @@ getDataAccess(*coll, *data) {
 updateAccess(*path, *oldAccess, *newAccess, *update) {
     if (*oldAccess.own != *newAccess.own) {
 	*own = *oldAccess.own;
+	foreach (*user in SELECT USER_NAME WHERE USER_ID = *own) {
+	    *own = *user.USER_NAME;
+	    break;
+	}
 	*newOwn = *newAccess.own;
+	foreach (*user in SELECT USER_NAME WHERE USER_ID = *newOwn) {
+	    *newOwn = *user.USER_NAME;
+	    break;
+	}
 	writeLine("stdout", "*path: owner *own should be *newOwn");
 
 	if (*update) {
 	    # add new owner
-	    msiSetACL("default", "own", *newAccess.own, *path);
+	    msiSetACL("default", "own", *newOwn, *path);
 	    # remove old access 
 	    foreach (*key in *oldAccess) {
 		if (*key != "own") {
-		    msiSetACL("default", "null", *key, *path);
+		    foreach (*user in SELECT USER_NAME WHERE USER_ID = *key) {
+			msiSetACL("default", "null", *user.USER_NAME, *path);
+			break;
+		    }
 		}
 	    }
 	    # add new access
 	    foreach (*key in *newAccess) {
 		if (*key != "own") {
-		    msiSetACL("default", *newAccess."*key", *key, *path);
+		    foreach (*user in SELECT USER_NAME WHERE USER_ID = *key) {
+			msiSetACL("default", *newAccess."*key", *user.USER_NAME, *path);
+			break;
+		    }
 		}
 	    }
 	    writeLine("stdout", "    ...fixed");
