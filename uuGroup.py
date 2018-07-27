729b3407cdb9e62f16e5e99f88a6a64dd329ea0d
Felix A. Croes <felix.croes@profit4cloud.nl>
2018-07-27 11:06:30 +0200

    Add comments to elucidate the python group code.

--- uuGroup.py
+++ uuGroup.py
@@ -1,9 +1,14 @@
+# \brief Write group data for all users to stdout.
+#
 def uuGetGroupData(rule_args, callback, rei):
     import irods_types
     import json
 
     groups = {}
 
+    #
+    # first query: obtain a list of groups with group attributes
+    #
     ret_val = callback.msiMakeGenQuery("USER_GROUP_NAME, META_USER_ATTR_NAME, META_USER_ATTR_VALUE", "USER_TYPE = 'rodsgroup'", irods_types.GenQueryInp())
     query = ret_val["arguments"][2]
 
@@ -15,6 +20,7 @@ def uuGetGroupData(rule_args, callback, rei):
 	    attr = result.sqlResult[1].row(row)
 	    value = result.sqlResult[2].row(row)
 
+	    # create/update group with this information
 	    try:
 		group = groups[name]
 	    except:
@@ -30,10 +36,14 @@ def uuGetGroupData(rule_args, callback, rei):
 	    if attr == "manager":
 		group["managers"].append(value)
 
+	# continue with this query
 	if result.continueInx == 0:
 	    break
 	ret_val = callback.msiGetMoreRows(query, result, 0)
 
+    #
+    # second query: obtain list of groups with memberships
+    #
     ret_val = callback.msiMakeGenQuery("USER_GROUP_NAME, USER_NAME, USER_ZONE", "USER_TYPE != 'rodsgroup'", irods_types.GenQueryInp())
     query = ret_val["arguments"][2]
 
@@ -48,22 +58,28 @@ def uuGetGroupData(rule_args, callback, rei):
 	    if name != user and name != "rodsadmin" and name != "public":
 		user = user + "#" + zone
 		if name.startswith("read-"):
+		    # match read-* group with research-* or initial-* group.
 		    name = name[5:]
 		    try:
+			# attempt to add to read list of research group
 			group = groups["research-" + name]
 			group["read"].append(user)
 		    except:
 			try:
+			    # attempt to add to read list of initial group
 			    group = groups["initial-" + name]
 			    group["read"].append(user)
 			except:
 			    pass
 		elif not name.startswith("vault-"):
+		    # ordinary group
 		    group = groups[name]
 		    group["members"].append(user)
 
+	# continue wirth query
 	if result.continueInx == 0:
 	    break
 	ret_val = callback.msiGetMoreRows(query, result, 0)
 
+    # convert to json string and write to stdout
     callback.writeString("stdout", json.dumps(groups.values()))
