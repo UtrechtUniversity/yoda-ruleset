d9be2ff5a8b7f5dcddec53978d740347dc2e352a
Felix A. Croes <felix@dworkin.nl>
2018-10-24 13:35:07 +0200

    Fix typo.

--- iiMetadata.r
+++ iiMetadata.r
@@ -695,7 +695,7 @@ iiGetLatestVaultMetadataXml(*vaultPackage, *metadataXmlPath, *metadataXmlSize) {
 	*dataName = "";
 	*metadataXmlPath = "";
 	foreach (*row in SELECT DATA_NAME, DATA_SIZE WHERE COLL_NAME = *vaultPackage AND DATA_NAME like *dataNameQuery) {
-		if (*dataName == "" || (*dataName < *rows.DATA_NAME && strlen(*dataName) < strlen(*rows.DATA_NAME))) {
+		if (*dataName == "" || (*dataName < *row.DATA_NAME && strlen(*dataName) < strlen(*row.DATA_NAME))) {
 			*dataName = *rows.DATA_NAME;
 			*metadataXmlPath = *vaultPackage ++ "/" ++ *dataName;
 			*metadataXmlSize = int(*row.DATA_SIZE);
