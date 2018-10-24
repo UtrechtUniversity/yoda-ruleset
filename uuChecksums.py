2bebba768422ef5fc0fb0bf231604fb4af0af8fc
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-10-24 16:22:43 +0200

    Add Python file for checksum code.

---
+++ uuChecksums.py
@@ -0,0 +1,5 @@
+# \file      uuChecksums.py
+# \brief     Functions for data integrity.
+# \author    Lazlo Westerhof
+# \copyright Copyright (c) 2018 Utrecht University. All rights reserved.
+# \license   GPLv3, see LICENSE.
