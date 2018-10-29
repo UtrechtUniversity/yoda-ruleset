18a285775eac31e9b3beda0124a13be0da1d5410
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-10-29 12:10:01 +0100

    YDA-2408: Adjust script to allow restore to new location.

--- tools/restore-collection.r
+++ tools/restore-collection.r
@@ -1,58 +1,76 @@
 #!/bin/env irule -F
 restoreCollection {
-	
-	if (*path == "") {
-		writeLine("stdout", "\*path parameter is required");
-		succeed;
-	}
 
-	if (*timestamp == 0) {
-		writeLine("stdout", "\*timestamp parameter is required");
-		succeed;
-	}
+        if (*path == "") {
+                writeLine("stdout", "\*path parameter is required (string)");
+                succeed;
+        }
 
-	uuGetUserType(uuClientFullName, *userType);
-	if (*userType == 'rodsadmin') {
-		writeLine("stdout", "Ensure rodsadmin has permission to write to *path");
-		msiSetACL("recursive", "admin:own", uuClientFullName, "/" ++ $rodsZoneClient ++ UUREVISIONCOLLECTION);
-		msiSetACL("recursive", "inherit", uuClientFullName, "/" ++ $rodsZoneClient ++ UUREVISIONCOLLECTION);
-		if (uuCollectionExists(*path)) {
-			msiSetACL("recursive", "admin:write", uuClientFullName, *path);
-		} else {
-			uuChopPath(*path, *parent, *_);
-			while(*parent like regex "/[^/]/home/research-.*") {
-				if (uuCollectionExists(*parent)) {
-					msiSetACL("recursive", "admin:write", *parent);
-					break;
-				}
-				uuChopPath(*parent, *grandparent, *_);
-				*parent = *grandparent;
-			}
-		}
-			
-	}
+        if (*timestamp == 0) {
+                writeLine("stdout", "\*timestamp parameter is required (timestamp integer)");
+                succeed;
+        }
 
+        # Make sure rodsadmin can read revisions and write to restore path.
+        uuGetUserType(uuClientFullName, *userType);
+        if (*userType == 'rodsadmin') {
+                # Grant access to revisions.
+                msiSetACL("recursive", "admin:own", uuClientFullName, "/" ++ $rodsZoneClient ++ UUREVISIONCOLLECTION);
+                msiSetACL("recursive", "inherit", uuClientFullName, "/" ++ $rodsZoneClient ++ UUREVISIONCOLLECTION);
 
-	
-	iiRevisionListOfCollectionBeforeTimestamp(*path, *timestamp, *revisions); 
-	foreach(*revision in *revisions) {
-		iirevisionwithpath(*revisionId, *originalPath) = *revision;
-		if (*revisionId != "") {
-			foreach(*row in SELECT COLL_NAME, DATA_NAME WHERE DATA_ID = *revisionId) {
-				*revPath = *row.COLL_NAME ++ "/" ++ *row.DATA_NAME;
-			}
-			uuChopPath(*originalPath, *coll, *filename);
-			if (!uuCollectionExists(*coll)) {
-				msiCollCreate(*coll, 1, *status);
-			}
-			writeLine("stdout", "Restoring *revPath to *originalPath");
-			msiAddKeyValToMspStr("forceFlag", "", *options);
-			msiDataObjCopy(*revPath, *originalPath, *options, *msistatus);
-		}
-			
-	}
+		# Grant write access to restore path.
+                if (*restorePath != "") {
+                       *writePath = *restorePath;
+                } else {
+                       *writePath = *path;
+                }
 
+                if (uuCollectionExists(*writePath)) {
+                        msiSetACL("recursive", "admin:write", uuClientFullName, *writePath);
+                } else {
+                        uuChopPath(*writePath, *parent, *_);
+                        while(*parent like regex "/[^/]/home/research-.*") {
+                                if (uuCollectionExists(*parent)) {
+                                        msiSetACL("recursive", "admin:write", *parent);
+                                        break;
+                                }
+                                uuChopPath(*parent, *grandparent, *_);
+                                *parent = *grandparent;
+                        }
+                }
+        }
+
+	# Retrieve all revisions before timestamp.
+        iiRevisionListOfCollectionBeforeTimestamp(*path, *timestamp, *revisions);
+        *iso8601 = uuiso8601(*timestamp);
+        writeLine("stdout", "Restoring revisions of collection *path from before *iso8601.");
+        writeLine("stdout", "Restoring to *writePath.");
+        writeLine("stdout", "##############################");
+
+        # Restore revisions in path.
+        foreach(*revision in *revisions) {
+                iirevisionwithpath(*revisionId, *originalPath) = *revision;
+
+                if (*revisionId != "") {
+                        foreach(*row in SELECT COLL_NAME, DATA_NAME WHERE DATA_ID = *revisionId) {
+                                *revPath = *row.COLL_NAME ++ "/" ++ *row.DATA_NAME;
+                        }
+                        uuChopPath(*originalPath, *coll, *filename);
+                        if (*restorePath != "") {
+                                *coll = *restorePath;
+                                *originalPath = "*coll/*filename";
+                        }
+
+                        # Create collection if it does not exists.
+                        if (!uuCollectionExists(*coll)) {
+                                msiCollCreate(*coll, 1, *status);
+                        }
+                        writeLine("stdout", "Restoring *revPath to *originalPath");
+                        msiAddKeyValToMspStr("forceFlag", "", *options);
+                        msiDataObjCopy(*revPath, *originalPath, *options, *msistatus);
+                }
+        }
 }
 
-input *path="", *timestamp=0
+input *path="", *timestamp=0, *restorePath=""
 output ruleExecOut
