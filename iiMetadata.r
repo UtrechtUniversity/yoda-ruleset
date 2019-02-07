e9225d64a2b97ce1a81343ac0ee22698ef6113e1
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-02-07 11:49:48 +0100

    Remove unused code.

--- iiMetadata.r
+++ iiMetadata.r
@@ -17,38 +17,15 @@
 iiFrontTransformXml(*path, *versionFrom, *versionTo, *status, *statusInfo)
 {
         *status = "Success";
-	*statusInfo = "";
-        *statusPy = "";
-        *statusInfoPy = "";
-        iiRuleTransformXml(*path, *versionFrom, *versionTo, *statusPy, *statusInfoPy);
-
-	*status = *statusPy;
-        *statusInfo = "*status -> Schema ID error: No conversion known for your yoda-metadatai.xml from version id '*versionFrom' into '*versionTo'";
-}
-
-# \brief Retrieve textual changes from versionFrom into versionTo. If at all possible
-#
-# \param[in]  path        Path of the yoda-metadata.xml
-# \param[in]  versionFrom Version within yoda-metadata.xml
-# \param[in]  versionTo   Version yoda-metadata.xml must be converted into
-# \param[in]  transformationChanges holds textual explanation of changes that took place for informing enduser
-# \param[out] status      Status of the action
-# \param[out] statusInfo  Information message when action was not successful
-#
-iiFrontTransformationChanges(*path, *versionFrom, *versionTo, *transformationChanges, *status, *statusInfo)
-{
-        *status = "Success";
         *statusInfo = "";
-        *transformationChanges = "Changes from within the system";
         *statusPy = "";
         *statusInfoPy = "";
-        *transformationChangesPy = "";
-        iiRuleTransformationChanges(*path, *versionFrom, *versionTo, *transformationChangesPy, *statusPy, *statusInfoPy);
+        iiRuleTransformXml(*path, *statusPy, *statusInfoPy);
 
-        *transformationChanges = *transformationChangesPy;
         *status = *statusPy;
         *statusInfo = *statusInfoPy;
 }
+
 
 # \brief Get the XSD location to be included the metadata form.
 #
