7d1026e99dc341d30ea5e3696b0e12099657fcec
Harm de Raaff <harm.apple@gmail.com>
2019-04-08 09:44:04 +0200

    Changed mail function name for archiving

--- uuMail.r
+++ uuMail.r
@@ -207,7 +207,7 @@ uuYourPackagePublishedMail(*researcher, *actor, *title, *yodaDOI, *status, *mess
 # \param[out] status      zero on success, non-zero on failure
 # \param[out] message     a user friendly error message
 #
-uuNewPackagePublishedMail(*datamanager, *actor, *title, *yodaDOI, *status, *message) {
+uuNewArchivedPackageMail(*datamanager, *actor, *title, *yodaDOI, *status, *message) {
         *status  = 1;
         *message = "An internal error occured.";
 
