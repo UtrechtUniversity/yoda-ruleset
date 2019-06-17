7d1b06fb02e41f934f7f083cdf8bafc0bbfdc01c
aucuparia <aucuparia@users.noreply.github.com>
2019-06-17 18:01:30 +0200

    Update getDatarequests

--- uuDatarequests.r
+++ uuDatarequests.r
@@ -5,40 +5,59 @@
 # \license   GPLv3, see LICENSE.
 
 
-# \brief Retrieve descriptive information of a number of data requests.
-#        This is used to render a paginated table of data requests.
+# \brief Retrieve descriptive information of a number of research proposals.
+#        This is used to render a paginated table of research proposals.
 #
-# \param[in] proposalId  Unique identifier of the research proposal whose data
-#                        requests are to queried.
-# \param[in] limit       The number of data requests to return.
-# \param[in] offset      Offset used for table pagination.
+# \param[in] limit  The number of proposals to return.
+# \param[in] offset Offset used for table pagination.
 #
-# \return List of unpreservable files.
-uuGetDatarequests(*proposal, *limit, *offset, *result, *status, *statusInfo) {
-	*status = "Success";
-	*statusInfo = "";
+# \return List of descriptive information about a number of research proposals.
+#
+uuGetDatarequests(*limit, *offset, *result, *status, *statusInfo) {
+        *status = "Success";
+        *statusInfo = "";
 
-	# Query iRODS to get a list of submitted proposals (i.e. subcollections
-	# of the the research-datarequest collection)
-	#*path = "/tempZone/home/datarequests-research";
-       
-	*path = "/tempZone/home/datarequests-research/" ++ *proposal ++
-                "/datarequests";
-	*fields = list("DATA_NAME", "DATA_CREATE_TIME", "DATA_OWNER_NAME",
-                       "META_DATA_ATTR_VALUE");
-	*conditions = list(uucondition("COLL_NAME", "=", *path),
-                           uucondition("DATA_NAME", "like", "%.json"),
-                           # This condition is needed to filter out duplicate
-                           # entries caused by replications of a data object
-                           # (which are automatically created by Yoda)
-                           uucondition("DATA_RESC_HIER", "like",
-                                       elem(UUPRIMARYRESOURCES, 0) ++ ";%"));
-	*orderby = "COLL_NAME";
-	*ascdesc = "asc";
+        # Query iRODS to get a list of submitted proposals (i.e. subcollections
+        # of the the research-datarequest collection)
+        *path = "/tempZone/home/datarequests-research";
+        *fields = list("COLL_NAME", "COLL_CREATE_TIME", "COLL_OWNER_NAME", "META_DATA_ATTR_VALUE");
+        *conditions = list(uucondition("COLL_PARENT_NAME", "=", *path),
+                           uucondition("DATA_NAME", "=", "datarequest.json"),
+                           uucondition("META_DATA_ATTR_NAME", "=", "status"));
+        *orderby = "COLL_NAME";
+        *ascdesc = "asc";
 
-	uuPaginatedQuery(*fields, *conditions, *orderby, *ascdesc, *limit,
-                         *offset, *kvpList, *status, *statusInfo);
-	uuKvpList2JSON(*kvpList, *result, *size);
+        uuPaginatedQuery(*fields, *conditions, *orderby, *ascdesc, *limit, *offset, *kvpList, *status, *statusInfo);
+        uuKvpList2JSON(*kvpList, *result, *size);
+}
+
+
+# \brief Same as uuGetProposals, but with a different META_DATA_ATTR_NAME as
+#        query condition and fewer return fields. This is a necessary hack,
+#        because the iRODS query language does not have an OR operator
+#
+# \param[in] limit         The number of proposals to return.
+# \param[in] offset        Offset used for table pagination.
+# \param[in] attributeName The attribute whose value should be returned
+#
+# \return List of descriptive information about a number of research proposals.
+#
+uuGetDatarequestsAdditionalFields(*limit, *offset, *attributeName, *result,
+                                  *status, *statusInfo) {
+        *status = "Success";
+        *statusInfo = "";
+
+        *path = "/tempZone/home/datarequests-research";
+        *fields = list("COLL_NAME", "META_DATA_ATTR_VALUE");
+        *conditions = list(uucondition("COLL_PARENT_NAME", "=", *path),
+                           uucondition("DATA_NAME", "=", "datarequest.json"),
+                           uucondition("META_DATA_ATTR_NAME", "=", *attributeName));
+        *orderby = "COLL_NAME";
+        *ascdesc = "asc";
+
+        uuPaginatedQuery(*fields, *conditions, *orderby, *ascdesc, *limit, *offset, *kvpList, *status, *statusInfo);
+        uuKvpList2JSON(*kvpList, *result, *size);
+}
 
 
 # \brief Request data request metadata change
