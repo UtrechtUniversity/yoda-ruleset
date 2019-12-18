2958fc2ee7b16ee0e3ffd4fcf1f2299716b7a93e
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-12-18 14:04:41 +0100

    Don't show grp-vault collections in the research space.

--- browse.py
+++ browse.py
@@ -66,10 +66,12 @@ def api_uu_browse_folder(ctx,
 
     # We make offset/limit act on two queries at once, placing qdata right after qcoll.
     if space == str(pathutil.Space.RESEARCH):
-        qcoll = Query(ctx, ccols, "COLL_PARENT_NAME = '{}' AND COLL_NAME not like '/{}/home/%vault-%'".format(coll, zone),
+        qcoll = Query(ctx, ccols,
+                      "COLL_PARENT_NAME = '{}' AND COLL_NAME not like '/{}/home/vault-%' AND COLL_NAME not like '/{}/home/grp-vault-%'".format(coll, zone, zone),
                       offset=offset, limit=limit, output=query.AS_DICT)
     elif space == str(pathutil.Space.VAULT):
-        qcoll = Query(ctx, ccols, "COLL_PARENT_NAME = '{}' AND COLL_NAME like '/{}/home/%vault-%'".format(coll, zone),
+        qcoll = Query(ctx, ccols,
+                      "COLL_PARENT_NAME = '{}' AND COLL_NAME like '/{}/home/%vault-%'".format(coll, zone),
                       offset=offset, limit=limit, output=query.AS_DICT)
     else:
         qcoll = Query(ctx, ccols, "COLL_PARENT_NAME = '{}'".format(coll),
