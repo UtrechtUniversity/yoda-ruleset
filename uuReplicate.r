17e926b1e8547ba1d35b05e9277db2c7bad9a96a
Chris Smeele <cj.smeele@xs4all.nl>
2020-04-28 15:20:47 +0200

    Fix: Asynchronous replication via batch job (work around PREP bugs)

--- uuReplicate.r
+++ uuReplicate.r
@@ -1,7 +1,8 @@
 # \file      uuReplicate.r
 # \brief     Replication functions.
 # \author    Ton Smeele
-# \copyright Copyright (c) 2015-2018, Utrecht University. All rights reserved.
+# \author    Chris Smeele
+# \copyright Copyright (c) 2015-2020, Utrecht University. All rights reserved.
 # \license   GPLv3, see LICENSE.
 
 # \brief Schedule replication of a data object.
@@ -21,6 +22,60 @@ uuReplicateAsynchronously(*object, *sourceResource, *targetResource) {
     # Mark data object for batch replication by setting 'org_replication_scheduled' metadata.
     msiString2KeyValPair("", *kv);
     msiAddKeyVal(*kv, UUORGMETADATAPREFIX ++ "replication_scheduled", "*sourceResource,*targetResource");
-    msiAssociateKeyValuePairsToObj(*kv, *object, "-d");
+    msiSetKeyValuePairsToObj(*kv, *object, "-d");
     #writeLine("serverLog", "uuReplicateAsynchronously: Replication scheduled for *object");
+}
+
+# Scheduled replication batch job.
+#
+# Performs replication for all data objects marked with 'org_replication_scheduled' metadata.
+# The metadata value indicates the source and destination resource.
+#
+# XXX: This function cannot be ported to Python in 4.2.7:
+#      msiDataObjRepl causes a deadlock for files larger than
+#      transfer_buffer_size_for_parallel_transfer_in_megabytes (4) due to an iRODS PREP bug.
+#
+uuReplicateBatch() {
+    writeLine("serverLog", "Batch replication job started");
+    *count   = 0;
+    *countOk = 0;
+
+    *attr = UUORGMETADATAPREFIX ++ "replication_scheduled";
+    foreach (*row in SELECT COLL_NAME, DATA_NAME, META_DATA_ATTR_VALUE
+                     WHERE  META_DATA_ATTR_NAME = '*attr') {
+        *count = *count + 1;
+
+        # Perform scheduled replication for one data object.
+
+        *path  = *row."COLL_NAME" ++ "/" ++ *row."DATA_NAME";
+        *rescs = *row."META_DATA_ATTR_VALUE";
+        *xs    = split(*rescs, ",");
+        if (size(*xs) == 2) {
+            *from = elem(*xs, 0);
+            *to   = elem(*xs, 1);
+            *opts = "rescName=*from++++destRescName=*to++++irodsAdmin=";
+            *status = errorcode(msiDataObjRepl(*path, *opts, *s));
+            if (*status == 0) {
+                *countOk = *countOk + 1;
+
+                # Remove replication_scheduled flag.
+
+                # Even the sudo msi respects ACLs.
+                # Need to set write or own on the object if we don't have it already.
+                errorcode(msiSudoObjAclSet("", "own", uuClientFullName, *path, ""));
+                *status = errorcode(msiSudoObjMetaRemove(*path, "-d", "",
+                                                         *attr,
+                                                         "*from,*to", "", ""));
+                if (*status != 0) {
+                    writeLine("serverLog", "Error: Scheduled replication of <*path>: could not remove schedule flag (*status)");
+                }
+            } else {
+                writeLine("serverLog", "Error: Scheduled replication of <*path> failed (*status)");
+            }
+        } else {
+            writeLine("serverLog", "Error: Scheduled replication of <*path> skipped: bad meta value <*rescs>");
+        }
+    }
+
+    writeLine("serverLog", "Batch replication job finished. *countOk/*count objects succesfully replicated.");
 }
