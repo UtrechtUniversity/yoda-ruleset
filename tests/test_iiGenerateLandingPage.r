6e55c16fdc902eabae70eb727fd9ea48aac3a9bd
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-05-25 11:52:10 +0200

    Remove deprecated code.

--- tests/test_iiGenerateLandingPage.r
+++
@@ -1,9 +0,0 @@
-testRule {
-	#msiXsltApply("/tempZone/yoda/xsl/default2landingpage.xsl", *combiXmlPath,*buf);
-	#writeBytesBuf("stdout", *buf);
-	iiGenerateLandingPage(*combiXmlPath, *landingPagePath);
-	writeLine("stdout", *landingPagePath);	
-
-}
-input *combiXmlPath="" 
-output ruleExecOut
