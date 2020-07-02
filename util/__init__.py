1c4947d2bcc0bf1ce9d42a5c71e66f9c99ca05e9
Chris Smeele <cj.smeele@xs4all.nl>
2020-07-02 11:26:05 +0200

    Fix .util imports (re: generate-openapi.py)

--- util/__init__.py
+++ util/__init__.py
@@ -5,30 +5,31 @@ This subpackage does not export any callable rules by itself.
 Rather, it provides utility Python functions to other rules.
 """
 
-__copyright__ = 'Copyright (c) 2019, Utrecht University'
+__copyright__ = 'Copyright (c) 2019-2020, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
 # Make sure that importing * from this package gives (qualified) access to all
 # contained modules.
 # i.e. importing code can use these modules like: collection.exists(callback, 'bla')
-__all__ = ['log',
-           'error',
-           'jsonutil',
-           'rule',
-           'api',
-           'msi',
-           'policy',
-           'pathutil',
-           'constants',
-           'collection',
-           'data_object',
-           'user',
-           'group',
-           'avu',
-           'misc',
-           'query',
-           'genquery',  # temporary
-           'config']
+
+import log
+import error
+import jsonutil
+import rule
+import api
+import msi
+import policy
+import pathutil
+import constants
+import collection
+import data_object
+import user
+import group
+import avu
+import misc
+import query
+import genquery  # temporary
+import config
 
 # Config items can be accessed directly as 'config.foo' by any module
 # that imports * from util.
@@ -36,6 +37,4 @@ from config import config
 
 if config.environment == 'development':
     import irods_type_info
-    import api
     ping = api_uu_ping = api.make()(lambda ctx, x=42: x)
-    __all__ += ['api_uu_ping', 'ping']
