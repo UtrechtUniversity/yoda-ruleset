8d1ee1bda6a977ec00f847545ba0af00cca1d357
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-07-06 11:22:19 +0200

    Fix URL for minting the DOI.

--- datacite.py
+++ datacite.py
@@ -24,7 +24,7 @@ def rule_uu_generate_random_id(ctx, length):
     return ''.join(random.choice(characters) for x in range(int(length)))
 
 
-@rule.make(inputs=[0], outputs=[1])
+@rule.make(inputs=[0, 1], outputs=[2])
 def rule_uu_register_doi_metadata(ctx, doi, payload):
     """Register DOI metadata with DataCite."""
     url = "{}/metadata/{}".format(config.datacite_url, doi)
@@ -39,7 +39,7 @@ def rule_uu_register_doi_metadata(ctx, doi, payload):
 @rule.make(inputs=[0, 1], outputs=[2])
 def rule_uu_register_doi_url(ctx, doi, url):
     """Register DOI url with DataCite."""
-    url = "{}/doi".format(config.datacite_url)
+    url = "{}/doi/{}".format(config.datacite_url, doi)
     auth = (config.datacite_username, config.datacite_password)
     payload = "doi={}\nurl={}".format(doi, url)
     headers = {'content-type': 'text/plain', 'charset': 'UTF-8'}
