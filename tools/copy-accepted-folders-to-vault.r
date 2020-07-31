366ef49d9da16008cdc3999c6200753dfcbd5cb0
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-07-31 12:17:28 +0200

    Remove deprecated folder secure methods.

--- tools/copy-accepted-folders-to-vault.r
+++ tools/copy-accepted-folders-to-vault.r
@@ -3,7 +3,7 @@
 copyToVault {
 	# Copy research folder to vault.
 	# This script is kept as dumb as possible.
-	# All processing and error handling is done by iiFolderSecure
+	# All processing and error handling is done by rule_folder_secure
 	*ContInxOld = 1;
 	msiAddSelectFieldToGenQuery("COLL_NAME", "", *GenQInp);
 	msiAddConditionToGenQuery("META_COLL_ATTR_NAME", "=", UUORGMETADATAPREFIX ++ "cronjob_copy_to_vault", *GenQInp);
@@ -15,9 +15,9 @@ copyToVault {
 	while(*ContInxOld > 0) {
 		foreach(*row in *GenQOut) {
 			*folder = *row.COLL_NAME;
-			# When iiFolderSecure fails continue with the other folders.
-                        *errorcode = '0';
-                        rule_folder_secure(*folder, *errorcode);
+			# When rule_folder_secure fails continue with the other folders.
+            *errorcode = '0';
+            rule_folder_secure(*folder, *errorcode);
 			if (*errorcode == '0') {
 				*cronjobState = UUORGMETADATAPREFIX ++ "cronjob_copy_to_vault=" ++ CRONJOB_OK;
 				msiString2KeyValPair(*cronjobState, *cronjobStateKvp);
--- tools/retry-copy-to-vault.r
+++ tools/retry-copy-to-vault.r
@@ -1,7 +1,7 @@
 retryCopyToVault {
 	# Copy research folder to vault.
 	# This script is kept as dumb as possible.
-	# All processing and error handling is done by iiFolderSecure
+	# All processing and error handling is done by rule_folder_secure
 	*ContInxOld = 1;
 	msiAddSelectFieldToGenQuery("COLL_NAME", "", *GenQInp);
 	msiAddConditionToGenQuery("META_COLL_ATTR_NAME", "=", UUORGMETADATAPREFIX ++ "cronjob_copy_to_vault", *GenQInp);
@@ -13,9 +13,9 @@ retryCopyToVault {
 	while(*ContInxOld > 0) {
 		foreach(*row in *GenQOut) {
 			*folder = *row.COLL_NAME;
-			# When iiFolderSecure fails continue with the other folders.
-                        *errorcode = '0';
-                        rule_folder_secure(ctx, *folder, *errorcode);
+			# When rule_folder_secure fails continue with the other folders.
+            *errorcode = '0';
+            rule_folder_secure(ctx, *folder, *errorcode);
 			if (*errorcode == '0') {
 				*cronjobState = UUORGMETADATAPREFIX ++ "cronjob_copy_to_vault=" ++ CRONJOB_OK;
 				msiString2KeyValPair(*cronjobState, *cronjobStateKvp);
