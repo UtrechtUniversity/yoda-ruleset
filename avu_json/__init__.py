88e13ae2ccb26a87af0bb7186c2e9a05c56fdd4e
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-08-05 12:30:34 +0200

    Add missing init file.

---
+++ avu_json/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Bidirectional conversion between JSON(-LD) and iRODS AVUs."""
+
+__copyright__ = ['Copyright (c) 2019, Maastricht University',
+                 'Copyright (c) 2020, Utrecht University']
+__license__   = 'Apache License 2.0, see LICENSE'
+
+import avu_json
