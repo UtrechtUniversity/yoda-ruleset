55ed37698ab8305caddeab0ffcd373ca64850360
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-09-25 15:52:40 +0200

    Force irule to run on iRODS rule language engine.

--- tools/admin-datarequestactions.sh
+++ tools/admin-datarequestactions.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-irule -F /etc/irods/irods-ruleset-uu/tools/process-datarequest-actions.r
+irule -r irods_rule_engine_plugin-irods_rule_language-instance -F /etc/irods/irods-ruleset-uu/tools/process-datarequest-actions.r
