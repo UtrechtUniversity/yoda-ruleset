afa536f8e3b38372bef4b506c0cf2d21fef6fde1
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-10-07 16:32:42 +0200

    Add more vault API tests.

--- tests/features/api_vault.feature
+++ tests/features/api_vault.feature
@@ -28,9 +28,30 @@ Feature: Vault API
         Then the response status code is "200"
         And data package status is "APPROVED_FOR_PUBLICATION"
 
+    Scenario: Vault preservable formats lists
+        Given the Yoda vault preservable formats lists API is queried
+        Then the response status code is "200"
+        And preservable formats lists are returned
+
+    Scenario: Vault unpreservable files
+        Given data package exists in "<vault>"
+        And the Yoda vault unpreservable files API is queried with "<list>" on datapackage in "<vault>"
+        Then the response status code is "200"
+        And unpreservable files are returned
+
+        Examples:
+            | list |
+            | 4TU  |
+            | DANS |
+
     Scenario: Vault system metadata
         Given data package exists in "<vault>"
         And the Yoda vault system metadata API is queried on datapackage in "<vault>"
+        Then the response status code is "200"
+
+    Scenario: Vault collection details
+        Given data package exists in "<vault>"
+        And the Yoda vault collection details API is queried on datapackage in "<vault>"
         Then the response status code is "200"
 
     Scenario: Vault get publication terms
--- tests/step_defs/test_api_vault.py
+++ tests/step_defs/test_api_vault.py
@@ -50,11 +50,32 @@ def api_response(vault, data_package):
         {"coll": vault + "/" + data_package}
     )
 
+@given('the Yoda vault preservable formats lists API is queried', target_fixture="api_response")
+def api_response():
+    return api_request(
+        "vault_preservable_formats_lists",
+        {}
+    )
+
+@given('the Yoda vault unpreservable files API is queried with "<list>" on datapackage in "<vault>"', target_fixture="api_response")
+def api_response(list, vault, data_package):
+    return api_request(
+        "vault_unpreservable_files",
+        {"coll": vault + "/" + data_package, "list_name": list}
+    )
+
 @given('the Yoda vault system metadata API is queried on datapackage in "<vault>"', target_fixture="api_response")
 def api_response(vault, data_package):
     return api_request(
         "vault_system_metadata",
         {"coll": vault + "/" + data_package}
+    )
+
+@given('the Yoda vault collection details API is queried on datapackage in "<vault>"', target_fixture="api_response")
+def api_response(vault, data_package):
+    return api_request(
+        "vault_collection_details",
+        {"path": vault + "/" + data_package}
     )
 
 @given('the Yoda vault get publication terms API is queried', target_fixture="api_response")
@@ -77,6 +98,18 @@ def data_package_status(vault, data_package, status):
     )
 
     assert body["data"]["status"] == status
+
+@then(parsers.parse('preservable formats lists are returned'))
+def api_response_code(api_response):
+    http_status, body = api_response
+    assert http_status == 200
+    assert len(body["data"]) > 0
+
+@then(parsers.parse('unpreservable files are returned'))
+def api_response_code(api_response):
+    http_status, body = api_response
+    assert http_status == 200
+    assert len(body["data"]) >= 0
 
 @then(parsers.parse('publication terms are returned'))
 def api_response_code(api_response):
