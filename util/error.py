6ad2b6ec477af6c137e7c4870a70c15851aabd13
aucuparia <aucuparia@users.noreply.github.com>
2020-10-22 13:23:18 +0200

    Fix PEP-8 errors

--- util/error.py
+++ util/error.py
@@ -16,5 +16,6 @@ class UUFileSizeError(UUError):
 class UUFileNotExistError(UUError):
     """File does not exist."""
 
+
 class UUJsonValidationError(UUError):
     """JSON data could not be validated."""
