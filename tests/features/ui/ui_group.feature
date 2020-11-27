d978ebad9839eb0cecaea256aeeb38abd82762e3
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-11-27 12:14:08 +0100

    Add group scnearios for UI test.

---
+++ tests/features/ui/ui_group.feature
@@ -0,0 +1,47 @@
+Feature: Group UI
+
+    Scenario: Group user add
+        Given user "groupmanager" is logged in
+        And module "group-manager" is shown
+        When user has access to group "<group>" in category "<category>"
+        And user adds "<user_add>" to group
+        Then user "<user_add>" is added to the group
+
+        Examples:
+            | category | group            | user_add      |
+            | initial  | research-initial | uitestpromote |
+            | initial  | research-initial | uitestdemote  |
+
+    Scenario: Group user promote
+        Given user "groupmanager" is logged in
+        And module "group-manager" is shown
+        When user has access to group "<group>" in category "<category>"
+        And user promotes "<user_promote>" to group manager
+        #Then user "<user_add>" is added to the group
+
+        Examples:
+            | category | group            | user_promote  |
+            | initial  | research-initial | uitestpromote |
+
+    Scenario: Group user demote
+        Given user "groupmanager" is logged in
+        And module "group-manager" is shown
+        When user has access to group "<group>" in category "<category>"
+        And user demotes "<user_demote>" to viewer
+        #Then user "<user_add>" is added to the group
+
+        Examples:
+            | category | group            | user_demote  |
+            | initial  | research-initial | uitestdemote |
+
+    Scenario: Group user remove
+        Given user "groupmanager" is logged in
+        And module "group-manager" is shown
+        When user has access to group "<group>" in category "<category>"
+        And user removes "<user_remove>" from group
+        #Then user "<user_add>" is removed from the group
+
+        Examples:
+            | category | group            | user_remove   |
+            | initial  | research-initial | uitestpromote |
+            | initial  | research-initial | uitestdemote  |
---
+++ tests/step_defs/ui/test_ui_group.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+"""Group UI feature tests."""
+
+__copyright__ = 'Copyright (c) 2020, Utrecht University'
+__license__   = 'GPLv3, see LICENSE'
+
+from pytest_bdd import (
+    scenarios,
+    then,
+    when,
+)
+
+scenarios('../../features/ui/ui_group.feature')
+
+
+@when('user has access to group "<group>" in category "<category>"')
+def ui_group_access(browser, category, group):
+    if not browser.is_element_present_by_css('a.list-group-item.active[data-name={}]'.format(group), wait_time=1):
+        browser.find_by_css('div.list-group-item[data-name={}]'.format(category)).click()
+        browser.find_by_css('a.list-group-item[data-name={}]'.format(group)).click()
+
+
+@when('user adds "<user_add>" to group')
+def ui_group_user_add(browser, user_add):
+    browser.find_by_css('a.user-create-text').click()
+    browser.find_by_xpath('//*[@id="s2id_autogen5_search"]').fill(user_add)
+    browser.find_by_css('.select2-results .select2-highlighted').click()
+    browser.find_by_css('#f-user-create-submit').click()
+
+
+@when('user promotes "<user_promote>" to group manager')
+def ui_group_user_promote(browser, user_promote):
+    browser.find_by_id('user-list').links.find_by_partial_text(user_promote).click()
+    browser.find_by_css('a.promote-button').click()
+
+
+@when('user demotes "<user_demote>" to viewer')
+def ui_group_user_demote(browser, user_demote):
+    browser.find_by_id('user-list').links.find_by_partial_text(user_demote).click()
+    browser.find_by_css('a.demote-button').click()
+
+
+@when('user removes "<user_remove>" from group')
+def ui_group_user_remove(browser, user_remove):
+    browser.find_by_id('user-list').links.find_by_partial_text(user_remove).click()
+    browser.find_by_css('a.delete-button').click()
+    browser.find_by_css('button.confirm').click()
+
+
+@then('user "<user_add>" is added to the group')
+def ui_group_user_added(browser, user_add):
+    assert browser.find_by_css('.users .active').value == user_add
