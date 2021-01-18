a4a59d02d75111de672d389b70abae3207d16c9b
Harm de Raaff <harm.apple@gmail.com>
2021-01-18 16:56:46 +0100

    linted

--- policies.py
+++ policies.py
@@ -4,16 +4,15 @@
 __copyright__ = 'Copyright (c) 2020, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
-import re # Stdlib
+import re  # Stdlib
 
 import policies_intake  # 3rd party
 import session_vars  # 3rd party
 
 import datarequest
 import folder
-import policies_datapackage_status #App
+import policies_datapackage_status  # App
 import policies_folder_status  # App
-
 from util import *
 
 
