2041a726363e74f48c69b3d6f470dd9b0e99d9fe
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-12 12:32:47 +0200

    Get data package and link from target path for research and vault packages.

--- notifications.py
+++ notifications.py
@@ -50,6 +50,17 @@ def api_notifications_load(ctx):
         try:
             notification = jsonutil.parse(result)
             notification["datetime"] = (datetime.fromtimestamp(notification["timestamp"])).strftime('%Y-%m-%d %H:%M')
+            notification["actor"] = user.from_str(ctx, notification["actor"])[0]
+
+            # Get data package and link from target path for research and vault packages.
+            space, _, group, subpath = pathutil.info(notification["target"])
+            if space is pathutil.Space.RESEARCH:
+                notification["data_package"] = pathutil.basename(subpath)
+                notification["link"] = "/research?dir={}/{}".format(group, subpath)
+            elif space is pathutil.Space.VAULT:
+                notification["data_package"] = pathutil.basename(subpath)
+                notification["link"] = "/vault?dir={}/{}".format(group, subpath)
+
             notifications.append(notification)
         except Exception:
             continue
