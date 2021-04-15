bf23d2cfc9499219d57e8ca8852e968a0e6026b2
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-04-15 14:49:41 +0200

    Adjust group data test for new group hierarchy.

--- tests/step_defs/api/test_api_group.py
+++ tests/step_defs/api/test_api_group.py
@@ -176,14 +176,17 @@ def then_users_found_match(api_response, users):
 def group_exists(api_response, group):
     _, body = api_response
 
-    assert len(body['data']) > 0
+    assert len(body['data']['group_hierarchy']) > 0
+    hierarchy = body['data']['group_hierarchy']
 
-    # Check if expected result is in group results.
+    # Check if expected result is in group hierarchy.
     found = False
-    for group_data in body['data']:
-        if group_data['name'] == group:
-            found = True
-            break
+    for category in hierarchy:
+        for subcategory in hierarchy[category]:
+            for group_name in hierarchy[category][subcategory]:
+                if group_name == group:
+                    found = True
+                    break
 
     assert found
 
