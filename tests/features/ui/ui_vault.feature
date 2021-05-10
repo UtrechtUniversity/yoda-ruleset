8fbddc645a26958c85eb3c13206148ab89462122
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-05-10 13:05:18 +0200

    Add missing vault UI tests (#74)
    * Flake8 fixes.
    
    * Finish publication workflow and vault package information UI tests.
    
    * Fix remaining vault tests.
    
    Co-authored-by: Harm de Raaff <harm.apple@gmail.com>

--- tests/features/ui/ui_vault.feature
+++ tests/features/ui/ui_vault.feature
@@ -25,9 +25,101 @@ Feature: Vault UI
         And user submits the data package for publication
         Then the data package status is "Submitted for publication"
 
-    Scenario: Vault approve
+    Scenario: Vault publication approve
         Given user "datamanager" is logged in
         And module "vault" is shown
         When user browses to data package in "<vault>"
         And user approves the data package for publication
         Then the data package status is "Approved for publication"
+
+    Scenario: Vault publication published
+        Given user "researcher" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        Then the data package status is "Published"
+
+    Scenario: Vault depublish publication
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user requests depublication of data package
+        Then the data package status is "Depublication pending"
+
+    Scenario: Vault publication depublished
+        Given user "researcher" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        Then the data package status is "Depublished"
+
+    Scenario: Vault republish publication
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user requests republication of data package
+        Then the data package status is "Republication pending"
+
+    Scenario: Vault publication republished
+        Given user "researcher" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        Then the data package status is "Published"
+
+    Scenario: Vault view metadata form
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks metatadata button
+        Then metadata form is visible
+
+    Scenario: Vault view system metadata
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks system metadata icon
+        Then system metadata is visible
+
+    Scenario: Vault view provenance information
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks provenance icon
+        Then provenance information is visible
+
+    Scenario: Revoke read access to research group
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks action menu to revoke access
+        Then action menu holds option to grant access to research group
+
+    Scenario: Grant read access to research group
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And clicks action menu to grant access
+        Then action menu holds option to revoke access from research group
+
+
+    Scenario: Copy datapackage to research space
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks action menu to copy data package to research
+        And user chooses research folder corresponding to "<vault>"
+        And user presses copy package button
+        #Then data package is copied to research area
+
+    Scenario: Check datapackage compliancy with policy [OK]
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks clicks action menu to check compliancy
+        And user chooses policy
+        Then compliancy result is presented
+
+    Scenario: Go to research space
+        Given user "datamanager" is logged in
+        And module "vault" is shown
+        When user browses to data package in "<vault>"
+        And user clicks action menu go to research
+        Then the research space of "<vault>" is shown
--- tests/step_defs/ui/test_ui_vault.py
+++ tests/step_defs/ui/test_ui_vault.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """Vault UI feature tests."""
 
-__copyright__ = 'Copyright (c) 2020, Utrecht University'
+__copyright__ = 'Copyright (c) 2020-2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
 from pytest_bdd import (
@@ -52,6 +52,141 @@ def ui_data_package_approve(browser):
     browser.find_by_css('a.action-approve-for-publication').click()
 
 
+@when('user requests depublication of data package')
+def ui_data_package_depublish(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-depublish-publication').click()
+    browser.find_by_css('.action-confirm-depublish-publication').click()
+
+
+@when('user requests republication of data package')
+def ui_data_package_republish(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-republish-publication').click()
+    # And confirm republication
+    browser.find_by_css('.action-confirm-republish-publication').click()
+
+
 @then(parsers.parse('the data package status is "{status}"'))
 def ui_data_package_status(browser, status):
-    browser.is_text_present(status, wait_time=10)
+    for _i in range(25):
+        if browser.is_text_present(status, wait_time=3):
+            return True
+        browser.reload()
+
+    raise AssertionError()
+
+
+@when('user clicks metatadata button')
+def ui_data_package_click_metadata_button(browser):
+    browser.find_by_css('button.metadata-form').click()
+
+
+@then('metadata form is visible')
+def ui_data_package_metadata_form_is_visible(browser):
+    assert browser.is_element_visible_by_css('.metadata-form', wait_time=5)
+
+
+@when('user clicks system metadata icon')
+def ui_data_package_click_system_metadata_icon(browser):
+    browser.is_element_visible_by_css('.system-metadata', wait_time=5)
+    browser.find_by_css('.system-metadata-icon').click()
+
+
+@then('system metadata is visible')
+def ui_data_package_system_metadata_is_visible(browser):
+    assert browser.is_element_visible_by_css('.system-metadata')
+
+
+@when('user clicks provenance icon')
+def ui_data_package_click_provenance_icon(browser):
+    browser.is_element_visible_by_css('.actionlog-icon', wait_time=5)
+    browser.find_by_css('.actionlog-icon').click()
+
+
+@then('provenance information is visible')
+def ui_data_package_provenance_information_is_visible(browser):
+    assert browser.is_element_visible_by_css('.actionlog')
+
+
+@when('user clicks action menu to revoke access')
+def ui_data_package_revoke_vault_access(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-revoke-vault-access').click()
+
+
+@then('action menu holds option to grant access to research group')
+def ui_data_package_grant_option_present(browser):
+    browser.find_by_id('actionMenu').click()
+    assert browser.is_element_present_by_css('.action-grant-vault-access')
+
+
+@when('clicks action menu to grant access')
+def ui_data_package_grant_vault_access(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-grant-vault-access').click()
+
+
+@then('action menu holds option to revoke access from research group')
+def ui_data_package_revoke_option_present(browser):
+    browser.find_by_id('actionMenu').click()
+    assert browser.is_element_present_by_css('.action-revoke-vault-access')
+
+
+@when('user clicks action menu to copy data package to research')
+def ui_data_package_copy_to_resarch(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-copy-vault-package-to-research').click()
+
+
+@when('user chooses research folder corresponding to "<vault>"')
+def ui_browse_research_to_copy_data_package_to(browser, vault):
+    research = vault.replace("vault-", "research-")
+    href = "?dir=%2F{}".format(research)
+    link = []
+    while len(link) == 0:
+        link = browser.links.find_by_href(href)
+        if len(link) > 0:
+            link.click()
+        else:
+            browser.find_by_id('folder-select-browser_next').click()
+
+
+@when('user presses copy package button')
+def ui_user_presses_copy_package_button(browser):
+    browser.find_by_id('btn-copy-package').click()
+
+
+@then('data package is copied to research area')
+def ui_data_package_is_copied_to_research(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.is_element_present_by_css('.action-revoke-vault-access')
+
+
+@when('user clicks clicks action menu to check compliancy')
+def ui_data_package_check_compliancy(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-check-for-unpreservable-files').click()
+
+
+@when('user chooses policy')
+def ui_data_package_choose_policy(browser):
+    browser.find_by_id('file-formats-list').click()
+    browser.find_option_by_value('DANS').click()
+
+
+@then('compliancy result is presented')
+def ui_data_package_compliancy_is_presented(browser):
+    assert browser.find_by_css('p.help')
+
+
+@when('user clicks action menu go to research')
+def ui_data_package_go_to_research(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-go-to-research').click()
+
+
+@then('the research space of "<vault>" is shown')
+def ui_vault_research_space(browser, vault):
+    research = vault.replace("vault-", "research-")
+    assert browser.is_text_present(research, wait_time=3)
