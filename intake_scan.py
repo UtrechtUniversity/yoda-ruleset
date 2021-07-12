921882bdf7ab44753208bc2d4c99c7de842b404e
Sietse Snel <s.t.snel@uu.nl>
2021-07-12 22:12:29 +0200

    YDA-4130: add intake experiment type infdualet

--- intake_scan.py
+++ intake_scan.py
@@ -240,7 +240,8 @@ def intake_extract_tokens(ctx, string):
                  # 16-Apr-2019 fbyoda email request new exp type:
                  "chdualet",
                  # 15-Feb-2021 fbyoda email request new exp type:
-                 "functionalmriscan"]
+                 "functionalmriscan",
+                 "infdualet"]
 
     str_lower = string.lower()
     str_upper = string.upper()
