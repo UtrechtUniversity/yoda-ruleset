1c21f18656648b8394f739ea33d36e7403982763
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-07-23 10:15:36 +0200

    Fix metadata vault ingest flow (#101)

--- publication.py
+++ publication.py
@@ -753,8 +753,8 @@ def process_publication(ctx, vault_package):
         publication_state["status"] = "OK"
         save_publication_state(ctx, vault_package, publication_state)
         provenance.log_action(ctx, "system", vault_package, "publication updated")
-        log.write(ctx, "process_publication: All steps for publication completed <{}>".format(vault_package))
 
+    log.write(ctx, "process_publication: All steps for publication completed <{}>".format(vault_package))
     return publication_state["status"]
 
 
@@ -1028,7 +1028,7 @@ def update_publication(ctx, vault_package):
     """Routine to update a publication with sanity checks at every step."""
     publication_state = {}
 
-    log.write(ctx, "update_landingpage: Process vault package <{}>".format(vault_package))
+    log.write(ctx, "update_publication: Process vault package <{}>".format(vault_package))
 
     # check permissions - rodsadmin only
     if user.user_type(ctx) != 'rodsadmin':
--- tools/ingest-changes-into-vault.r
+++ tools/ingest-changes-into-vault.r
@@ -1,7 +1,7 @@
 ingestChangesIntoVault {
 	# First scan for any pending changes in the datamanager area
 	*ContInxOld = 1;
-	msiAddSelectFieldToGenQuery("COLL_NAME", "", *GenQInp);	
+	msiAddSelectFieldToGenQuery("COLL_NAME", "", *GenQInp);
 	msiAddConditionToGenQuery("DATA_NAME", "=", IIJSONMETADATA, *GenQInp);
 	msiAddConditionToGenQuery("META_DATA_ATTR_NAME", "=", UUORGMETADATAPREFIX ++ "cronjob_vault_ingest", *GenQInp);
 	msiAddConditionToGenQuery("META_DATA_ATTR_VALUE", "=", CRONJOB_PENDING, *GenQInp);
@@ -14,8 +14,8 @@ ingestChangesIntoVault {
 		foreach(*row in *GenQOut) {
 			*collName = *row.COLL_NAME;
 			*metadataPath = *row.COLL_NAME ++ "/" ++ IIJSONMETADATA;
-			
-			if (*collName like regex "/[^/]+/home/datamanager-.*") {	
+
+			if (*collName like regex "/[^/]+/home/datamanager-.*") {
 				# ensure rodsadmin access to the datamanager collection and metadata
 				*status     = "";
 				*statusInfo = "";
@@ -45,50 +45,6 @@ ingestChangesIntoVault {
 		}
 	}
 	msiCloseGenQuery(*GenQInp, *GenQOut);
-
-
-	# Scan for vault packages with a pending publication update.
-	*ContInxOld = 1;
-	msiAddSelectFieldToGenQuery("COLL_NAME", "", *GenQ2Inp);
-	msiAddConditionToGenQuery("COLL_NAME", "like", "%%/home/vault-%%", *GenQ2Inp);
-	msiAddConditionToGenQuery("META_COLL_ATTR_NAME", "=", UUORGMETADATAPREFIX ++ "cronjob_publication_update", *GenQ2Inp);
-	msiAddConditionToGenQuery("META_COLL_ATTR_VALUE", "=", CRONJOB_PENDING, *GenQ2Inp);
-
-	msiExecGenQuery(*GenQ2Inp, *GenQ2Out);
-	msiGetContInxFromGenQueryOut(*GenQ2Out, *ContInxNew);
-
-	while(*ContInxOld > 0) {
-		foreach(*row in *GenQ2Out) {
-			*collName = *row.COLL_NAME;
-
-			# Check if this really is a vault package
-			if (*collName like regex "/[^/]+/home/vault-.*") {
-				*err = errorcode(iiProcessPublication(*collName, *status));
-				if (*err < 0) {
-					writeLine("stdout", "iiProcessPublication *collName returned errorcode *err");
-				} else {
-				        if (*status == "OK") {
-                                           msiString2KeyValPair("", *publicationUpdateKvp);
-                                           *publicationUpdate = UUORGMETADATAPREFIX ++ "cronjob_publication_update=" ++ CRONJOB_PENDING;
-                                           msiString2KeyValPair(*publicationUpdate, *publicationUpdateKvp);
-                                           *err = errormsg(msiRemoveKeyValuePairsFromObj(*publicationUpdateKvp, *collName, "-C"), *msg);
-					}
-					writeLine("stdout", "iiProcessPublication *collName returned with status: *status");
-					if (*status == "Retry") {
-						delay ("<PLUSET>60s</PLUSET>") {
-							iiScheduledVaultActions();
-						}
-					}
-				}
-			}
-		}
-
-		*ContInxOld = *ContInxNew;
-		if(*ContInxOld > 0) {
-			msiGetMoreRows(*GenQ2Inp, *GenQ2Out, *ContInxNew);
-		}
-	}
-	msiCloseGenQuery(*GenQ2Inp, *GenQ2Out);
 }
 input null
 output ruleExecOut
