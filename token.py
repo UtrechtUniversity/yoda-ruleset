2efb26a371daf9c6630716104551ad6b22b49cb4
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-10-11 11:04:34 +0200

    Do not include username and label in token.

--- token.py
+++ token.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Functions for token management"""
+"""Functions for token management."""
 
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
@@ -18,30 +18,21 @@ __all__ = ['api_generate_token',
 
 # Location of the database that contain the tokens
 TOKEN_DB = '/etc/irods/irods-ruleset-uu/tokens.db'
+
 # Lifetime of a token. TODO: Currently NOT actively being checked
-TOKEN_LIFETIME = timedelta(minutes=30)
+TOKEN_LIFETIME = timedelta(hours=72)
+
+# Length of token
+TOKEN_LENGTH = 32
 
 
 @api.make()
 def api_generate_token(ctx, label=None):
-    def generate_token(user, label):
-        # Ideally all forms of passwords/tokens have a prefix so the
-        # correct authentication method can be selected in the PAM stack.
-        # Unfortunately there is no way to control all sources of
-        # authentication (i.e. iCommands client) so we can't rely on the
-        # prefix to tell us what auth method should be used (because in
-        # the iCommands client example the prefixes could be added manually).
-        # In the PAM stack itself there is no known way of differentiating between
-        # sources for the authentication request: authentication goes through
-        # PamAuthReq_In types of messages which do not contain any information
-        # about the source, afaik.
-        # TODO: actual randomly generated tokens. Simple here for debugging
-        # purposes.
-
-        return user + ':' + label + ':' + secrets.token_urlsafe(32)
+    def generate_token():
+        return secrets.token_urlsafe(TOKEN_LENGTH)
 
     user_id = user.name(ctx)
-    token = generate_token(user_id, label)
+    token = generate_token()
     # !!! TODO
     # The timestamps are just for show now; no checking for validity is done
     # and it should be noted that no thought has been put into the strange
