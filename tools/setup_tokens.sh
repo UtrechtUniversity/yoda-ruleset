9e7245ce7f2e350eb937a50e82519865fdc3dc56
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-10-26 14:54:31 +0200

    Make token database location configurable.

--- tools/setup_tokens.sh
+++ tools/setup_tokens.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # This command is for setting up the database for token management
 
-touch /etc/irods/irods-ruleset-uu/tokens.db
-sqlite3 /etc/irods/irods-ruleset-uu/tokens.db 'create table tokens (user, label, token, gen_time, exp_time)' 
+touch $1
+sqlite3 $1 'create table tokens (user, label, token, gen_time, exp_time)'
