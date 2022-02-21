d91df7edf6d0c3fa29d9a601326a631d391e8d19
Felix A. Croes <felix@dworkin.nl>
2022-02-21 13:02:39 +0100

    YDA-4522 Only index "OwnerRole" if it exists.

--- meta.py
+++ meta.py
@@ -317,9 +317,10 @@ def update_index_metadata(ctx, path, metadata):
         avu.associate_to_data(ctx, path,
                               constants.UUINDEXMETADATAPREFIX + 'Creator',
                               name['Given_Name'] + ' ' + name['Family_Name'])
-        avu.associate_to_data(ctx, path,
-                              constants.UUINDEXMETADATAPREFIX + 'OwnerRole',
-                              creator['Owner_Role'])
+        if 'Owner_Role' in creator:
+            avu.associate_to_data(ctx, path,
+                                  constants.UUINDEXMETADATAPREFIX + 'OwnerRole',
+                                  creator['Owner_Role'])
     avu.associate_to_data(ctx, path, constants.UUINDEXMETADATAPREFIX + 'Title',
                           metadata['Title'])
     avu.associate_to_data(ctx, path, constants.UUINDEXMETADATAPREFIX + 'Description',
