5af588311cd348cc60e625492af2e1814e8094f4
Felix A. Croes <felix@dworkin.nl>
2022-03-04 08:21:50 +0100

    YDA-4537 Scripts to enable/disable indexing for existing vault collections.

---
+++ tools/add-vault-indexing.r
@@ -0,0 +1,18 @@
+addVaultIndexing {
+	uuGetUserType("$userNameClient#$rodsZoneClient", *usertype);
+
+	if (*usertype != "rodsadmin") {
+		failmsg(-1, "This script needs to be run by a rodsadmin");
+	}
+
+	foreach (*row in SELECT USER_NAME WHERE USER_TYPE = 'rodsgroup' AND USER_NAME like 'vault-%') {
+		*vaultGroupName = *row.USER_NAME;		
+		*vaultHome = "/" ++ $rodsZoneClient ++ "/home/" ++ *vaultGroupName;
+		if (uuCollectionExists(*vaultHome)) {
+			msiExecCmd("enable-indexing.sh", *vaultHome, "", "", 0, *out);
+		}
+	}
+}
+
+input null
+output ruleExecOut
