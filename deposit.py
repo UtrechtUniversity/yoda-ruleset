9b446208150495ad69e86400dbe45ac7f170c101
Harm de Raaff <harm.apple@gmail.com>
2022-03-15 11:32:53 +0100

    wip

--- deposit.py
+++ deposit.py
@@ -4,15 +4,123 @@
 __copyright__ = 'Copyright (c) 2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
+import re
+from collections import OrderedDict
+
+from genquery import AS_DICT, Query
+import genquery
+
+
 import folder
 import meta
 from util import *
 
 __all__ = ['api_deposit_create',
            'api_deposit_status',
-           'api_deposit_submit']
+           'api_deposit_submit',
+           'api_deposit_browse_collections']
 
 DEPOSIT_GROUP = "deposit-pilot"
+
+
+@api.make()
+def api_deposit_browse_collections(ctx,
+                                   coll='/',
+                                   sort_on='name',
+                                   sort_order='asc',
+                                   offset=0,
+                                   limit=10,
+                                   space=pathutil.Space.OTHER.value):
+    """Get paginated collection contents, including size/modify date information.
+
+    This function browses a folder and only looks at the collections in it. No dataobjects.
+    Specifically for folder selection for copying data to research area from vault for instance.
+
+    :param ctx:        Combined type of a callback and rei struct
+    :param coll:       Collection to get paginated contents of
+    :param sort_on:    Column to sort on ('name', 'modified' or size)
+    :param sort_order: Column sort order ('asc' or 'desc')
+    :param offset:     Offset to start browsing from
+    :param limit:      Limit number of results
+    :param space:      Space the collection is in
+
+    :returns: Dict with paginated collection contents
+    """
+    def transform(row):
+        # Remove ORDER_BY etc. wrappers from column names.
+        x = {re.sub('.*\((.*)\)', '\\1', k): v for k, v in row.items()}
+
+        deposit_title = '[No title]'
+        deposit_size = 0
+        deposit_count = 0
+        log.write(ctx, x['COLL_NAME'])
+        iter = genquery.row_iterator(
+            "SUM(DATA_SIZE), COUNT(DATA_ID)",
+            "COLL_NAME like '{}/%'".format(x['COLL_NAME']),
+            genquery.AS_LIST, ctx
+        )
+        for row in iter:
+            deposit_size = int(row[0])
+            deposit_count = int(row[1])
+
+        iter = genquery.row_iterator(
+            "META_DATA_ATTR_VALUE",
+            "COLL_NAME = '{}' AND META_DATA_ATTR_NAME = 'usr_Title'".format(x['COLL_NAME']),
+            genquery.AS_LIST, ctx
+        )
+        for row in iter:
+            deposit_title = row[0]
+
+        return {'name':        x['COLL_NAME'].split('/')[-1],
+                'type':        'coll',
+                'modify_time': int(x['COLL_MODIFY_TIME']),
+                'deposit_title': deposit_title,
+                'deposit_count': deposit_count,
+                'deposit_size': deposit_size}
+
+    if sort_on == 'modified':
+        # FIXME: Sorting on modify date is borked: There appears to be no
+        # reliable way to filter out replicas this way - multiple entries for
+        # the same file may be returned when replication takes place on a
+        # minute boundary, for example.
+        # We would want to take the max modify time *per* data name.
+        # (or not? replication may take place a long time after a modification,
+        #  resulting in a 'too new' date)
+        ccols = ['COLL_NAME', 'ORDER(COLL_MODIFY_TIME)']
+    elif sort_on == 'size':
+        ccols = ['COLL_NAME', 'COLL_MODIFY_TIME']
+    else:
+        ccols = ['ORDER(COLL_NAME)', 'COLL_MODIFY_TIME']
+
+    if sort_order == 'desc':
+        ccols = [x.replace('ORDER(', 'ORDER_DESC(') for x in ccols]
+
+    zone = user.zone(ctx)
+
+    # We make offset/limit act on two queries at once, placing qdata right after qcoll.
+    if space == str(pathutil.Space.RESEARCH):
+        qcoll = Query(ctx, ccols,
+                      "COLL_PARENT_NAME = '{}' AND COLL_NAME not like '/{}/home/vault-%' AND COLL_NAME not like '/{}/home/grp-vault-%'".format(coll, zone, zone),
+                      offset=offset, limit=limit, output=AS_DICT)
+    elif space == str(pathutil.Space.VAULT):
+        qcoll = Query(ctx, ccols,
+                      "COLL_PARENT_NAME = '{}' AND COLL_NAME like '/{}/home/%vault-%'".format(coll, zone),
+                      offset=offset, limit=limit, output=AS_DICT)
+    else:
+        qcoll = Query(ctx, ccols, "COLL_PARENT_NAME = '{}'".format(coll),
+                      offset=offset, limit=limit, output=AS_DICT)
+
+    colls = map(transform, list(qcoll))
+
+    if len(colls) == 0:
+        # No results at all?
+        # Make sure the collection actually exists.
+        if not collection.exists(ctx, coll):
+            return api.Error('nonexistent', 'The given path does not exist')
+        # (checking this beforehand would waste a query in the most common situation)
+
+    return OrderedDict([('total', qcoll.total_rows()),
+                        ('items', colls)])
 
 
 def determine_deposit_path(ctx):
