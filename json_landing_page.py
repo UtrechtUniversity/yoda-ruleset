93596d830269d9a880876cd7bac93df066a45777
Harm de Raaff <harm.apple@gmail.com>
2022-06-03 04:51:29 +0200

    flake8

--- json_landing_page.py
+++ json_landing_page.py
@@ -8,13 +8,6 @@ import jinja2
 
 from util import *
 
-def orcid():
-    """ example of presentation of orcid - not used at the moment """
-
-    return ('<a href="https://orcid.org/0000-0001-5727-2427">'
-            '<img alt="ORCID logo" src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png" width="16" height="16" />'
-            'https://orcid.org/0000-0001-2345-6789'
-            '</a>')
 
 def orcid_schema_id_to_identifier(identifier):
     """ in case of ORCID -> return only the id part of, what could be, a complete uri that is passed as identifier """
@@ -80,7 +73,7 @@ def schema_id_to_uri(schema_id, identifier):
         domain = identifier
         id = ''
     else:
-        domain = 'https://' + schema_id + '.org/' 
+        domain = 'https://' + schema_id + '.org/'
         id = identifier
 
     return domain + id
