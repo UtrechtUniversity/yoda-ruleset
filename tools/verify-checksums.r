1610332543b65da3da7423f78d646be0c414efc9
Felix A. Croes <felix@dworkin.nl>
2022-06-08 13:37:21 +0200

    YDA-4678 Checksum verification in batches.
    First step towards delay-based batch system that we can use for checksums,
    replications, revisions, etc.

--- tools/verify-checksums.r
+++ tools/verify-checksums.r
@@ -1,50 +1,8 @@
 #!/usr/bin/irule -r irods_rule_engine_plugin-irods_rule_language-instance -F 
 #
-# Verify, and optionally set, checksums.
-#
-# usage: irule -F verify-checksums.r
-#        irule -F verify-checksums.r "*update=1"
-#
-verifyChecksums() {
-    # all collections under home
-    foreach (*row in SELECT COLL_NAME WHERE COLL_NAME like '/$rodsZoneClient/home/%') {
-	collChecksums(*row.COLL_NAME, *update);
-    }
+verifyChecksum() {
+    verifyChecksumBatch(0, *max, *update);
 }
 
-collChecksums(*coll, *update) {
-    # check data objects in collection
-    foreach (*data in SELECT DATA_NAME,DATA_CHECKSUM WHERE COLL_NAME = *coll) {
-	*name = *data.DATA_NAME;
-	dataChecksum("*coll/*name", *data.DATA_CHECKSUM, *update);
-    }
-}
-
-dataChecksum(*path, *chksum, *update) {
-    msiCheckAccess(*path, "read object", *access);
-    if (*access == 0) {
-	msiSetACL("default", "admin:read", uuClientFullName, *path);
-    }
-
-    if (*chksum == "") {
-	writeLine("stdout", "*path: no checksum");
-	if (*update != 0) {
-	    errorcode(msiDataObjChksum(*path, "ChksumAll=", *status));
-	}
-    } else {
-	msiSubstr(*chksum, "0", "5", *type);
-	writeLine("stdout", "*path: *chksum");
-	if (*type == "sha2:") {
-	    errorcode(msiDataObjChksum(*path, "verifyChksum=", *status));
-	} else if (*update != 0) {
-	    errorcode(msiDataObjChksum(*path, "ChksumAll=++++forceChksum=", *status));
-	}
-    }
-
-    if (*access == 0) {
-	msiSetACL("default", "admin:null", uuClientFullName, *path);
-    }
-}
-
-input *update=0
+input *max=1, *update=0
 output ruleExecOut
