4ca5c4db13a77189c90ea7a03bf58da23719b637
Felix A. Croes <felix@dworkin.nl>
2022-06-14 11:42:13 +0200

    YDA-4663 only add/remove index for vault-pilot.
    Other vault collections are currently not indexed.

--- tools/add-vault-indexing.r
+++ tools/add-vault-indexing.r
@@ -6,13 +6,10 @@ addVaultIndexing {
 		failmsg(-1, "This script needs to be run by a rodsadmin");
 	}
 
-	foreach (*row in SELECT USER_NAME WHERE USER_TYPE = 'rodsgroup' AND USER_NAME like 'vault-%') {
-		*vaultGroupName = *row.USER_NAME;		
-		*vaultHome = "/" ++ $rodsZoneClient ++ "/home/" ++ *vaultGroupName;
-		if (uuCollectionExists(*vaultHome)) {
-			foreach (*row2 in SELECT COLL_NAME WHERE COLL_PARENT_NAME = *vaultHome) {
-				msiExecCmd("enable-indexing.sh", *row2.COLL_NAME, "", "", 0, *out);
-			}
+	*vaultHome = "/" ++ $rodsZoneClient ++ "/home/vault-pilot";
+	if (uuCollectionExists(*vaultHome)) {
+		foreach (*row in SELECT COLL_NAME WHERE COLL_PARENT_NAME = *vaultHome) {
+			msiExecCmd("enable-indexing.sh", *row.COLL_NAME, "", "", 0, *out);
 		}
 	}
 }
