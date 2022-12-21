cf07599832ff0fb68c53206da3c862a396c9dbad
HarmdR <harm.apple@gmail.com>
2022-12-21 12:48:15 +0100

    Changed group retention period to group expiration date.
    Co-authored-by: Lazlo Westerhof <l.r.westerhof@uu.nl>

--- tests/step_defs/api/test_api_group.py
+++ tests/step_defs/api/test_api_group.py
@@ -74,9 +74,8 @@ def api_group_create(user, group_name):
          "category": "api-test",
          "subcategory": "api-test",
          "schema_id": "default-2",
-         "retention_period": "",
+         "expiration_date": "",
          "description": "",
-         "retention_period": "",
          "data_classification": "public"}
     )
 
