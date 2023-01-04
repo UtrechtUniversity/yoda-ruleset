784430882b956bcc0550097d487d96a7eea55916
Sietse Snel <s.t.snel@uu.nl>
2023-01-04 12:59:50 +0100

    YDA-5005: fix exception in daily report
    by adjusting reference to mail wrapper

--- notifications.py
+++ notifications.py
@@ -185,11 +185,11 @@ def rule_mail_notification_report(ctx, to, notifications):
     if not user.is_admin(ctx):
         return api.Error('not_allowed', 'Only rodsadmin can send test mail')
 
-    return _wrapper(ctx,
-                    to=to,
-                    actor='system',
-                    subject='[Yoda] {} notification(s)'.format(notifications),
-                    body="""
+    return mail.wrapper(ctx,
+                        to=to,
+                        actor='system',
+                        subject='[Yoda] {} notification(s)'.format(notifications),
+                        body="""
 You have {} notification(s).
 
 Login to view all your notifications: https://{}/user/notifications
