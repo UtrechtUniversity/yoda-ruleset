835cd4fa1b7f33f1ff2a2cc5523389a5a0ef9677
Sietse Snel <s.t.snel@uu.nl>
2023-01-26 14:43:09 +0100

    YDA-4925: add API endpoint delete expired DAPs
    Add API endpoint to delete expired data access passwords. Called
    from portal in order to prevent label conflicts between newly
    created tokens and expired tokens that are no longer visible to user.

--- data_access_token.py
+++ data_access_token.py
@@ -15,7 +15,8 @@ from util import *
 
 __all__ = ['api_token_generate',
            'api_token_load',
-           'api_token_delete']
+           'api_token_delete',
+           'api_token_delete_expired']
 
 
 @api.make()
@@ -121,6 +122,69 @@ def api_token_delete(ctx, label):
     except Exception:
         print_exc()
         result = api.Error('DatabaseError', 'Error during deletion from database')
+
+    # Connection object used as context manager only commits or rollbacks transactions,
+    # so the connection object should be closed manually
+    conn.close()
+
+    return result
+
+
+@api.make()
+def api_token_delete_expired(ctx):
+    """Deletes expired tokens of current user
+
+    :param ctx:   Combined type of a callback and rei struct
+
+    :returns: Status of token deletion
+    """
+    if not token_database_initialized():
+        return api.Error('DatabaseError', 'Internal error: token database unavailable')
+
+    user_id = user.name(ctx)
+    conn = sqlite3.connect(config.token_database)
+    result = None
+
+    try:
+        with conn:
+            conn.execute("PRAGMA key='%s'" % (config.token_database_password))
+            conn.execute('''DELETE FROM tokens WHERE user = ? AND exp_time < ? ''', (user_id, datetime.now()))
+            result = api.Result.ok()
+    except Exception:
+        print_exc()
+        result = api.Error('DatabaseError', 'Error during deletion from database')
+
+    # Connection object used as context manager only commits or rollbacks transactions,
+    # so the connection object should be closed manually
+    conn.close()
+
+    return result
+
+
+def get_all_tokens(ctx):
+    """Retrieve all valid tokens.
+    :param ctx: Combined type of a callback and rei struct
+
+    :returns: Valid tokens
+    """
+    # check permissions - rodsadmin only
+    if user.user_type(ctx) != 'rodsadmin':
+        return []
+
+    if not token_database_initialized():
+        return []
+
+    conn = sqlite3.connect(config.token_database)
+    result = []
+    try:
+        with conn:
+            conn.execute("PRAGMA key='%s'" % (config.token_database_password))
+            for row in conn.execute('''SELECT user, label, exp_time FROM tokens WHERE exp_time > :now''',
+                                    {"now": datetime.now()}):
+                result.append({"user": row[0], "label": row[1], "exp_time": row[2]})
+    except Exception:
+        print_exc()
+        result = api.Error('DatabaseError', 'Error occurred while reading database')
 
     # Connection object used as context manager only commits or rollbacks transactions,
     # so the connection object should be closed manually
