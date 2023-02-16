efab652977484a2df87094371b3728dbe3c258ed
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-02-16 15:37:10 +0100

    Add UI tests for unarchival of vault data packages.

--- tests/features/ui/ui_vault_archive.feature
+++ tests/features/ui/ui_vault_archive.feature
@@ -6,8 +6,45 @@ Feature: Vault Archive UI
         When user browses to data package in <vault>
         And user submits the data package for archival
         And user confirms archival of data package
-        Then the data package archive status is "Submitted for archive"
-        And provenance log includes "Submitted for archive"
+        Then the data package archive status is "Scheduled for archive"
+        And provenance log includes "Scheduled for archive"
+
+        Examples:
+            | vault          |
+            | vault-initial1 |
+
+
+    Scenario Outline: Vault archived
+        Given user datamanager is logged in
+        And module "vault" is shown
+        When user browses to data package in <vault>
+        Then the data package archive status is "Archived"
+        And provenance log includes "Archived"
+
+        Examples:
+            | vault          |
+            | vault-initial1 |
+
+
+    Scenario Outline: Vault unarchive
+        Given user datamanager is logged in
+        And module "vault" is shown
+        When user browses to data package in <vault>
+        And user submits the data package for unarchival
+        And user confirms unarchival of data package
+        Then the data package archive status is "Scheduled for unarchive"
+        And provenance log includes "Scheduled for unarchive"
+
+        Examples:
+            | vault          |
+            | vault-initial1 |
+
+
+    Scenario Outline: Vault unarchived
+        Given user datamanager is logged in
+        And module "vault" is shown
+        When user browses to data package in <vault>
+        Then provenance log includes "Unarchived"
 
         Examples:
             | vault          |
--- tests/step_defs/ui/test_ui_vault_archive.py
+++ tests/step_defs/ui/test_ui_vault_archive.py
@@ -34,20 +34,31 @@ def ui_browse_data_package(browser, vault):
 
 
 @when('user submits the data package for archival')
-def ui_data_package_submit(browser):
+def ui_data_package_archive(browser):
     browser.find_by_id('actionMenu').click()
     browser.find_by_css('a.action-vault-archival').click()
 
 
 @when('user confirms archival of data package')
-def ui_data_package_agree(browser):
+def ui_data_package_archival_agree(browser):
     browser.find_by_css('.action-confirm-vault-archival').click()
+
+
+@when('user submits the data package for unarchival')
+def ui_data_package_unarchival(browser):
+    browser.find_by_id('actionMenu').click()
+    browser.find_by_css('a.action-vault-unarchive').click()
+
+
+@when('user confirms unarchival of data package')
+def ui_data_package_unarchival_agree(browser):
+    browser.find_by_css('.action-confirm-vault-unarchive').click()
 
 
 @then(parsers.parse('the data package archive status is "{status}"'))
 def ui_data_package_archive_status(browser, status):
-    for _i in range(25):
-        if browser.is_text_present(status, wait_time=3):
+    for _i in range(36):
+        if browser.is_text_present(status, wait_time=10):
             return True
         browser.reload()
 
@@ -60,14 +71,17 @@ def ui_provenance_log(browser, status):
     # This test can be executed repeatedly as always the n top statuses of the package in research will be checked
     # eventhough the folder is used several times in a different test run
     browser.find_by_css('.actionlog-icon')[0].click()
-    prov_statuses = {"Submitted for archive": "Archive scheduled"}
+    prov_statuses = {"Scheduled for archive": "Archive scheduled",
+                     "Archived": "Archive completed",
+                     "Scheduled for unarchive": "Unarchive scheduled",
+                     "Unarchived": "Unarchive completed"}
 
-    for _i in range(25):
+    for _i in range(36):
         if len(browser.find_by_css('.list-group-item-action')):
             action_log_rows = browser.find_by_css('.list-group-item-action')
             break
         else:
-            time.sleep(1)
+            time.sleep(10)
 
     for index in range(0, len(prov_statuses)):
         if action_log_rows[index].value.find(prov_statuses[status]) != -1:
