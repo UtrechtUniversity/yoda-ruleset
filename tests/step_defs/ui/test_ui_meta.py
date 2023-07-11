b3af8bfe6d26051a1bc77c566ea306d6eb92ab95
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-07-11 15:14:18 +0200

    Fix person identifier UI tests.

--- tests/step_defs/ui/test_ui_meta.py
+++ tests/step_defs/ui/test_ui_meta.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """Meta UI feature tests."""
 
-__copyright__ = 'Copyright (c) 2020-2022, Utrecht University'
+__copyright__ = 'Copyright (c) 2020-2023, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
 import time
@@ -32,56 +32,50 @@ def ui_metadata_fill(browser):
 
 @when('users checks person identifier field in metadata form')
 def ui_metadata_check_person_id_field(browser):
-    # Find the fieldset for the first 'Person identifier'
+    # Find the fieldset for the first 'Person identifier'.
     fieldset = browser.find_by_text('Person identifier')[0].find_by_xpath('..')
 
-    # Find the PersonIdentifier scheme selectbox and open the list
+    # Find the PersonIdentifier scheme selectbox and open the list.
     fieldset.find_by_css('.select-box').click()
 
-    # Click ORCID for a PI scheme
-    fieldset.find_by_text('ORCID').click()
-
+    # Check ORCID lookup link.
     lookup_link = browser.links.find_by_partial_text('Lookup ORCID')
     assert len(lookup_link) > 0
     assert lookup_link[0]['href'] == 'https://orcid.org/orcid-search/search?searchQuery='
 
-    # ORCID brings a specific pattern with it
-    # Pattern test
     parent = lookup_link.find_by_xpath('..')
 
+    # Check ORCID placeholder.
     assert parent.find_by_css('.form-control')[0]['placeholder'] == 'https://orcid.org/0009-0005-4692-1299'
-    # Enter own orcid
-    orcid = '1234-5678-1234-5678'
+
+    # Enter ORCID.
+    orcid = '0009-0005-4692-1299'
     parent.find_by_css('.form-control')[0].fill(orcid.replace('-', ''))
     assert parent.find_by_css('.form-control')[0].value == 'https://orcid.org/' + orcid
 
-    # SCOPUS
+    # Select Author identifier (Scopus).
     fieldset.find_by_css('.select-box').click()
     fieldset.find_by_text('Author identifier (Scopus)').click()
 
-    lookup_link = browser.links.find_by_partial_text('Lookup Scopus Author Identifier')
+    # Check Author identifier (Scopus) lookup link.
+    lookup_link = browser.links.find_by_partial_text('Lookup Author identifier (Scopus)')
     assert len(lookup_link) > 0
     assert lookup_link[0]['href'] == 'https://www.scopus.com/freelookup/form/author.uri?zone=TopNavBar&origin='
 
     parent = lookup_link.find_by_xpath('..')
 
-    scopus_id = 'scopus123'
-    parent.find_by_css('.form-control')[0].fill(scopus_id)
-    assert parent.find_by_css('.form-control')[0]['value'] == scopus_id
+    # Check Author identifier (Scopus) placeholder.
+    assert parent.find_by_css('.form-control')[0]['placeholder'] == '51161516100'
 
 
 @when('users clicks save button')
 def ui_metadata_save(browser):
-    # Sleep required as it didn't work otherwise
-    time.sleep(1)
     browser.find_by_css('.yodaButtons .btn-primary').click()
 
 
 @when('users clicks delete all metadata button')
 def ui_metadata_delete(browser):
     browser.find_by_css('.yodaButtons .btn-danger').click()
-    # Sleep required as it didn't work otherwise
-    time.sleep(1)
     browser.find_by_css('.confirm').click()
 
 
