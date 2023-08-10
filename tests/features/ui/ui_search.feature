cc9faf675f8b7559514c6fbc536e22880edb5ea8
HarmdR <harm.apple@gmail.com>
2023-08-10 10:19:40 +0200

    YDA-5272: add UI tests for advanced top search bar
    Co-authored-by: Lazlo Westerhof <l.r.westerhof@uu.nl>

--- tests/features/ui/ui_search.feature
+++ tests/features/ui/ui_search.feature
@@ -1,7 +1,7 @@
 @ui
 Feature: Search UI
 
-    Scenario Outline: Search file
+    Scenario Outline: Search file from search page
         Given user researcher is logged in
         And module "search" is shown
         When the user searches by filename with <file>
@@ -11,7 +11,18 @@ Feature: Search UI
             | file               | result                                |
             | yoda-metadata.json | /research-initial1/yoda-metadata.json |
 
-    Scenario Outline: Search folder
+
+    Scenario Outline: Search file from top search bar
+        Given user researcher is logged in
+        When the user top-searches by filename with <file>
+        Then result <result> is found
+
+        Examples:
+            | file               | result                                |
+            | yoda-metadata.json | /research-initial1/yoda-metadata.json |
+
+
+    Scenario Outline: Search folder from search page
         Given user researcher is logged in
         And module "search" is shown
         When the user searches by folder with <folder>
@@ -21,7 +32,17 @@ Feature: Search UI
             | folder            | result             |
             | research-initial1 | /research-initial1 |
 
-    Scenario Outline: Search metadata
+    Scenario Outline: Search folder from top search bar
+        Given user researcher is logged in
+        When the user top-searches by folder with <folder>
+        Then result <result> is found
+
+        Examples:
+            | folder            | result             |
+            | research-initial1 | /research-initial1 |
+
+
+    Scenario Outline: Search metadata from search page
         Given user researcher is logged in
         And module "search" is shown
         When the user searches by metadata with <metadata>
@@ -31,10 +52,31 @@ Feature: Search UI
             | metadata | result             |
             | yoda     | /research-initial1 |
 
-    Scenario Outline: Search folder status
+
+    Scenario Outline: Search metadata from top search bar
+        Given user researcher is logged in
+        When the user top-searches by metadata with <metadata>
+        Then result <result> is found
+
+        Examples:
+            | metadata | result             |
+            | yoda     | /research-initial1 |
+
+
+    Scenario Outline: Search folder status from search page
         Given user researcher is logged in
         And module "search" is shown
         When the user searches by folder status with <status>
+        Then result <result> is found
+
+        Examples:
+            | status           | result             |
+            | research:SECURED | /research-initial1 |
+
+
+    Scenario Outline: Search folder status from top search bar
+        Given user researcher is logged in
+        When the user top-searches by folder status with <status>
         Then result <result> is found
 
         Examples:
--- tests/step_defs/ui/test_ui_search.py
+++ tests/step_defs/ui/test_ui_search.py
@@ -23,12 +23,28 @@ def ui_search_file(browser, file):
     browser.find_by_css('input#search-filter').type(Keys.RETURN)
 
 
+@when(parsers.parse("the user top-searches by filename with {file}"))
+def ui_search_top_file(browser, file):
+    browser.find_by_css('.container button.dropdown-toggle').click()
+    browser.find_by_css('a.dropdown-item[data-type=filename]').click()
+    browser.find_by_css('input#q').fill(file)
+    browser.find_by_css('input#q').type(Keys.RETURN)
+
+
 @when(parsers.parse("the user searches by folder with {folder}"))
 def ui_search_folder(browser, folder):
     browser.find_by_css('.page button.dropdown-toggle').click()
     browser.find_by_css('a.dropdown-item[data-type=folder]').click()
     browser.find_by_css('input#search-filter').fill(folder)
     browser.find_by_css('input#search-filter').type(Keys.RETURN)
+
+
+@when(parsers.parse("the user top-searches by folder with {folder}"))
+def ui_search_top_folder(browser, folder):
+    browser.find_by_css('.container button.dropdown-toggle').click()
+    browser.find_by_css('a.dropdown-item[data-type=folder]').click()
+    browser.find_by_css('input#q').fill(folder)
+    browser.find_by_css('input#q').type(Keys.RETURN)
 
 
 @when(parsers.parse("the user searches by metadata with {metadata}"))
@@ -39,6 +55,14 @@ def ui_search_metadata(browser, metadata):
     browser.find_by_css('input#search-filter').type(Keys.RETURN)
 
 
+@when(parsers.parse("the user top-searches by metadata with {metadata}"))
+def ui_search_top_metadata(browser, metadata):
+    browser.find_by_css('.container button.dropdown-toggle').click()
+    browser.find_by_css('a.dropdown-item[data-type=metadata]').click()
+    browser.find_by_css('input#q').fill(metadata)
+    browser.find_by_css('input#q').type(Keys.RETURN)
+
+
 @when(parsers.parse("the user searches by folder status with {status}"))
 def ui_search_status(browser, status):
     browser.find_by_css('.page button.dropdown-toggle').click()
@@ -47,12 +71,21 @@ def ui_search_status(browser, status):
     browser.find_by_value(status).click()
 
 
-@then(parsers.parse("result {result} is found"))
-def ui_search_result(browser, result):
-    link = []
-    while len(link) == 0:
-        link = browser.find_by_css(".search-results").links.find_by_partial_text(result)
-        if len(link) > 0:
-            assert browser.is_text_present(result)
-        else:
-            browser.find_by_id('search_next').click()
+@when(parsers.parse("the user top-searches by folder status with {status}"))
+def ui_search_top_status(browser, status):
+    browser.find_by_css('.container button.dropdown-toggle').click()
+    browser.find_by_css('a.dropdown-item[data-type=status]').click()
+    browser.find_by_css('.top-search-status').click()
+    browser.find_by_value(status).click()
+
+
+@then(parsers.parse("result {file} is found"))
+def ui_search_result(browser, file):
+    # no need to step through the list as the results are from searching on this keyword
+    for link in browser.find_by_css("table#search tr td"):
+        if file in link.value:
+            assert True
+            return True
+    assert False
+    # if rows are present, that is good enough
+    assert len(link) == 0
