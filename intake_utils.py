1a279584e5aebe3dd17f43823d1ee547f4e2c675
Sietse Snel <s.t.snel@uu.nl>
2023-10-03 14:34:17 +0200

    YDA-5460 Improve intake collection scan logic
    - Extract logic for collection scans intake module to separate utility
      function, so that we can test it separately.
    - Add error logging for intake scan failures
    - Add some basic unit tests
    - Ensure that we can't overwrite WEP(V) values once we are in a dataset
      (YDA-5460). WEP(V) values can only be overwritten as long as we don't have a
      complete Wave / Experiment type / Pseudocode set

--- intake_utils.py
+++ intake_utils.py
@@ -28,16 +28,25 @@ def intake_tokens_identify_dataset(tokens):
     return (missing == 0)
 
 
-def intake_extract_tokens_from_name(ctx, path, name, is_collection, scoped_buffer):
+def intake_ensure_version_present(ctx, metadata):
+    """Adds a version attribute with a default value to metadata if it is not yet present.
+
+    :param ctx:           Combined type of a callback and rei struct
+    :param metadata:      Dictionary with intake module metadata
+    """
+    if "version" not in metadata:
+        metadata["version"] = "Raw"
+
+
+def intake_extract_tokens_from_name(ctx, path, scoped_buffer):
     """Extract one or more tokens from a file / directory name and add dataset information as metadata.
     :param ctx:           Combined type of a callback and rei struct
-    :param path:          Path to object or collection
-    :param name:          Name of object or collection
-    :param is_collection: Indicates if object or collection
+    :param path:          Full path of the data object or collection
     :param scoped_buffer: Holds dataset buffer with prefilled keys
     :returns: Returns extended scope buffer
     """
-    name_without_ext = os.path.splitext(name)[0]
+    basename = os.path.basename(path)
+    name_without_ext = os.path.splitext(basename)[0]
     parts = re.split("[_-]", name_without_ext)
     for part in parts:
         scoped_buffer.update(intake_extract_tokens(ctx, part))
@@ -120,3 +129,77 @@ def intake_extract_tokens(ctx, string):
         foundKVs["experiment_type"] = str_lower
 
     return foundKVs
+
+
+def intake_scan_get_metadata_update(ctx, path, is_collection, in_dataset, parent_metadata):
+    """Determine metadata to be updated for a particular collection or data object, based
+       on its name and parent metadata.
+
+       This function is separate from the function that actually performs the updates, so
+       that we can test the logic separately.
+
+    :param ctx:    Combined type of a callback and rei struct
+    :param path:   Full path of the data object or collection
+    :param is_collection: true if it's a collection, false if it's a data object
+    :param in_dataset: true if the parent already has complete WEP(V) attributes. Otherwise false.
+    :param parent_metadata: dict containing the intake module metadata of the parent collection ( if any)
+
+    :returns: Returns a dictionary with the following keys / values:
+        new_metadata: dictionary of new metadata to apply to this data object or collection
+        in_dataset: true if current object (along with values passed from parents) has complete WEP(V) values.
+                    otherwise false.
+    """
+
+    local_metadata = parent_metadata.copy()
+
+    result = {"new_metadata": local_metadata, "in_dataset": in_dataset}
+
+    if in_dataset:
+        # If we already are in a dataset, we get all the metadata from the parent. We
+        # cannot override attributes in this case. However we need to remove the top-level
+        # attribute, because the present object is within in a dataset, and thus not a top-level
+        # data object.
+        if "dataset_toplevel" in local_metadata:
+            del [local_metadata["dataset_toplevel"]]
+    else:
+        intake_extract_tokens_from_name(ctx, path, local_metadata)
+        if intake_tokens_identify_dataset(local_metadata):
+            intake_ensure_version_present(ctx, local_metadata)
+            local_metadata["directory"] = path if is_collection else os.path.dirname(path)
+            local_metadata["dataset_id"] = dataset_make_id(local_metadata)
+            local_metadata["dataset_toplevel"] = dataset_make_id(local_metadata)
+            result["in_dataset"] = True
+        else:
+            # result["in_dataset"] is already set to false
+            pass
+
+    return result
+
+
+def dataset_make_id(scope):
+    """Construct a dataset based on WEPV and directory.
+
+    :param scope: Create a dataset id
+
+    :returns: Dataset identifier
+    """
+    return scope['wave'] + '\t' + scope['experiment_type'] + '\t' + scope['pseudocode'] + '\t' + scope['version'] + '\t' + scope['directory']
+
+
+def dataset_parse_id(dataset_id):
+    """Parse a dataset into its consructive data.
+
+    :param dataset_id: Dataset identifier
+
+    :returns: Dataset as a dict
+    """
+    dataset_parts = dataset_id.split('\t')
+    dataset = {}
+    dataset['wave'] = dataset_parts[0]
+    dataset['experiment_type'] = dataset_parts[1]  # Is DIT NOG ERGENS GOED VOOR
+    dataset['expType'] = dataset_parts[1]
+    dataset['pseudocode'] = dataset_parts[2]
+    dataset['version'] = dataset_parts[3]
+    dataset['directory'] = dataset_parts[4]
+
+    return dataset
--- unit-tests/test_intake.py
+++ unit-tests/test_intake.py
@@ -6,12 +6,13 @@
 __copyright__ = 'Copyright (c) 2019-2021, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
+import os
 import sys
 from unittest import TestCase
 
 sys.path.append('..')
 
-from intake_utils import intake_extract_tokens, intake_extract_tokens_from_name, intake_tokens_identify_dataset
+from intake_utils import dataset_make_id, dataset_parse_id, intake_extract_tokens, intake_extract_tokens_from_name, intake_scan_get_metadata_update, intake_tokens_identify_dataset
 
 
 class IntakeTest(TestCase):
@@ -44,9 +45,115 @@ class IntakeTest(TestCase):
 
     def test_intake_extract_tokens_from_name(self):
         buffer = dict()
-        output = intake_extract_tokens_from_name(None, None, "20w_chantigap_B12345_VerABC.txt", None, buffer)
+        output = intake_extract_tokens_from_name(None, "20w_chantigap_B12345_VerABC.txt", buffer)
         self.assertEquals(len(output), 4)
         self.assertEquals(output["wave"], "20w")
         self.assertEquals(output["experiment_type"], "chantigap")
         self.assertEquals(output["version"], "ABC")
         self.assertEquals(output["pseudocode"], "B12345")
+
+    def test_intake_scan_get_metadata_update_coll_in_dataset(self):
+        parent_path = "/foo/bar/chantigap_10w_B12345"
+        path = parent_path + "/chantigap_20w_B12346"
+        complete_metadata = {"wave": "1",
+                             "pseudocode": "2",
+                             "experiment_type": "3",
+                             "version": "Raw",
+                             "directory": parent_path,
+                             "dataset_id": "4",
+                             "dataset_toplevel": "5"}
+
+        output = intake_scan_get_metadata_update(None, path, True, True, complete_metadata)
+        self.assertEquals(output["in_dataset"], True)
+        self.assertEquals(len(output["new_metadata"]), 6)
+        self.assertEquals(output["new_metadata"]["directory"], parent_path)
+        self.assertEquals(output["new_metadata"]["wave"], "1")
+        self.assertEquals(output["new_metadata"]["pseudocode"], "2")
+        self.assertEquals(output["new_metadata"]["experiment_type"], "3")
+        self.assertEquals(output["new_metadata"]["version"], "Raw")
+        self.assertEquals(output["new_metadata"]["dataset_id"], "4")
+        self.assertTrue("dataset_toplevel" not in output["new_metadata"])
+
+    def test_intake_scan_get_metadata_update_coll_out_dataset_complete(self):
+        incomplete_metadata = {"wave": "1", "pseudocode": "2"}
+        path = "/foo/bar/chantigap_10w_B12345/chantigap_B12346"
+        output = intake_scan_get_metadata_update(None, path, True, False, incomplete_metadata)
+        self.assertEquals(output["in_dataset"], True)
+        self.assertEquals(len(output["new_metadata"]), 7)
+        self.assertEquals(output["new_metadata"]["directory"], path)
+        self.assertEquals(output["new_metadata"]["dataset_toplevel"], dataset_make_id(output["new_metadata"]))
+        self.assertEquals(output["new_metadata"]["dataset_id"], dataset_make_id(output["new_metadata"]))
+        self.assertEquals(output["new_metadata"]["wave"], "1")
+        self.assertEquals(output["new_metadata"]["version"], "Raw")
+        self.assertEquals(output["new_metadata"]["pseudocode"], "B12346")
+        self.assertEquals(output["new_metadata"]["experiment_type"], "chantigap")
+
+    def test_intake_scan_get_metadata_update_coll_out_dataset_incomplete(self):
+        incomplete_metadata = {"wave": "1"}
+        path = "/foo/bar/chantigap_10w_B12345/B12346"
+        output = intake_scan_get_metadata_update(None, path, True, False, incomplete_metadata)
+        self.assertEquals(output["in_dataset"], False)
+        self.assertEquals(len(output["new_metadata"]), 2)
+        self.assertEquals(output["new_metadata"]["wave"], "1")
+        self.assertEquals(output["new_metadata"]["pseudocode"], "B12346")
+
+    def test_intake_scan_get_metadata_update_do_in_dataset(self):
+        complete_metadata = {"wave": "1",
+                             "pseudocode": "2",
+                             "experiment_type": "3",
+                             "version": "Raw",
+                             "dataset_id": "4",
+                             "dataset_toplevel": "5",
+                             "directory": "6"}
+        path = "/foo/bar/chantigap_10w_B12345/chantigap_20w_B12346.txt"
+        output = intake_scan_get_metadata_update(None, path, False, True, complete_metadata)
+        self.assertEquals(output["in_dataset"], True)
+        self.assertEquals(len(output["new_metadata"]), 6)
+        self.assertEquals(output["new_metadata"]["wave"], "1")
+        self.assertEquals(output["new_metadata"]["pseudocode"], "2")
+        self.assertEquals(output["new_metadata"]["experiment_type"], "3")
+        self.assertEquals(output["new_metadata"]["version"], "Raw")
+        self.assertEquals(output["new_metadata"]["dataset_id"], "4")
+        self.assertTrue("dataset_toplevel" not in output["new_metadata"])
+
+    def test_intake_scan_get_metadata_update_do_out_dataset_complete(self):
+        incomplete_metadata = {"wave": "1", "pseudocode": "2"}
+        path = "/foo/bar/chantigap_10w_B12345/chantigap_B12346.txt"
+        coll = os.path.dirname(path)
+        output = intake_scan_get_metadata_update(None, path, False, False, incomplete_metadata)
+        self.assertEquals(output["in_dataset"], True)
+        self.assertEquals(len(output["new_metadata"]), 7)
+        self.assertEquals(output["new_metadata"]["directory"], coll)
+        self.assertEquals(output["new_metadata"]["dataset_id"], dataset_make_id(output["new_metadata"]))
+        self.assertEquals(output["new_metadata"]["dataset_toplevel"], dataset_make_id(output["new_metadata"]))
+        self.assertEquals(output["new_metadata"]["wave"], "1")
+        self.assertEquals(output["new_metadata"]["version"], "Raw")
+        self.assertEquals(output["new_metadata"]["pseudocode"], "B12346")
+        self.assertEquals(output["new_metadata"]["experiment_type"], "chantigap")
+
+    def test_intake_scan_get_metadata_update_do_out_dataset_incomplete(self):
+        incomplete_metadata = {"wave": "1"}
+        path = "/foo/bar/chantigap_10w_B12345/B12346.txt"
+        output = intake_scan_get_metadata_update(None, path, False, False, incomplete_metadata)
+        self.assertEquals(output["in_dataset"], False)
+        self.assertEquals(len(output["new_metadata"]), 2)
+        self.assertEquals(output["new_metadata"]["wave"], "1")
+        self.assertEquals(output["new_metadata"]["pseudocode"], "B12346")
+
+    def test_dataset_make_id(self):
+        input = {"wave": "20w",
+                 "experiment_type": "echo",
+                 "pseudocode": "B12345",
+                 "version": "Raw",
+                 "directory": "/foo/bar/baz"}
+        self.assertEquals(dataset_make_id(input),
+                          "20w\techo\tB12345\tRaw\t/foo/bar/baz")
+
+    def test_dataset_parse_id(self):
+        input = "20w\techo\tB12345\tRaw\t/foo/bar/baz"
+        output = dataset_parse_id(input)
+        self.assertEquals(output.get("wave"), "20w")
+        self.assertEquals(output.get("experiment_type"), "echo")
+        self.assertEquals(output.get("pseudocode"), "B12345")
+        self.assertEquals(output.get("version"), "Raw")
+        self.assertEquals(output.get("directory"), "/foo/bar/baz")
