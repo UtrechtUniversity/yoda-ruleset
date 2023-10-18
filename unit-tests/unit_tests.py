90a6a6799714456445b9049a0e15afeefafb9870
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-10-18 15:41:40 +0200

    Unit tests: add tests for the misc utils module

--- unit-tests/unit_tests.py
+++ unit-tests/unit_tests.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-__copyright__ = 'Copyright (c) 2019-2021, Utrecht University'
+__copyright__ = 'Copyright (c) 2019-2023, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
 from unittest import makeSuite, TestSuite
 
 from test_intake import IntakeTest
+from test_util_misc import UtilMiscTest
 
 
 def suite():
     test_suite = TestSuite()
     test_suite.addTest(makeSuite(IntakeTest))
+    test_suite.addTest(makeSuite(UtilMiscTest))
     return test_suite
