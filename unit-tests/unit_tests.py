5bb2134e1d6a139eb6698325d8c475bebdec2fad
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-10-19 10:23:30 +0200

    Unit tests: add tests for pathutil

--- unit-tests/unit_tests.py
+++ unit-tests/unit_tests.py
@@ -7,10 +7,12 @@ from unittest import makeSuite, TestSuite
 
 from test_intake import IntakeTest
 from test_util_misc import UtilMiscTest
+from test_util_pathutil import UtilPathutilTest
 
 
 def suite():
     test_suite = TestSuite()
     test_suite.addTest(makeSuite(IntakeTest))
     test_suite.addTest(makeSuite(UtilMiscTest))
+    test_suite.addTest(makeSuite(UtilPathutilTest))
     return test_suite
