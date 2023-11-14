fac1cc349f3017813e8682cbb1674d604d1c85b8
Lazlo Westerhof <l.r.westerhof@uu.nl>
2023-11-14 14:59:42 +0100

    YDA-5317: refactor test structure to prepare for smoke tests

---
+++ tests/step_defs/smoke/test_smoke_test.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""Browse API feature tests."""
+
+__copyright__ = 'Copyright (c) 2023, Utrecht University'
+__license__   = 'GPLv3, see LICENSE'
+
+from pytest_bdd import (
+    scenarios
+)
+
+
+scenarios('../../features/smoke/smoke_test.feature')
