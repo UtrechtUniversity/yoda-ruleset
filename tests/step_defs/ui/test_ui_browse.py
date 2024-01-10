bc3b6e766ae2a5e4bb819d6787be9200beb2c89b
claravox <claravox@users.noreply.github.com>
2024-01-10 10:17:02 +0100

    Tests for checking path param in metadata form

--- tests/step_defs/ui/test_ui_browse.py
+++ tests/step_defs/ui/test_ui_browse.py
@@ -6,12 +6,6 @@ __license__   = 'GPLv3, see LICENSE'
 
 from pytest_bdd import (
     scenarios,
-    then,
 )
 
 scenarios('../../features/ui/ui_browse.feature')
-
-
-@then('the 404 error page is shown')
-def ui_browse_404(browser):
-    browser.is_text_present("Page not found")
