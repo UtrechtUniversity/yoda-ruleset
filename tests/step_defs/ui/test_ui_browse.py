309fe13630d595cda26714f67985ea90329f2d9b
claravox <claravox@users.noreply.github.com>
2024-02-15 08:26:32 +0100

    YDA-5575: Add file viewer with API and UI tests

--- tests/step_defs/ui/test_ui_browse.py
+++ tests/step_defs/ui/test_ui_browse.py
@@ -5,7 +5,14 @@ __copyright__ = 'Copyright (c) 2020-2022, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
 from pytest_bdd import (
+    parsers,
     scenarios,
+    then
 )
 
 scenarios('../../features/ui/ui_browse.feature')
+
+
+@then(parsers.parse("there is no link to {file} in folder {folder}"))
+def ui_browse_stay_research_space(browser, file, folder):
+    assert len(browser.find_by_css("[data-path='/{}/{}']".format(folder, file))) == 0
