dd2df188ee2cadb8df2de102fa4722e7934bbdaf
claravox <claravox@users.noreply.github.com>
2024-05-01 11:46:31 +0200

    Yda-5661 Do not put errors in logs for large files (#421)
    If a big file has a revision_scheduled flag, do not put this as an error in the logs.

--- unit-tests/test_revisions.py
+++ unit-tests/test_revisions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Unit tests for the revision functions"""
 
-__copyright__ = 'Copyright (c) 2023, Utrecht University'
+__copyright__ = 'Copyright (c) 2023-2024, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
 import sys
@@ -10,48 +10,90 @@ from unittest import TestCase
 sys.path.append('..')
 
 from revision_strategies import get_revision_strategy
-from revision_utils import get_deletion_candidates, revision_cleanup_prefilter
+from revision_utils import get_deletion_candidates, revision_cleanup_prefilter, revision_eligible
 
 
 class RevisionTest(TestCase):
 
+    def test_revision_eligible(self):
+        # Happy flow
+        eligible, msg = revision_eligible(100, True, 2, "/zone/home/research-test/obj", [["research-initial"]], True)
+        self.assertTrue(eligible)
+        self.assertEqual(msg, "")
+
+        # Data obj does not exist
+        eligible, msg = revision_eligible(100, False, None, "/zone/home/research-test/obj", [["research-initial"]], True)
+        self.assertFalse(eligible)
+        self.assertIn("was not found", msg)
+
+        # No groups
+        eligible, msg = revision_eligible(100, True, 2, "/zone/home/research-test/obj", [], True)
+        self.assertFalse(eligible)
+        self.assertIn("Cannot find owner", msg)
+
+        # Too many groups
+        eligible, msg = revision_eligible(100, True, 2, "/zone/home/research-test/obj", [["research-initial"], ["research-initial1"]], True)
+        self.assertFalse(eligible)
+        self.assertIn("Cannot find unique owner", msg)
+
+        # No revision store
+        eligible, msg = revision_eligible(100, True, 2, "/zone/home/research-test/obj", [["research-initial"]], False)
+        self.assertFalse(eligible)
+        self.assertIn("Revision store", msg)
+        self.assertIn("does not exist", msg)
+
+        # Not in research space
+        eligible, msg = revision_eligible(100, True, "5", "/zone/home/vault-test/obj", [["research-initial"]], True)
+        self.assertFalse(eligible)
+        self.assertEqual(msg, "")
+
+        # Blocklist file
+        eligible, msg = revision_eligible(100, True, 2, "/zone/home/research-test/.DS_Store", [["research-initial"]], True)
+        self.assertFalse(eligible)
+        self.assertEqual(msg, "")
+
+        # Too large data object (not an error)
+        eligible, msg = revision_eligible(2, True, "100", "/zone/home/research-test/obj", [["research-initial"]], True)
+        self.assertFalse(eligible)
+        self.assertEqual(msg, "")
+
     def test_revision_strategy(self):
         strategy = get_revision_strategy("B")
-        self.assertEquals(len(strategy.get_buckets()), 8)
-        self.assertEquals(strategy.get_minimum_bucket_size(), 2)
+        self.assertEqual(len(strategy.get_buckets()), 8)
+        self.assertEqual(strategy.get_minimum_bucket_size(), 2)
         # Tests total length of all buckets in seconds; equivalent to roughly 29 weeks.
-        self.assertEquals(strategy.get_total_bucket_timespan(), 17755200)
+        self.assertEqual(strategy.get_total_bucket_timespan(), 17755200)
 
     def test_revision_cleanup_prefilter(self):
         empty_input = []
         empty_output = revision_cleanup_prefilter(None, empty_input, "B", False)
-        self.assertEquals(empty_output, [])
+        self.assertEqual(empty_output, [])
         single_input = [[(1, 123, "/foo/bar/baz")]]
         single_output = revision_cleanup_prefilter(None, single_input, "B", False)
-        self.assertEquals(single_output, [])  # Does not exceed min. bucket size for strategy B
+        self.assertEqual(single_output, [])  # Does not exceed min. bucket size for strategy B
         two_input = [[(1, 123, "/foo/bar/baz"), (2, 234, "/foo/bar/baz")]]
         two_output = revision_cleanup_prefilter(None, two_input, "B", False)
         # Does not exceed min. bucket size for strategy B
         # But more than 1 revision (so cannot prefilter, because
         # revisions could be outside defined buckets)
-        self.assertEquals(two_output, two_input)
+        self.assertEqual(two_output, two_input)
         three_input = [[(1, 123, "/foo/bar/baz"), (2, 234, "/foo/bar/baz"), (3, 345, "/foo/bar/baz")]]
         three_output = revision_cleanup_prefilter(None, three_input, "B", False)
-        self.assertEquals(three_output, three_input)  # Exceeds min. bucket size for strategy B
+        self.assertEqual(three_output, three_input)  # Exceeds min. bucket size for strategy B
 
     def test_revision_deletion_candidates_empty(self):
         dummy_time = 1000000000
         revision_strategy = get_revision_strategy("B")
         revisions = []
         output = get_deletion_candidates(None, revision_strategy, revisions, dummy_time, False)
-        self.assertEquals(output, [])
+        self.assertEqual(output, [])
 
     def test_revision_deletion_candidates_1_bucket_no_exceed(self):
         dummy_time = 1000000000
         revision_strategy = get_revision_strategy("B")
         revisions = [(1, dummy_time - 60, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [])
+        self.assertEqual(output, [])
 
     def test_revision_deletion_candidates_2_bucket_no_exceed(self):
         dummy_time = 1000000000
@@ -59,7 +101,7 @@ class RevisionTest(TestCase):
         revisions = [(1, dummy_time - 60, "/foo/bar/baz"),
                      (2, dummy_time - 120, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [])
+        self.assertEqual(output, [])
 
     def test_revision_deletion_candidates_2_multi_bucket_no_exceed(self):
         dummy_time = 1000000000
@@ -67,7 +109,7 @@ class RevisionTest(TestCase):
         revisions = [(1, dummy_time - 60, "/foo/bar/baz"),
                      (2, dummy_time - 13 * 3600, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [])
+        self.assertEqual(output, [])
 
     def test_revision_deletion_candidates_4_multi_bucket_no_exceed(self):
         dummy_time = 1000000000
@@ -77,7 +119,7 @@ class RevisionTest(TestCase):
                      (3, dummy_time - 3600 * 16, "/foo/bar/baz"),
                      (4, dummy_time - 3600 * 17, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [])
+        self.assertEqual(output, [])
 
     def test_revision_deletion_candidates_3_bucket_exceed(self):
         dummy_time = 1000000000
@@ -86,7 +128,7 @@ class RevisionTest(TestCase):
                      (2, dummy_time - 120, "/foo/bar/baz"),
                      (3, dummy_time - 180, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [2])
+        self.assertEqual(output, [2])
 
     def test_revision_deletion_candidates_6_buckets_exceed(self):
         dummy_time = 1000000000
@@ -98,14 +140,14 @@ class RevisionTest(TestCase):
                      (5, dummy_time - 3600 * 16 - 120, "/foo/bar/baz"),
                      (6, dummy_time - 3600 * 16 - 180, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [2, 5])
+        self.assertEqual(output, [2, 5])
 
     def test_revision_deletion_1_before_buckets(self):
         dummy_time = 1000000000
         revision_strategy = get_revision_strategy("B")
         revisions = [(1, dummy_time - 365 * 24 * 3600, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [])
+        self.assertEqual(output, [])
 
     def test_revision_deletion_1_bucket_1_before(self):
         dummy_time = 1000000000
@@ -113,7 +155,7 @@ class RevisionTest(TestCase):
         revisions = [(1, dummy_time - 60, "/foo/bar/baz"),
                      (2, dummy_time - 365 * 24 * 3600, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [2])
+        self.assertEqual(output, [2])
 
     def test_revision_deletion_1_bucket_2_before(self):
         dummy_time = 1000000000
@@ -122,7 +164,7 @@ class RevisionTest(TestCase):
                      (2, dummy_time - 365 * 24 * 3600 - 60, "/foo/bar/baz"),
                      (3, dummy_time - 365 * 24 * 3600 - 90, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [2, 3])
+        self.assertEqual(output, [2, 3])
 
     def test_revision_deletion_3_before_buckets(self):
         dummy_time = 1000000000
@@ -131,4 +173,4 @@ class RevisionTest(TestCase):
                      (2, dummy_time - 365 * 24 * 3600 - 120, "/foo/bar/baz"),
                      (3, dummy_time - 365 * 24 * 3600 - 180, "/foo/bar/baz")]
         output = get_deletion_candidates(None, revision_strategy, revisions, 1000000000, False)
-        self.assertEquals(output, [2, 3])
+        self.assertEqual(output, [2, 3])
