4e0d12bdaa35fa34aae2046719f2908aa78c3423
Sirjan <s.kaur@uu.nl>
2024-05-21 07:47:30 +0000

    YDA-5631 - Modified the repair script.

--- tools/modify-data-object.r
+++ tools/modify-data-object.r
@@ -8,32 +8,44 @@ import session_vars
 import csv
 import os.path
 
+
+def errorcode(e):
+      start = e.message.find(':')
+      stop  = e.message.find(']')
+      return  e.message[start + 1: stop]
+
+
 def data_file_exists(resc_name, data_path, callback):
     # Verify that data file exists in given data path
-    output = callback.msi_stat_vault(resc_name, data_path, "", "")
-    return output['arguments'][2], output['arguments'][3]
+    try:
+        output = callback.msi_stat_vault(resc_name, data_path, "", "")
+        type, size = output['arguments'][2], output['arguments'][3]
+    except RuntimeError as e:
+        callback.writeLine("stdout","[msi_stat_vault failed on path '{}' with error - '{}'".format(data_path, errorcode(e)))
+        return -1, "[msi_stat_vault failed on path '{}' with error - '{}'".format(data_path, errorcode(e))
 
+    return type, size
 
-def preconditions_for_data_object(data, zone, callback):
+def preconditions_for_data_object(data, callback):
 
     replicas_list = []
 
     # Check if file exists in the current data path
-    actual_file_type, actual_file_size = data_file_exists(data[1], data[3], callback)
+    actual_file_type, actual_file_size = data_file_exists(data['resc_name'], data['actual_datapath'], callback)
 
     if actual_file_type == 'NOTEXIST':
-        expected_type, expected_size = data_file_exists(data[1], data[4], callback)
+        expected_type, expected_size = data_file_exists(data['resc_name'], data['expected_datapath'], callback)
 
         if expected_type == 'FILE':
             # Count other replicas with same data path
             expected_iter = genquery.row_iterator(
             "DATA_ID, DATA_REPL_NUM, RESC_NAME, RESC_LOC", 
-            "USER_ZONE = '{}' AND DATA_PATH = '{}'".format(zone, data[4]),
+            "DATA_PATH = '{}'".format(data['expected_datapath']),
             genquery.AS_LIST,
             callback)
 
             for row in expected_iter:
-                if row[0] != data[0]:
+                if row[0] != data['data_id']:
                     replicas_list.append(row[0])
 
             if len(replicas_list) == 0:
@@ -42,7 +54,7 @@ def preconditions_for_data_object(data, zone, callback):
                 # The data file should pass compatibility check with the replica
                 actual_iter = genquery.row_iterator(
                 "DATA_SIZE, DATA_CHECKSUM, RESC_LOC",
-                "USER_ZONE = '{}' AND DATA_ID ='{}' AND DATA_REPL_NUM = '{}'".format(zone, data[0], data[2]),
+                "DATA_ID ='{}' AND DATA_REPL_NUM = '{}'".format(data['data_id'], data['data_repl_num']),
                 genquery.AS_TUPLE,
                 callback)
 
@@ -55,21 +67,32 @@ def preconditions_for_data_object(data, zone, callback):
 
                     #  Calculate checksum of data file if replica checksum is registered.
                     if actual_chksum:
-                        chksum_output = callback.msi_file_checksum(data[4], data[1], '')
-                        expected_chksum = chksum_output['arguments'][2]
+                        try:
+                            chksum_output = callback.msi_file_checksum(data['expected_datapath'], data['resc_name'], '')
+                            expected_chksum = chksum_output['arguments'][2]
+                        except RuntimeError as e:
+                            callback.writeLine("stdout","msi_file_checksum failed on path '{}' with error - '{}'".format(data['expected_datapath'], errorcode(e)))
+                            return -1, "msi_file_checksum failed on path '{}' with error - '{}'".format(data['expected_datapath'], errorcode(e))
+                    else:
+                        try:
+                            chksum_output = callback.msi_file_checksum(data['actual_datapath'], data['resc_name'], '')
+                            actual_chksum = chksum_output['arguments'][2]
+                        except RuntimeError as e:
+                            callback.writeLine("stdout","msi_file_checksum failed on path '{}' with error - '{}'".format(data['actual_datapath'], errorcode(e)))
+                            return -1, "msi_file_checksum failed on path '{}' with error - '{}'".format(data['actual_datapath'], errorcode(e))
 
-                        if expected_chksum == actual_chksum:
-                            callback.writeLine("stdout", "The checksum of replica and data file matches.")
+                    if expected_chksum == actual_chksum:
+                        callback.writeLine("stdout", "The checksum of replica and data file matches.")
 
-                            # Modify data object
-                            status = modify_data_object(data, callback)
-                            if status == '':
-                                return 0, status
-                            else:
-                                return -1, status
-
+                        # Modify data object
+                        status = modify_data_object(data, callback)
+                        if status == '':
+                            return 0, status
                         else:
-                            return -1, "preconditions_for_data_object: Checksum of data file does not match with the replica."
+                            return -1, status
+
+                    else:
+                        return -1, "preconditions_for_data_object: Checksum of data file does not match with the replica."
                 else:
                     return -1, "preconditions_for_data_object: The data file and replica are not compatible."
             else:
@@ -82,31 +105,35 @@ def preconditions_for_data_object(data, zone, callback):
 
 def modify_data_object(data, callback):
     # Modify the replica with correct data path
-    status = subprocess.check_output(['iadmin', 'modrepl', 'data_id', data[0], 'replica_number', data[2], 'DATA_PATH', data[4]], stderr=subprocess.STDOUT)
+    status = subprocess.check_output(['iadmin', 'modrepl', 'data_id', data['data_id'], 'replica_number', data['data_repl_num'], 'DATA_PATH', data['expected_datapath']], stderr=subprocess.STDOUT)
     return str(status)
 
 
 def main(rule_args, callback, rei):
-    zone = session_vars.get_map(rei)['client_user']['irods_zone']
+
     file = global_vars["*file"]
     # Read CSV
-    with open(file, 'r') as csvfile, open('/etc/irods/yoda-ruleset/tools/result.csv', 'w') as result:
-        reader = csv.reader(csvfile, delimiter=';')
-        writer = csv.writer(result, delimiter=';')
-
-        header = reader.next()
-        writer.writerow(header)
+    with open(file, 'r') as csvfile, open('/etc/irods/yoda-ruleset/tools/result_1.csv', 'w') as result:
+        reader = csv.DictReader(csvfile)
+        fieldnames = reader.fieldnames + ['message']
+        writer = csv.DictWriter(result, fieldnames)
+        writer.writeheader()
 
         for row in reader:
-            if row[5] in ('ERROR', 'NO'):
-                retCode, msg = preconditions_for_data_object(row, zone, callback)
+            row['resc_name'] = row['resc_name'].strip('\'')
+            row['expected_datapath'] = row['expected_datapath'].strip('\'')
+            row['actual_datapath'] = row['actual_datapath'].strip('\'')
+            print(row['data_id'])
+            if row['processed'] in ('ERROR', 'NO'):
+                retCode, msg = preconditions_for_data_object(row, callback)
                 if retCode == 0:
-                    row[5] = 'YES'
-                    writer.writerow(row)
+                    msg = 'Success'
+                    row['processed'] = 'YES'
+                    writer.writerow(dict(row, message=msg))
                 else:
-                    callback.writeLine("stdout", msg)
-                    row[5] = 'ERROR'
-                    writer.writerow(row)
+                    callback.writeLine("stdout", "Status: " + msg)
+                    row['processed'] = 'ERROR'
+                    writer.writerow(dict(row, message=msg))
 
-INPUT *file=/etc/irods/yoda-ruleset/tools/test.csv
+INPUT *file=/etc/irods/yoda-ruleset/tools/test_1.csv
 OUTPUT ruleExecOut 
