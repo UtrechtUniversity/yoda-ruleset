00ab68e56295ddeeb1c196986625785527955aa0
FuHsinyu <x.fu1@uu.nl>
2024-06-13 15:58:46 +0200

    improve code styles

--- admin.py
+++ admin.py
@@ -2,32 +2,33 @@
 """Functions for administration management."""
 
 __copyright__ = 'Copyright (c) 2018-2024, Utrecht University'
-__license__   = 'GPLv3, see LICENSE'
+__license__ = 'GPLv3, see LICENSE'
 
 __all__ = [
-           'api_admin_has_access',
+    'api_admin_has_access',
 ]
 
 from util import *
 
+
 @api.make()
-def api_admin_has_access(ctx, group_name = "priv-group-add"):# TODO: Update the default group
+def api_admin_has_access(ctx, group_name="priv-group-add"):  # TODO: Update the default group
     """
     Checks if the user has admin access based on user rights or membership in a specified group.
 
     Args:
         group_name (str): The name of the privilege group to check against. Defaults to "priv-group-add".
 
-    Returns:
+    Return:
         bool: True if the user has the admin access, False otherwise.
     """
     # if user has admin right
     is_admin = user.is_admin(ctx)
-    log.write(ctx, "is_admin?:"+str(is_admin))
-    log.write(ctx, "usertype is:"+str(user.user_type(ctx))) 
-    
+    log.write(ctx, "is_admin?:" + str(is_admin))
+    log.write(ctx, "usertype is:" + str(user.user_type(ctx)))
+
     # if user is in the privilege group
-    in_priv_group = user.is_member_of(ctx,group_name)
-    log.write(ctx, "in_priv_group?:"+str(in_priv_group)) 
+    in_priv_group = user.is_member_of(ctx, group_name)
+    log.write(ctx, "in_priv_group?:" + str(in_priv_group))
 
     return is_admin or in_priv_group
