c0acd08030ef8b960558233c7d958e3189bc0cb4
FuHsinyu <x.fu1@uu.nl>
2024-06-20 10:20:38 +0200

    add ui tests

--- tests/features/ui/ui_admin.feature
+++ tests/features/ui/ui_admin.feature
@@ -2,11 +2,24 @@
 Feature: Admin UI
 
     Scenario Outline: Admin page view
-        Given user technicaladmin is logged in #FIXME: User var in user
-        When user opens link to adminpage
+        Given user <user> is logged in 
+        When user opens link to admin page
         Then the text Administration is shown
-        
-#TODO: PAGE view suessful, Text administration is present? #Menu item present 
+        And Administration option is available in the menu dropdown
 
-# TODO: scenarios: dropdown button for admin user and not shown for non-admin users
-# TODO: Scenarios: text access denied is present for non-admin users
+        Examples:
+            | user                |  
+            | technicaladmin      | # Role: irodsadmin
+            | functionaladminpriv | # Group: priv-admin-group
+            | technicaladmin      | # Role: irodsadmin and Group: priv-group-add #TODO: FUnctionaladmivpriv, add user to needed group
+
+
+    Scenario Outline: Admin page view by non-admin user
+        Given user <user> is logged in 
+        When user opens link to admin page
+        Then the text Access forbidden is shown
+        And Administration option is not available in the menu dropdown
+
+        Examples:
+            | user                | 
+            | researcher          | # Role: non-admin and Group: non-priv-group
--- tests/step_defs/ui/test_ui_admin.py
+++ tests/step_defs/ui/test_ui_admin.py
@@ -1,8 +1,8 @@
 # coding=utf-8
-"""Fileviewer UI feature tests."""
+"""Admin UI feature tests."""
 
-__copyright__ = 'Copyright (c) 2024, Utrecht University'
-__license__   = 'GPLv3, see LICENSE'
+__copyright__ = "Copyright (c) 2024, Utrecht University"
+__license__ = "GPLv3, see LICENSE"
 
 from pytest_bdd import (
     parsers,
@@ -10,18 +10,40 @@ from pytest_bdd import (
     then,
     when,
 )
-
+import time
 from conftest import portal_url
 
-scenarios('../../features/ui/ui_admin.feature')
+scenarios("../../features/ui/ui_admin.feature")
 
 
-@when(parsers.parse('user opens link to admin page'))
+@when(parsers.parse("user opens link to admin page"))
 def ui_admin_access(browser):
     url = "{}/admin".format(portal_url)
+    # time.sleep(10)  # FIXME:In case of slow VM machine
     browser.visit(url)
 
-@then('the text Administration is shown')
+
+@then("the text Administration is shown")
 def ui_admin_administration_present(browser):
-    print("browser HTML", browser.html)
-    assert browser.is_text_present('administration')
+    h1_tags = browser.find_by_tag("h1")  # Avoid finding the one in dropdown
+    found = any("administration" in h1.text.lower() for h1 in h1_tags)
+    assert found
+
+
+@then("Administration option is available in the menu dropdown")
+def ui_admin_administration_dropdown_present(browser):
+    xpath = "//a[@class='dropdown-item' and @href='/admin/']"
+    found = browser.is_element_present_by_xpath(xpath)
+    assert found
+
+
+@then("Administration option is not available in the menu dropdown")
+def ui_admin_administration_dropdown_not_present(browser):
+    xpath = "//a[@class='dropdown-item' and @href='/admin/']"
+    not_found = browser.is_element_not_present_by_xpath(xpath)
+    assert not_found
+
+
+@then("the text Access forbidden is shown")
+def ui_admin_access_forbidden(browser):
+    assert browser.is_text_present("Access forbidden")
