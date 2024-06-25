bc09a1000f926807993179fb22baf904a0656b8e
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-06-25 08:41:56 +0200

    Update module description

--- tests/step_defs/ui/test_ui_data_transfer.py
+++ tests/step_defs/ui/test_ui_data_transfer.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-"""Settings UI feature tests."""
+"""Data Transfer UI feature tests."""
 
 __copyright__ = 'Copyright (c) 2024, Utrecht University'
 __license__ = 'GPLv3, see LICENSE'
