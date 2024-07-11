58af902a4e85519b947927f714e50c72993e13f0
Dylan Hsin <x.fu1@uu.nl>
2024-07-11 12:57:41 +0200

    YDA-5718: add maintenance banner's UI tests

--- tests/step_defs/ui/test_ui_admin.py
+++ tests/step_defs/ui/test_ui_admin.py
@@ -5,6 +5,7 @@ __copyright__ = "Copyright (c) 2024, Utrecht University"
 __license__ = "GPLv3, see LICENSE"
 
 from pytest_bdd import (
+    given,
     parsers,
     scenarios,
     then,
@@ -16,10 +17,43 @@ from conftest import portal_url
 scenarios("../../features/ui/ui_admin.feature")
 
 
-@when(parsers.parse("user opens link to admin page"))
+@given(parsers.parse("the banner display the message {message}"))
+@then(parsers.parse("the banner display the message {message}"))
+def ui_admin_display_banner(browser, message):
+    assert browser.is_element_present_by_name('banner head')
+    assert browser.is_text_present(message)
+
+
+@when("the user navigates to the home page")
+def ui_admin_home_access(browser):
+    url = "{}/".format(portal_url)
+    browser.visit(url)
+
+
+@when("the user navigates to the admin page")
 def ui_admin_access(browser):
     url = "{}/admin".format(portal_url)
     browser.visit(url)
+
+
+@when(parsers.parse("the user input banner text with message {message}"))
+def ui_admin_input_banner_msg(browser, message):
+    browser.fill("banner", message)
+
+
+@when(parsers.parse("the user {action} the checkbox to mark the banner as important"))
+def ui_admin_control_importance(browser, action):
+    if action == "checks":
+        browser.check("importance")
+    elif action == "unchecks":
+        browser.uncheck("importance")
+    else:
+        raise ValueError("Unsupported action.")
+
+
+@when(parsers.parse("the user click button {button}"))
+def ui_admin_click_button(browser, button):
+    browser.find_by_name(button).first.click()
 
 
 @then("the text Administration is shown")
@@ -46,3 +80,24 @@ def ui_admin_administration_dropdown_not_present(browser):
 @then("the text Access forbidden is shown")
 def ui_admin_access_forbidden(browser):
     assert browser.is_text_present("Access forbidden")
+
+
+@then("the banner setup option should be visible")
+def ui_admin_banner_option_prensent(browser):
+    assert browser.is_text_present("Set maintenance banner"), "Banner title not found on the page"
+    assert browser.find_by_name("banner").visible, "Textarea for banner message not found on the page"
+    assert browser.find_by_id("importance").visible, "Checkbox for 'Mark as Important' not found on the page"
+    assert browser.find_by_css("button[name='Set Banner']").visible, "Button to set the banner not found on the page"
+    assert browser.find_by_css("button[name='Remove Banner']").visible, "Button to remove the banner not found on the page"
+
+
+@then("the banner does not exist")
+def ui_admin_remove_banner(browser):
+    assert browser.is_element_not_present_by_name('banner head')
+
+
+@then(parsers.parse("the banner background color should be {color}"))
+def ui_admin_display_banner_color(browser, color):
+    element = browser.find_by_css('div.non-production[role="alert"]').first
+    is_color_present = color in element['class'].split()
+    assert is_color_present
