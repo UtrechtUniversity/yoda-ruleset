b38b62b841d899daec240a74dd7dee796d5d9fcc
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-07-12 13:22:31 +0200

    YDA-5790: move file checksum wrapper to uuFunctions

--- uuFunctions.r
+++ uuFunctions.r
@@ -92,3 +92,24 @@ uuReplicateBatch(*verbose, *balance_id_min, *balance_id_max, *batch_size_limit, 
 msiSendMail(*xtoAddr,*xsubjectLine,*xbody){
     writeLine('serverLog','WARNING: msiSendMail is disabled');
 }
+
+#
+# Wrapper of file checksum microservice to handle host delegation
+#
+wrap_msi_file_checksum(*file, *resc, *sum) {
+    *host = "";
+    *result = "";
+
+    foreach (*row in select RESC_LOC where RESC_NAME = *resc) {
+        *host = *row.RESC_LOC;
+    }
+    if (*host == "") {
+        *result = "-1";
+        writeLine("serverLog","Could not find resource location for *resc when invoking file checksum microservice. Resource probably does not exist.");
+    } else {
+        remote(*host, "null") {
+            *result = errorcode(msi.file_checksum(*file, *resc, *sum));
+        }
+    }
+    *result;
+}
