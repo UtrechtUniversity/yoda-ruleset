716c58a9d02e079290b22b144e01e2a5cdeacec4
Dylan Hsin <x.fu1@uu.nl>
2024-07-23 16:24:55 +0200

    remove enable_tape

--- util/config.py
+++ util/config.py
@@ -72,7 +72,6 @@ class Config(object):
 
 # Note: Must name all valid config items.
 config = Config(environment=None,
-                enable_tape_archive=False,  # TODO: Remove later
                 default_yoda_schema=None,
                 resource_primary=[],
                 resource_trigger_pol=[],
