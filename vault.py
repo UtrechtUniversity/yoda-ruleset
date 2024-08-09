dadf47d1c81171ba8d8ee97c7a5eb0027f853c6f
kaur16 <126662478+kaur16@users.noreply.github.com>
2024-08-09 13:20:42 +0200

    YDA-5808: add all the versions of data package to vault space
    Co-authored-by: Lazlo Westerhof <l.r.westerhof@uu.nl>

--- vault.py
+++ vault.py
@@ -485,22 +485,6 @@ def api_vault_system_metadata(ctx, coll):
         landinpage_url = row[0]
         system_metadata["Landingpage"] = "<a href=\"{}\">{}</a>".format(landinpage_url, landinpage_url)
 
-    # Check for previous version.
-    previous_version = get_previous_version(ctx, coll)
-    if previous_version:
-        previous_version_doi = get_doi(ctx, previous_version)
-        system_metadata["Persistent Identifier DOI"] = persistent_identifier_doi = "previous version: <a href=\"https://doi.org/{}\">{}</a>".format(previous_version_doi, previous_version_doi)
-
-    # Persistent Identifier DOI.
-    package_doi = get_doi(ctx, coll)
-
-    if package_doi:
-        if previous_version:
-            persistent_identifier_doi = "<a href=\"https://doi.org/{}\">{}</a> (previous version: <a href=\"https://doi.org/{}\">{}</a>)".format(package_doi, package_doi, previous_version_doi, previous_version_doi)
-        else:
-            persistent_identifier_doi = "<a href=\"https://doi.org/{}\">{}</a>".format(package_doi, package_doi)
-        system_metadata["Persistent Identifier DOI"] = persistent_identifier_doi
-
     # Data Package Reference.
     data_package_reference = ""
     iter = genquery.row_iterator(
@@ -561,6 +545,46 @@ def get_coll_vault_status(ctx, path, org_metadata=None):
     return constants.vault_package_state.EMPTY
 
 
+def get_all_published_versions(ctx, path):
+    """Get all published versions of a data package."""
+    base_doi = get_doi(ctx, path, 'base')
+    package_doi = get_doi(ctx, path)
+    coll_parent_name = path.rsplit('/', 1)[0]
+
+    org_publ_info, data_packages, grouped_base_dois = get_all_doi_versions(ctx, coll_parent_name)
+
+    count = 0
+    all_versions = []
+
+    for data in data_packages:
+        if data[2] == package_doi:
+            count += 1
+
+    if count == 1:  # Base DOI does not exist as it is first version of the publication
+        # Convert the date into two formats for display and tooltip (Jan 1, 1990 and 1990-01-01 00:00:00)
+        data_packages = [[x[0], datetime.strptime(x[1], "%Y-%m-%dT%H:%M:%S.%f").strftime("%b %d, %Y"), x[2],
+                          datetime.strptime(x[1], "%Y-%m-%dT%H:%M:%S.%f").strftime('%Y-%m-%d %H:%M:%S%z'), x[3]] for x in data_packages]
+
+        for item in data_packages:
+            if item[2] == package_doi:
+                all_versions.append([item[1], item[2], item[3]])
+    else:  # Base DOI exists
+        # Sort by publication date
+        sorted_publ = [sorted(x, key=lambda x: datetime.strptime(x[1], "%Y-%m-%dT%H:%M:%S.%f"), reverse=True) for x in grouped_base_dois]
+
+        sorted_publ = [element for innerList in sorted_publ for element in innerList]
+
+        # Convert the date into two formats for display and tooltip (Jan 1, 1990 and 1990-01-01 00:00:00)
+        sorted_publ = [[x[0], datetime.strptime(x[1], "%Y-%m-%dT%H:%M:%S.%f").strftime("%b %d, %Y"), x[2],
+                        datetime.strptime(x[1], "%Y-%m-%dT%H:%M:%S.%f").strftime('%Y-%m-%d %H:%M:%S%z'), x[3]] for x in sorted_publ]
+
+        for item in sorted_publ:
+            if item[0] == base_doi:
+                all_versions.append([item[1], item[2], item[3]])
+
+    return base_doi, package_doi, all_versions
+
+
 @api.make()
 def api_vault_collection_details(ctx, path):
     """Return details of a vault collection.
@@ -605,6 +629,8 @@ def api_vault_collection_details(ctx, path):
         return {'member_type': member_type, 'is_datamanager': is_datamanager}
     else:
         metadata = True
+        # Retreive all published versions
+        base_doi, package_doi, all_versions = get_all_published_versions(ctx, path)
 
     # Check if a vault action is pending.
     vault_action_pending = False
@@ -654,7 +680,10 @@ def api_vault_collection_details(ctx, path):
         "has_datamanager": has_datamanager,
         "is_datamanager": is_datamanager,
         "vault_action_pending": vault_action_pending,
-        "research_group_access": research_group_access
+        "research_group_access": research_group_access,
+        "all_versions": all_versions,
+        "base_doi": base_doi,
+        "package_doi": package_doi
     }
     if config.enable_data_package_archive:
         import vault_archive
@@ -1467,17 +1496,21 @@ def get_approver(ctx, path):
         return None
 
 
-def get_doi(ctx, path):
+def get_doi(ctx, path, doi='version'):
     """Get the DOI of a data package in the vault.
 
     :param ctx:  Combined type of a callback and rei struct
     :param path: Vault package to get the DOI of
+    :param doi: 'base' or 'version' to retrieve required DOI
 
     :return: Data package DOI or None
     """
+    if doi != 'base':
+        doi = 'version'
+
     iter = genquery.row_iterator(
         "META_COLL_ATTR_VALUE",
-        "COLL_NAME = '%s' AND META_COLL_ATTR_NAME = 'org_publication_versionDOI'" % (path),
+        "COLL_NAME = '{}' AND META_COLL_ATTR_NAME = 'org_publication_{}DOI'".format(path, doi),
         genquery.AS_LIST, ctx
     )
 
