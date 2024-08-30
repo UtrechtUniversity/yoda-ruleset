ae0cfc92f526c257f04ecd7e4a71df3be2e0b65e
FuHsinyu <x.fu1@uu.nl>
2024-08-30 14:56:22 +0200

    remove unused files

--- tools/troubleshoot-vault-data-packages1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Command to execute
-# TODO: Add option to trouble shoot a single data package
-# TODO: Modify original codes 
-irule -r irods_rule_engine_plugin-python-instance -F metadata/vault-metadata-schema-report.r
