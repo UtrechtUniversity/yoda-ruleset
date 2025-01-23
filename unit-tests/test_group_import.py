180ef2aa1ff38975ef3054fc12636ea26a8bfbb3
leonidastri <l.triantafyllou@uu.nl>
2025-01-23 16:43:17 +0100

    feat: csv parse commas test

--- unit-tests/test_group_import.py
+++ unit-tests/test_group_import.py
@@ -221,3 +221,15 @@ class GroupImportTest(TestCase):
         no_duplicate_data, no_duplicate_err = self.parse_csv_file("files/without-duplicates2.csv")
         self.assertNotEqual(no_duplicate_data, [])
         self.assertEqual(no_duplicate_err, '')
+
+
+    def test_parse_csv_file_commas(self):
+        # CSV file with commas
+        commas_data, commas_err = self.parse_csv_file("files/with-commas.csv")
+        self.assertEqual(commas_data, [])
+        self.assertIn("Data error", commas_err)
+
+         # CSV file without duplicates
+        no_commas_data, no_commas_err = self.parse_csv_file("files/without-commas.csv")
+        self.assertNotEqual(no_commas_data, [])
+        self.assertEqual(no_commas_err, '') 
