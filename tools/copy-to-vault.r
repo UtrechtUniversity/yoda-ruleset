38bfd33eef138a0f686863abc71a08b53fab9f3c
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-02-24 12:44:56 +0100

    YDA-6208: use the same copy to vault route for both new and retry packages

--- tools/copy-to-vault.r
+++ tools/copy-to-vault.r
@@ -1,8 +1,10 @@
-retryCopyToVault {
+#!/usr/bin/irule -F
+
+copyToVault {
 	# Try to copy accepted and retry research folders to vault.
 	# This script is kept as dumb as possible.
-	# All processing and error handling is done by rule_vault_retry_copy_to_vault
-	rule_vault_retry_copy_to_vault();
+	# All processing and error handling is done by rule_vault_copy_to_vault
+	rule_vault_copy_to_vault();
 }
 input null
 output ruleExecOut
