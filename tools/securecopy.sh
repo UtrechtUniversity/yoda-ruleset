8a87637ffed02ff80f1d2b715857167e9cebb21c
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-03-07 14:27:28 +0100

    Github Actions: add shellcheck workflow

--- tools/securecopy.sh
+++ tools/securecopy.sh
@@ -4,11 +4,11 @@ PHYPATH=$1
 HOST=$2
 USER=$3
 DESTINATION=$4
-DESTDIR=$(dirname $DESTINATION)
+DESTDIR=$(dirname "$DESTINATION")
 echo "Calling:"
 echo "ssh $USER@$HOST mkdir -p $DESTDIR"
-timeout 5s ssh $USER@$HOST mkdir -p $DESTDIR
+timeout 5s ssh "$USER"@"$HOST" mkdir -p "$DESTDIR"
 echo "scp -i $HOME/.ssh/$4 $PHYPATH $USER@$HOST:$DESTINATION"
-timeout 5s scp $PHYPATH $USER@$HOST:$DESTINATION
+timeout 5s scp "$PHYPATH" "$USER"@"$HOST":"$DESTINATION"
 echo "ssh $USER@$HOST chmod go+r $DESTINATION"
-timeout 5s ssh $USER@$HOST chmod go+r $DESTINATION
+timeout 5s ssh "$USER"@"$HOST" chmod go+r "$DESTINATION"
