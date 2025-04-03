928d60c52aef63b9d65399a6eee65be70aedf776
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-04-03 13:45:07 +0200

    YDA-6231: check for read and modify access separate before setting ACLs

--- folder.py
+++ folder.py
@@ -357,12 +357,16 @@ def set_last_run_time(ctx: rule.Context, coll: str) -> bool:
 
 def set_can_modify(ctx: rule.Context, coll: str) -> bool:
     """Check if have permission to modify, set if necessary"""
-    check_access_result = msi.check_access(ctx, coll, 'modify_object', irods_types.BytesBuf())
-    modify_access = check_access_result['arguments'][2]
-    if modify_access != b'\x01':
+    check_access_result = msi.check_access(ctx, coll, 'read_object', irods_types.BytesBuf())
+    read_access = check_access_result['arguments'][2]
+    if read_access != b'\x01':
         # This allows us permission to copy the files
         if not set_acl_check(ctx, "recursive", "admin:read", coll, "Could not set ACL (admin:read) for collection: {}".format(coll)):
             return False
+
+    check_access_result = msi.check_access(ctx, coll, 'modify_object', irods_types.BytesBuf())
+    modify_access = check_access_result['arguments'][2]
+    if modify_access != b'\x01':
         # This allows us permission to set AVUs
         if not set_acl_check(ctx, "default", "admin:write", coll, "Could not set ACL (admin:write) for collection: {}".format(coll)):
             return False
