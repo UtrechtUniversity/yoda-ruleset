567075b0b3fcf645fa3eca19ee636b5d3b77e185
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-06-02 13:56:04 +0200

    YDA-6287: encode landingpage open access link

--- json_landing_page.py
+++ json_landing_page.py
@@ -3,6 +3,7 @@
 __copyright__ = 'Copyright (c) 2019-2025, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
+import urllib.parse
 from datetime import datetime
 from typing import Dict
 
@@ -195,6 +196,7 @@ def json_landing_page_create_json_landing_page(ctx: rule.Context,
     ############################################################################
     persistent_identifier_datapackage = json_data["System"]["Persistent_Identifier_Datapackage"]
     open_access_link = json_data["System"].get("Open_access_Link", "")
+    open_access_link = urllib.parse.quote(open_access_link, safe=":/?&=")
     license_uri = json_data["System"].get("License_URI", "")
 
     # Format last modified and publication date.
