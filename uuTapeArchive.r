70962797baae871c5a4ea1ae2c60e3b8d2b36dd6
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-06-16 13:46:17 +0200

    Update DA commands after migration (wip)

--- uuTapeArchive.r
+++ uuTapeArchive.r
@@ -1,47 +1,32 @@
 # \file      uuTapeArchive.r
 # \author    Lazlo Westerhof
-# \copyright Copyright (c) 2021-2024, Utrecht University. All rights reserved.
+# \copyright Copyright (c) 2021-2025, Utrecht University. All rights reserved.
 # \license   GPLv3, see LICENSE.
 
-# \brief Perform dmput command.
+# \brief Perform daget command.
 #
-# \param[in] data Physical path of data object.
-# \param[in] dmfs Current DMF state of data object.
+# \param[in] data        Physical path of data object
+# \param[in] hostAddress Host where to execute command
 #
-dmput(*data, *hostAddress, *dmfs) {
-    #if (*dmfs not like "DUL" && *dmfs not like "OFL" && *dmfs not like "UNM" && *dmfs not like "MIG") {
-        msiExecCmd("dmput", *data, *hostAddress, "", "", *dmRes);
-        msiGetStdoutInExecCmdOut(*dmRes, *dmStat);
-        writeString("serverLog", "DEBUG: $userNameClient:$clientAddr - Archive dmput started: *data. Returned Status - *dmStat.");
-    #}
+daget(*data, *hostAddress) {
+    msiExecCmd("daget", *data, *hostAddress, "", "", *daRes);
+    msiGetStdoutInExecCmdOut(*daRes, *dmStat);
+    writeString("serverLog", "DEBUG: $userNameClient:$clientAddr - Archive daget started: *data. Returned Status - *dmStat.");
 }
 
 
-# \brief Perform dmget command.
+# \brief Perform daattr command.
 #
-# \param[in] data Physical path of data object.
-# \param[in] dmfs Current DMF state of data object.
+# \param[in]  data        Physical path of data object
+# \param[in]  hostAddress Host where to execute command
+# \param[out] state       Current DA state of data object
 #
-dmget(*data, *hostAddress, *dmfs) {
-    #if (*dmfs not like "DUL" && *dmfs not like "REG" && *dmfs not like "UNM" && *dmfs not like "MIG") {
-        msiExecCmd("dmget", *data, *hostAddress, "", "", *dmRes);
-        msiGetStdoutInExecCmdOut(*dmRes, *dmStat);
-        writeString("serverLog", "DEBUG: $userNameClient:$clientAddr - Archive dmget started: *data. Returned Status - *dmStat.");
-    #}
-}
+daattr(*data, *hostAddress, *state) {
+    msiExecCmd("daattr", *data, *hostAddress, "", "", *daRes);
+    msiGetStdoutInExecCmdOut(*daRes, *state);
+    *state = trimr(*state, "\n");
 
-
-# \brief Perform dmattr command.
-#
-# \param[in]  data Physical path of data object.
-# \param[out] dmfs Current DMF state of data object.
-#
-dmattr(*data, *hostAddress, *dmfs) {
-    msiExecCmd("dmattr", *data, *hostAddress, "", "", *dmRes);
-    msiGetStdoutInExecCmdOut(*dmRes, *dmfs);
-    *dmfs = trimr(*dmfs, "\n");
-
-    if (*dmfs like "") {
-        *dmfs = "INV";
+    if (*state like "") {
+        *state = "INV";
     }
 }
