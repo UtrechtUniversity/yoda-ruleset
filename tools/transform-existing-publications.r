39c54a238fc2b9c82b40e4cf3cf3b604a4d6a2a2
kaur16 <126662478+kaur16@users.noreply.github.com>
2025-09-24 14:59:12 +0200

    YDA-6580: include baseDOIMinted changes in transform-existing-publications

--- tools/transform-existing-publications.r
+++ tools/transform-existing-publications.r
@@ -1,36 +1,147 @@
 #!/usr/bin/irule -r irods_rule_engine_plugin-python-instance -F
 #
 # Transform existing publications according to the new changes in the publication process.
-# This script handles converting all the prefixes from yoda to version. 
-# Additionally, this script will add prefix version to DOIAvailable and DOI Minted variables.
 #
+# Parameters:
+# modify_prefix:        If True, this script will convert all the prefixes from yoda to version
+#                       and add prefix version to DOIAvailable and DOI Minted variables.
+# modify_basedoiminted: If True, this script will copy baseDOIMinted attribute value from new version
+#                       to previous version of data package.
+# 
+#
+# irule -r irods_rule_engine_plugin-python-instance -F /etc/irods/yoda-ruleset/tools/transform-existing-publications.r 
+# '*modify_prefix=False' '*modify_basedoiminted=True'
 # 
 import subprocess
 import genquery
 import session_vars
+import constants
 
-def main(rule_args, callback, rei):
-    zone = session_vars.get_map(rei)['client_user']['irods_zone']
+def prefix_transformation(zone, callback):
+    """Replace 'yoda' prefix with 'version'. Add 'version' prefix to
+    DOIAvailable and DOIMinted AVUs.
+
+    :param zone:          iRODS zone
+    :param callback:      iRODS callback
+    """
+    org_prefix = constants.UUORGMETADATAPREFIX
 
     # Changing yoda prefix -> version
-    iter = genquery.row_iterator(
+    change_prefix = genquery.row_iterator(
         "COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE",
-        "COLL_ZONE_NAME = '{}' AND META_COLL_ATTR_NAME LIKE 'org_publication_yoda%'".format(zone),
+        "COLL_ZONE_NAME = '{}' AND META_COLL_ATTR_NAME LIKE '{}publication_yoda%'".format(zone, org_prefix),
         genquery.AS_TUPLE,
         callback) 
 
-    iter2 = genquery.row_iterator(
+    # Add 'version' prefix to DOIAvailable and DOIMinted AVUs
+    add_prefix = genquery.row_iterator(
         "COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE",
-        "COLL_ZONE_NAME = '{}' AND META_COLL_ATTR_NAME in ('org_publication_DOIAvailable', 'org_publication_DOIMinted')".format(zone),
+        "COLL_ZONE_NAME = '{}' AND META_COLL_ATTR_NAME in ('{p}publication_DOIAvailable', '{p}publication_DOIMinted')".format(zone, p=org_prefix),
         genquery.AS_TUPLE,
         callback) 
 
-    for row in iter:
+    for row in change_prefix:
+        callback.writeLine("stdout", "Changing 'yoda' prefix to 'version' for collection: {}".format(row[0]))
         subprocess.call(["imeta", "mod", "-C", row[0], row[1], row[2], "n:{}".format(row[1].replace("yoda", "version")), "v:{}".format(row[2])])
 
-    for row in iter2:
+    for row in add_prefix:
+        callback.writeLine("stdout", "Adding 'version' prefix to DOIAvailable and DOIMinted AVUs for collection: {}".format(row[0]))
         attr_name = row[1].rsplit('_', 1)[0] + "_version" + row[1].split('_')[-1]
         subprocess.call(["imeta", "mod", "-C", row[0], row[1], row[2], "n:{}".format(attr_name), "v:{}".format(row[2])])
 
-INPUT null
+
+def datapackages_and_versions(zone, callback):
+    """Get published data packages and their newer version stored in metadata. 
+
+    :param zone:     iRODS zone
+    :param callback: iRODS callback
+
+    :returns: List of relevant data packages
+    """
+    package_list = []
+    
+    datapackages = genquery.row_iterator(
+        "COLL_NAME, META_COLL_ATTR_VALUE",
+        "COLL_ZONE_NAME = '{}' AND META_COLL_ATTR_NAME = '{}publication_next_version'".format(zone, constants.UUORGMETADATAPREFIX),
+        genquery.AS_LIST,
+        callback) 
+
+    # Create list with placeholders for baseDOIMinted values
+    for row in datapackages:
+        package_list.append([row[0], "", row[1], ""])
+
+    return package_list
+
+
+def basedoiminted_value(zone, package, callback):
+    """ Get baseDOIMinted value from the metadata of a data package.
+
+    :param zone:     iRODS zone
+    :param package:  Data package to get baseDOIMinted from
+    :param callback: iRODS callback
+
+    :returns: Value of baseDOIMinted attribute in the metadata
+    """
+    value = ""
+
+    basedoiminted = genquery.row_iterator(
+        "COLL_NAME, META_COLL_ATTR_VALUE",
+        "COLL_ZONE_NAME = '{}' AND META_COLL_ATTR_NAME = '{}publication_baseDOIMinted' AND COLL_NAME = '{}'".format(zone, constants.UUORGMETADATAPREFIX, package),
+        genquery.AS_LIST,
+        callback)
+
+    for row in basedoiminted:
+        value = row[1]
+
+    return value
+
+
+def basedoiminted_correction(zone, list, callback):
+    """ Get the baseDOIMinted attribute and value from previous and new version 
+    of data package. If the previous version does not have baseDOIMinted, get the 
+    value from new version and add it to metadata.
+
+    :param zone:     iRODS zone
+    :param list:     List of published data packages with their newer versions
+    :param callback: iRODS callback
+
+    :returns: List of data packages that do not have baseDOIMinted value
+    """
+
+    transformed_data_packages = []
+
+    for row in list:
+        # Get baseDOIMinted value from previous version
+        row[1] = basedoiminted_value(zone, row[0], callback)
+
+        # Get baseDOIMinted value from new version
+        row[3] = basedoiminted_value(zone, row[2], callback)
+
+        if row[1] == '':
+            callback.writeLine("stdout", "Modify baseDOIMinted attribute for collection: {}".format(row[0]))
+            try:                
+                subprocess.call(["imeta", "add", "-C", row[0], "{}publication_baseDOIMinted".format(constants.UUORGMETADATAPREFIX), row[3]])
+                transformed_data_packages.append(row[0])
+            except Exception as e:
+                callback.writeLine("stdout", "modify_basedoiminted: Error while adding baseDOIMinted to metadata for collection: {} - {}".format(row[0], e))
+
+
+def main(rule_args, callback, rei):
+    modify_prefix = global_vars["*modify_prefix"]
+    modify_basedoiminted = global_vars["*modify_basedoiminted"]
+
+    zone = session_vars.get_map(rei)['client_user']['irods_zone']
+
+    if modify_prefix == 'True':
+        try:
+            prefix_transformation(zone, callback)
+        except Exception as e:
+            callback.writeLine("stdout", "prefix_transformation: Error encountered while modifying prefix for existing publications - {}".format(e))
+
+    if modify_basedoiminted == 'True':
+        datapackages = datapackages_and_versions(zone, callback)
+        basedoiminted_correction(zone, datapackages, callback)
+
+
+INPUT *modify_prefix=False, *modify_basedoiminted=True
 OUTPUT ruleExecOut
