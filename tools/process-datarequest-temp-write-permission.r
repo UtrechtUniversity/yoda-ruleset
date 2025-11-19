1ca2fae9dc175e3f0a6ba525c4755904379cac79
Lazlo Westerhof <l.r.westerhof@uu.nl>
2025-11-19 11:51:15 +0100

    YDA-6667: run msiSetAcl in admin mode for datarequest script

--- tools/process-datarequest-temp-write-permission.r
+++ tools/process-datarequest-temp-write-permission.r
@@ -1,18 +1,25 @@
+#!/usr/bin/irule -r irods_rule_engine_plugin-irods_rule_language-instance -F
 processTempWritePermission() {
+    uuGetUserType("$userNameClient#$rodsZoneClient", *usertype);
 
-	if (*permission == "grant") {
-		msiSetACL("default", "write", *user, *path);
-        } else if (*permission == "grantread") {
-                msiSetACL("default", "read", *user, *path);
-        } else if (*permission == "own") {
-                msiSetACL("default", "own", *user, *path);
-	} else if (*permission == "revoke") {
-		msiSetACL("default", "null", *user, *path);
-	} else {
-		writeLine("stdout", "processTempWritePermission: invalid permission value");
-		*status = "InternalError";
-		*statusInfo = "";
-	}
+    if (*usertype != "rodsadmin") {
+        failmsg(-1, "This script needs to be run by a rodsadmin");
+    }
+
+    if (*permission == "grant") {
+        msiSetACL("default", "admin:write", *user, *path);
+    } else if (*permission == "grantread") {
+        msiSetACL("default", "admin:read", *user, *path);
+    } else if (*permission == "own") {
+        msiSetACL("default", "admin:own", *user, *path);
+    } else if (*permission == "revoke") {
+        msiSetACL("default", "admin:null", *user, *path);
+    } else {
+        writeLine("stdout", "processTempWritePermission: invalid permission value");
+        *status = "InternalError";
+        *statusInfo = "";
+    }
 }
+
 input *actor="", *path="", *permission="", *user=""
 output ruleExecOut
