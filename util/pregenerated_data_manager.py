a12accecf580352eb4601f2a13f03bece39e30e2
Sietse Snel <s.t.snel@uu.nl>
2026-03-06 12:44:15 +0100

    YDA-6205 pregenerate statistics export
    Pregenerate the export data of the statistics module, because
    it takes too long to generate on large environments to do this
    synchronously.

---
+++ util/pregenerated_data_manager.py
@@ -0,0 +1,38 @@
+"""This file contains functions for storing and retrieving local
+   pregenerated data in Yoda."""
+
+__copyright__ = 'Copyright (c) 2019-2026, Utrecht University'
+__license__   = 'GPLv3, see LICENSE'
+
+import json
+import os.path
+from typing import Dict, Union
+
+from config import config
+
+
+def _get_file_path(name: str) -> str:
+    if "/" in name or "." in name:
+        raise Exception("Pregenerated data file name must not contain dots or slashes.")
+    return os.path.join(config.pregenerated_data_dir, name)
+
+
+def pregenerated_data_load(name: str) -> Union[Dict, None]:
+    """ Loads pregenerated data item
+
+        :param name: name of the data item
+
+        :returns: either a dictionary of data, or None if the item is not found
+    """
+    file_path = _get_file_path(name)
+    if os.path.exists(file_path):
+        with open(file_path, "r") as input:
+            return json.load(input)
+    else:
+        return None
+
+
+def pregenerated_data_save(name: str, data: Dict) -> None:
+    file_path = _get_file_path(name)
+    with open(file_path, "w") as output:
+        json.dump(data, output, ensure_ascii=False, indent=4)
