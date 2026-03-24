31defe62271b8cd7f6e294c5456b9cf85e427ea0
Lazlo Westerhof <l.r.westerhof@uu.nl>
2026-03-24 09:26:01 +0100

    YDA-5806: sync folder rename checks and error messages with new folder name

--- research.py
+++ research.py
@@ -231,45 +231,52 @@ def api_research_folder_rename(ctx: rule.Context, new_folder_name: str, coll: st
         return error_response
 
     try:
-        validate_filepath(coll_target)
-    except ValidationError:
-        return api.Error('invalid_foldername', 'This is not a valid folder name. Please choose another name for your folder')
+        validate_filepath(coll_target, max_len=1024)
+    except ValidationError as ve:
+        if ve.reason.name == "INVALID_LENGTH":
+            return api.Error('invalid_foldername', 'New folder path is too long. Please choose a shorter name for your new folder.')
+        else:
+            return api.Error('invalid_foldername', 'New folder path is not valid. Please choose another name for your new folder.')
+
+    # path must not contain traversal sequences
+    if coll_target.startswith('./') or coll_target.startswith('../') or '/./' in coll_target or '/../' in coll_target:
+        return api.Error('invalid_foldername', 'New folder path contains traversal sequences ./ or ../ which are not allowed.')
 
     # Same name makes no sense
     if new_folder_name == org_folder_name:
-        return api.Error('invalid_foldername', 'Origin and target folder names are equal. Please choose another name')
+        return api.Error('invalid_foldername', 'Origin and target folder names are equal. Please choose another name.')
 
     # not in home - a groupname must be present ie at least 2!?
     if not len(coll.split('/')) > 2:
-        return api.Error('invalid_destination', 'It is not possible to add folder ' + org_folder_name + ' at this location')
+        return api.Error('invalid_destination', 'It is not possible to add folder \'' + new_folder_name + '\' at this location.')
 
     # in vault?
     target_group_name = coll_target.split('/')[3]
     if target_group_name.startswith('vault-'):
-        return api.Error('not_allowed', 'It is not possible to rename folders in the vault')
+        return api.Error('not_allowed', 'It is not possible to rename folders in the vault.')
 
     # permissions ok for group?
     user_full_name = user.full_name(ctx)
     if groups.user_role(ctx, user_full_name, target_group_name) in ['none', 'reader']:
-        return api.Error('not_allowed', 'You do not have sufficient permissions to rename the selected folder')
+        return api.Error('not_allowed', 'You do not have sufficient permissions to rename the selected folder.')
 
     # Collection exists?
     if not collection.exists(ctx, coll):
-        return api.Error('invalid_foldername', 'The selected folder does not exist')
+        return api.Error('invalid_foldername', 'The current folder does not exist.')
 
     # Folder not locked?
     if folder.is_locked(ctx, coll):
-        return api.Error('not_allowed', 'The indicated folder is locked and therefore can not be renamed')
+        return api.Error('not_allowed', 'The current folder is locked and therefore can not be renamed.')
 
     # new collection exists?
     if collection.exists(ctx, coll_target):
-        return api.Error('invalid_foldername', 'The folder already exists. Please choose another name')
+        return api.Error('invalid_foldername', 'The folder already exists. Please choose another name.')
 
     # All requirements OK
     try:
         collection.rename(ctx, coll + '/' + org_folder_name, coll_target)
     except msi.Error:
-        return api.Error('internal', 'Something went wrong. Please try again')
+        return api.Error('internal', 'Something went wrong. Please try again.')
 
     return api.Result.ok()
 
