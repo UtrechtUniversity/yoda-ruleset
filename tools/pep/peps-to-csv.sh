30d1888e287d465beb4ed8a40c3b1080e44d93ba
Lazlo Westerhof <l.r.westerhof@uu.nl>
2026-04-02 11:38:28 +0200

    Update PEP prototypes for iRODS 5

--- tools/pep/peps-to-csv.sh
+++ tools/pep/peps-to-csv.sh
@@ -3,7 +3,7 @@
 # usage: peps-to-csv.sh dynamic_peps.json
 #
 # dynamic_peps.json is the file in the irods_docs repo:
-# https://github.com/irods/irods_docs/blob/4-2-stable/dynamic_peps.json
+# https://raw.githubusercontent.com/irods/irods_docs/refs/heads/main/dynamic_peps.json
 #
 # Turns peps JSON into TAB-separated csv data.
 # Depends on jq.
