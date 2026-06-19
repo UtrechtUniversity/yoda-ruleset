55d898497c9cb385ce5d3f94ff17700de10fdb92
Lazlo Westerhof <l.r.westerhof@uu.nl>
2026-06-19 14:47:23 +0200

    YDA-5937: add unit tests for DOI generation

--- publication_utils.py
+++ publication_utils.py
@@ -3,6 +3,9 @@
 __copyright__ = 'Copyright (c) 2026, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
+from typing import Dict
+
+import datacite
 from util import constants
 
 
@@ -19,3 +22,31 @@ def should_abort(status: constants.publication_status) -> bool:
         constants.publication_status.UNRECOVERABLE,
         constants.publication_status.RETRY,
     )
+
+
+def generate_preliminary_doi(publication_config: Dict, publication_state: Dict) -> None:
+    """Generate a preliminary DOI. Preliminary, because we check for collision later.
+
+    :param publication_config: Dict with publication configuration
+    :param publication_state:  Dict with state of the publication process
+    """
+    datacite_prefix = publication_config["dataCitePrefix"]
+    yoda_prefix = publication_config["yodaPrefix"]
+    random_id = datacite.generate_random_id(publication_config["randomIdLength"])
+
+    publication_state["randomId"] = random_id
+    publication_state["versionDOI"] = f"{datacite_prefix}/{yoda_prefix}-{random_id}"
+
+
+def generate_base_doi(publication_config: Dict, publication_state: Dict) -> None:
+    """Generate a base DOI.
+
+    :param publication_config: Dict with publication configuration
+    :param publication_state:  Dict with state of the publication process
+    """
+    datacite_prefix = publication_config["dataCitePrefix"]
+    yoda_prefix = publication_config["yodaPrefix"]
+    random_id = datacite.generate_random_id(publication_config["randomIdLength"])
+
+    publication_state["baseRandomId"] = random_id
+    publication_state["baseDOI"] = f"{datacite_prefix}/{yoda_prefix}-{random_id}"
