b0782e3d02232981bd24eaa5cc81b09010fcf3f6
Sietse Snel <s.t.snel@uu.nl>
2026-07-21 15:10:22 +0200

    YDA-7210: improve performance get_groups_data
    The get_groups_data function used to query all group metadata. However, this
    includes statistics data, which is irrelevant to the group manager
    and can be quite large on production environments (much larger than the
    group data itself). This could slow down the group manager considerably.
    Fix this by only querying for attributes that we actually need.

--- groups.py
+++ groups.py
@@ -47,9 +47,10 @@ def getGroupsData(ctx: rule.Context) -> Iterable[Any]:
     groups = {}
 
     # First query: obtain a list of groups with group attributes.
+    included_attributes = "('category', 'subcategory', 'schema_id', 'data_classification', 'manager', 'description', 'expiration_date', 'sram_co')"
     iter = genquery.row_iterator(
         "USER_GROUP_NAME, META_USER_ATTR_NAME, META_USER_ATTR_VALUE",
-        "USER_TYPE = 'rodsgroup'",
+        f"USER_TYPE = 'rodsgroup' AND META_USER_ATTR_NAME IN {included_attributes}",
         genquery.AS_LIST, ctx
     )
 
